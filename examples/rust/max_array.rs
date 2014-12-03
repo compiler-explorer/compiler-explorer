3ee715691ebe785d5113f84374883234475de2ca
Matt Godbolt <matt@godbolt.org>
2014-12-02 21:10:16 -0600

    Fix warnings in rust example

--- examples/rust/max_array.rs
+++ examples/rust/max_array.rs
@@ -1,4 +1,4 @@
-pub fn maxArray(x: &mut[int, ..65536], y: &[int, ..65536]) {
+pub fn max_array(x: &mut[int, ..65536], y: &[int, ..65536]) {
   for i in range(0u, 65536) {
     if y[i] > x[i] {
       x[i] = y[i];
