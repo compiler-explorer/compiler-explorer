1a124be93283524e09f7ef49196af6ee9bd451f8
Matt Godbolt <matt@godbolt.org>
2015-01-11 20:39:49 -0600

    Fix up rust example for 1.0

--- examples/rust/max_array.rs
+++ examples/rust/max_array.rs
@@ -1,5 +1,5 @@
-pub fn max_array(x: &mut[int, ..65536], y: &[int, ..65536]) {
-  for i in range(0u, 65536) {
+pub fn max_array(x: &mut[i32; 65536], y: &[i32; 65536]) {
+  for i in (0..65536) {
     if y[i] > x[i] {
       x[i] = y[i];
     }
