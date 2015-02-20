6b97e767b5eac29f96d52e1447efb738d5176c64
Peter Bourgon <peter@bourgon.org>
2015-02-20 10:45:43 +0100

    Fix example Go code

--- examples/go/max_array.go
+++ examples/go/max_array.go
@@ -1,7 +1,11 @@
-package main;
+package main
 
-func MaxArray(x []float64, y []float64) {
-  for i, c := range x {
-    if y[i] > c { x[i] = y[i] }
-  }
+func MaxArray(x, y []float64) {
+	for i, c := range x {
+		if y[i] > c {
+			x[i] = y[i]
+		}
+	}
 }
+
+func main() {}
