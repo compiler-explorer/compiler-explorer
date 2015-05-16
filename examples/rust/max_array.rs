a66a0858740e46d0512d5d68522877cdfaa4b810
Matt Godbolt <matt@godbolt.org>
2015-05-16 08:12:23 -0500

    Fix up some rust syntax in the examples

--- examples/rust/max_array.rs
+++ examples/rust/max_array.rs
@@ -1,5 +1,5 @@
 pub fn max_array(x: &mut[f64; 65536], y: &[f64; 65536]) {
-  for i in (0..65536) {
+  for i in 0..65536 {
     x[i] = if y[i] > x[i] { y[i] } else { x[i] };
   }
 }
--- examples/rust/sum_over_array_opt.rs
+++ examples/rust/sum_over_array_opt.rs
@@ -1,4 +1,6 @@
-#[allow(unstable)]
+#![feature(core)]
+// Requires the use of the nightly rust
+// Compile with -O
 pub fn sum_array(x: &[i32]) -> i32 {
   unsafe {
     std::intrinsics::assume(x.as_ptr() as usize % 64 == 0);   
