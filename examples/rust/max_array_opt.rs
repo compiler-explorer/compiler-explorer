8c59c340c0801f0cff5db143eb204d977369dbea
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2016-03-02 23:50:58 +0100

    Fix Rust examples for current nightly
    Also remove trailing whitespaces.

--- examples/rust/max_array_opt.rs
+++ examples/rust/max_array_opt.rs
@@ -1,9 +1,9 @@
-#![feature(core)]
+#![feature(core_intrinsics)]
 // Requires the use of the nightly rust
 // Compile with -O
 pub fn max_array(x: &mut[f64; 65536], y: &[f64; 65536]) {
   unsafe {
-    std::intrinsics::assume(x.as_ptr() as usize % 64 == 0);   
+    std::intrinsics::assume(x.as_ptr() as usize % 64 == 0);
     std::intrinsics::assume(y.as_ptr() as usize % 64 == 0);
   }
   for i in 0..65536 {
