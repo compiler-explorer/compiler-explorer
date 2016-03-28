8610ec931685d8568aed0c886180adbf1f023c1c
Matt Godbolt <matt@godbolt.org>
2016-03-28 08:22:49 -0500

    Update copyright

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-// Copyright (c) 2012-2015, Matt Godbolt
+// Copyright (c) 2012-2016, Matt Godbolt
 // All rights reserved.
 // 
 // Redistribution and use in source and binary forms, with or without 
