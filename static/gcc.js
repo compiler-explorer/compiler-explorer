870355a530a622d077fd99ca05198cc8cdb1d435
Matt Godbolt <matt@godbolt.org>
2016-04-05 21:36:09 -0500

    Fix for compiler output window being too high.
    Fixes #119

--- static/gcc.js
+++ static/gcc.js
@@ -369,12 +369,12 @@ function initialise(options) {
 
     function resizeEditors() {
         var codeMirrors = $('.CodeMirror');
-        var top = codeMirrors.position().top;
+        var top = codeMirrors.offset().top;
         var windowHeight = $(window).height();
         var compOutputSize = Math.max(100, windowHeight * 0.05);
         $('.output').height(compOutputSize);
         var resultHeight = $('.result').height();
-        var height = windowHeight - top - resultHeight - 160;
+        var height = windowHeight - top - resultHeight - 40;
         currentCompiler.setEditorHeight(height);
     }
 
