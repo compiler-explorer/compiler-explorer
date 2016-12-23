2fefdc98ee5e0242cbc019a3bfb7e1533aaad6c5
Matt Godbolt <mgodbolt@drw.com>
2016-12-23 09:11:35 -0600

    More rust work in progress

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,21 +1,25 @@
 use std::io;
 use std::io::prelude::*;
+use regex::Regex;
+use regex::Captures;
 use rustc_demangle::demangle;
 
 extern crate rustc_demangle;
+extern crate regex;
 
 fn main() {
     let stdin = io::stdin();
+    let token = Regex::new(r"[_a-zA-Z$][_a-zA-Z$0-9]*").unwrap();
 
     for line in stdin.lock().lines() {
-        let a = line.unwrap();
-        println!("{}", demangle(&a));
+        println!("{}",
+                 line.unwrap()
+                     .split_whitespace()
+                     .map(|x| demangle(x).to_string())
+                     .collect::<Vec<String>>()
+                     .join(" "));
+//        println!("{}", token.replace(line.unwrap(), |caps: &Captures| {
+//            demangle(caps.at(0).unwrap()).to_string()
+//        }));
     }
-    /*
-                    let var: Vec<String> = input.split(' ')
-                    .into_iter()
-                    .map(|x| demangle(x).to_string())
-                    .collect();
-                println!("{}", var.join(" "));
-*/
 }
