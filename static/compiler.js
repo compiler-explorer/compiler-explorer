f277a166fdaf71d697ab6c23a549318b91db7ad2
Matt Godbolt <matt@godbolt.org>
2017-03-15 18:12:51 -0500

    Handle hex properly; extract function. Handle  hex style. Throttle mouse moves

--- static/compiler.js
+++ static/compiler.js
@@ -121,36 +121,7 @@ define(function (require) {
             }
         });
 
-        this.outputEditor.onMouseMove(function (e) {
-            if (e === null || e.target === null || e.target.position === null) return;
-            if (self.settings.hoverShowSource === true && self.assembly) {
-                var desiredLine = e.target.position.lineNumber - 1;
-                if (self.assembly[desiredLine]) {
-                    // We check that we actually have something to show at this point!
-                    self.eventHub.emit('editorSetDecoration', self.sourceEditorId, self.assembly[desiredLine].source);
-                }
-            }
-            if (e.target.element !== null && $.isNumeric(e.target.element.textContent)) {
-                var elementContent = e.target.element.textContent;
-                self.rawDecorations[0] = {
-                    range: e.target.range,
-                    options: {
-                        isWholeLine: false,
-                        hoverMessage: [
-                            parseInt(elementContent,16).toString(16) === elementContent.toLowerCase() ? // Is Hex?
-                                '0x' + parseInt(elementContent).toString(16)
-                            :
-                            (parseInt(elementContent,10) === elementContent ? // Is Decimal
-                                parseInt(elementContent).toString(10) 
-                            : // Else ... Show both
-                            ('0x' + parseInt(elementContent, 16).toString() + ' (' + parseInt(elementContent, 10).toString() + ')'))
-                            
-                        ]
-                    }
-                };
-                self.decorations = self.outputEditor.deltaDecorations(self.decorations, self.rawDecorations);
-            }
-        });
+        this.outputEditor.onMouseMove(_.throttle(_.bind(this.onMouseMove, this)), 250);
 
         this.fontScale = new FontScale(this.domRoot, state, this.outputEditor);
         this.fontScale.on('change', _.bind(function () {
@@ -555,6 +526,34 @@ define(function (require) {
         this.settings = _.clone(newSettings);
         if (!lastHoverShowSource && this.settings.hoverShowSource) {
             this.onCompilerSetDecorations(this.id, []);
+        }
+    };
+
+    var hexLike = /^([$]|0x)([0-9a-fA-F]+)$/;
+
+    function getToolTip(value) {
+        var match = hexLike.exec(value);
+        if (match) return parseInt(match[2], 16).toString();
+        if ($.isNumeric(value)) return '0x' + parseInt(value).toString(16);
+        return null;
+    }
+
+    Compiler.prototype.onMouseMove = function (e) {
+        if (e === null || e.target === null || e.target.position === null) return;
+        if (this.settings.hoverShowSource === true && this.assembly) {
+            var desiredLine = e.target.position.lineNumber - 1;
+            if (this.assembly[desiredLine]) {
+                // We check that we actually have something to show at this point!
+                this.eventHub.emit('editorSetDecoration', this.sourceEditorId, this.assembly[desiredLine].source);
+            }
+        }
+        var numericToolTip = e.target.element ? getToolTip(e.target.element.textContent) : null;
+        if (numericToolTip) {
+            this.rawDecorations[0] = {
+                range: e.target.range,
+                options: {isWholeLine: false, hoverMessage: [numericToolTip]}
+            };
+            this.decorations = this.outputEditor.deltaDecorations(this.decorations, this.rawDecorations);
         }
     };
 
