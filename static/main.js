55b9c94768f7c4e1e57359595ebec726be168374
Matt Godbolt <matt@godbolt.org>
2017-04-27 07:26:30 -0500

    Fix paths for FileSaver

--- static/main.js
+++ static/main.js
@@ -40,7 +40,7 @@ require.config({
         'lru-cache': 'ext/lru-cache/lib/lru-cache',
         vs: "ext/monaco-editor/min/vs",
         'bootstrap-slider': 'ext/seiyria-bootstrap-slider/dist/bootstrap-slider',
-        filesaver: '/ext/file-saver/FileSaver'
+        filesaver: 'ext/file-saver/FileSaver'
     },
     shim: {
         underscore: {exports: '_'},
