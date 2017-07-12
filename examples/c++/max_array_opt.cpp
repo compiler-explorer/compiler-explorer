26d7bfa6fa900edc05be795d7cff1774f84ec7e8
Matt Godbolt <matt@godbolt.org>
2017-07-12 12:34:04 -0500

    Fix max array opt version

--- examples/c++/max_array_opt.cpp
+++ examples/c++/max_array_opt.cpp
@@ -1,5 +1,5 @@
 // Compile with -O3 -march=native to see autovectorization
-typedef double * __attribute__((aligned(64))) aligned_double;
+typedef double *__attribute__((aligned(64))) aligned_double;
 
 void maxArray(aligned_double __restrict x, aligned_double __restrict y) {
     for (int i = 0; i < 65536; i++) {
