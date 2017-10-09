30999e90628a3ce212c93ac6f0c9cdc4cef632de
Rubén <rubrinbla@gmail.com>
2017-10-09 22:52:11 +0200

    Remove non-standard keywords and add missing ones

--- static/cppp-mode.js
+++ static/cppp-mode.js
@@ -16,10 +16,27 @@ define(function (require) {
             }
         }
 
-        removeKeyword("array");
-        removeKeyword("in");
-        removeKeyword("interface");
-        removeKeyword("ref");
+        function removeKeywords(keywords) {
+            for (var i = 0; i < keywords.length; ++i) {
+                removeKeyword(keywords[i]);
+            }
+        }
+
+        function addKeywords(keywords) {
+            // (Ruben) Done one by one as if you just push them all, Monaco complains that they're not strings, but as
+            // far as I can tell, they indeed are all strings. This somehow fixes it. If you know how to fix it, plz go
+            for (var i = 0; i < keywords.length; ++i) {
+                cppp.keywords.push(keywords[i]);
+            }
+        }
+
+        // We remove everything that's not an identifier, underscore reserved name and not an official C++ keyword...
+        removeKeywords(["abstract", "amp", "array", "cpu", "delegate", "each", "event", "final", "finally", "gcnew",
+            "generic", "in", "initonly", "interface", "interior_ptr", "internal", "literal", "partial", "pascal",
+            "pin_ptr", "property", "ref", "restrict", "safe_cast", "sealed", "title_static", "where"]);
+
+        addKeywords(["alignas", "alignof", "and", "and_eq", "asm", "bitand", "bitor", "char16_t", "char32_t", "compl",
+            "not", "not_eq", "or", "or_eq", "xor", "xor_eq"]);
 
         return cppp;
     }
