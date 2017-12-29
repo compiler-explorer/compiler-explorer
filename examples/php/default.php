52bc9cdad257e40d5888f03bf46ac56ef05cc390
Mike Cochrane <mikec@mikenz.geek.nz>
2017-12-30 10:56:02 +1300

    Disable opcache.file_update_protection so the zend optimizer can work

--- examples/php/default.php
+++ examples/php/default.php
@@ -1,8 +1,10 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 // Type your code here, or load an example.
+
+// Add the "-e" compiler option to see what the extra
+// opcodes to "Generate extended information for debugger/profiler"
+
 function add(int $a, int $b): int {
     return $a + $b;
 }
