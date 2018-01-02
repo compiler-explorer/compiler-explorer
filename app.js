429ffd92cda29e81b7555d295e0199eceaffc093
partouf <partouf@gmail.com>
2018-01-02 18:39:48 +0100

    const

--- app.js
+++ app.js
@@ -219,9 +219,9 @@ function ClientOptionsHandler(fileSources) {
         return {name: source.name, urlpart: source.urlpart};
     }), "name");
 
-    var supportsBinary = compilerPropsAT(languages, res => !!res, "supportsBinary", true);
-    var supportsExecutePerLanguage = compilerPropsAT(languages, (res, lang) => supportsBinary[lang.id] && !!res, "supportsExecute", true);
-    var supportsExecute = Object.values(supportsExecutePerLanguage).some((value) => value);
+    const supportsBinary = compilerPropsAT(languages, res => !!res, "supportsBinary", true);
+    const supportsExecutePerLanguage = compilerPropsAT(languages, (res, lang) => supportsBinary[lang.id] && !!res, "supportsExecute", true);
+    const supportsExecute = Object.values(supportsExecutePerLanguage).some((value) => value);
     var libs = {};
 
     var baseLibs = compilerPropsA(languages, "libs");
