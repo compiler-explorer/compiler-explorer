3e713b06679f650012b96e2c4f927056ff8034ad
Adrian Bibby Walther <adrianbibbywalther@gmail.com>
2018-02-27 13:13:20 +0100

    Fixed copyright and renamed to OptCompiler

--- test/llvm-ir-tests.js
+++ test/llvm-ir-tests.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2012-2018, Patrick Quist
+// Copyright (c) 2018, Adrian Bibby Walther
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
