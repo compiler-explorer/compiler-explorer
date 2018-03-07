a412d5aafb7bde3bd9c061c3c774f720ae261d74
Partouf <partouf@gmail.com>
2018-03-07 19:40:41 +0100

    ppci argument filter

---
+++ test/ppci-tests.js
@@ -0,0 +1,67 @@
+// Copyright (c) 2018, Patrick Quist
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+const chai = require('chai');
+const chaiAsPromised = require("chai-as-promised");
+const PPCICompiler = require('../lib/compilers/ppci');
+const CompilationEnvironment = require('../lib/compilation-env');
+const fs = require('fs-extra');
+const utils = require('../lib/utils');
+const logger = require('../lib/logger').logger;
+
+chai.use(chaiAsPromised);
+chai.should();
+
+const props = function (key, deflt) {
+    return deflt;
+};
+
+
+describe('PPCI', function () {
+    const ce = new CompilationEnvironment(props);
+    const info = {
+        "exe": null,
+        "remote": true,
+        "lang": "c"
+    };
+
+    ce.compilerPropsL = function (lang, property, defaultValue) {
+        return "";
+    };
+
+    it('Should be ok with most arguments', () => {
+        const compiler = new PPCICompiler(info, ce);
+        compiler.filterArgs(["hello", "-help", "--something"]).should.deep.equal(["hello", "-help", "--something"]);
+    });
+
+    it('Should be Not ok with path argument', () => {
+        const compiler = new PPCICompiler(info, ce);
+        compiler.filterArgs(["hello", "--stuff", "/proc/cpuinfo"]).should.deep.equal(["hello", "--stuff"]);
+    });
+    
+    it('Should be Not ok with report arguments', () => {
+        const compiler = new PPCICompiler(info, ce);
+        compiler.filterArgs(["hello", "--report", "--text-report"]).should.deep.equal(["hello"]);
+    });
+});
