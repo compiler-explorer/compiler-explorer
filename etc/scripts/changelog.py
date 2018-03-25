fac2f526b0ad9ec0a2132b5a3b59644d23dbb593
RabsRincon <rubrinbla@gmail.com>
2018-03-25 21:31:02 +0200

    Use git to check for commits locally
    Based on @jaredwy comments

--- etc/scripts/changelog.py
+++ etc/scripts/changelog.py
@@ -4,58 +4,31 @@ from datetime import date
 from dateutil.relativedelta import relativedelta
 import traceback
 import json
+import subprocess
 
 
-def format_commit(commit):
+def format_commit(url, commit):
+    splitted_commit = commit.split(' * ')
+    print(splitted_commit)
     return '<div class="row commits-list">\n' \
-           '    <div class="col-sm-10">\n' \
-           '        <p>{}{}</p>\n' \
-           '    </div>\n' \
-           '    <div class="col-sm-2">\n' \
-           '        <a href="{}" rel="noreferrer noopener" target="_blank">{}\n' \
+           '    <div class="col-sm-3">\n' \
+           '        <a href="{}commit/{}" rel="noreferrer noopener" target="_blank">{}\n' \
            '            <sup><small class="glyphicon glyphicon-new-window opens-new-window" ' \
            'title="Opens in a new window"></small></sup>\n' \
            '        </a>\n' \
            '    </div>\n' \
-           '</div>\n'.format(commit['message'], commit['ellipsis'], commit['url'], commit['sha'][0:6])
+           '    <div class="col-sm-9">\n' \
+           '        <p>{}</p>\n' \
+           '    </div>\n' \
+           '</div>\n'.format(url, splitted_commit[0], splitted_commit[0], splitted_commit[1])
 
 
-def get_commits(url):
-    three_months_ago = (date.today() + relativedelta(months=-3)).isoformat()
-    headers = {'Accept': 'application/vnd.github.v3+json',
-               'Content-Type': 'application/json'}
-    useful_commits = query_commits('{}?{}'.format(url, urllib.urlencode({'since': three_months_ago})), headers, [])
-    print('Got {} noteworthy commits. Writing to static/changelog.html'.format(len(useful_commits)))
+def get_commits(repo):
+    coms = subprocess.check_output(['git', 'log', '--date=local', '--after="3 months ago"', '--grep=^* ', '--oneline'])
     with open('static/changelog.html', 'w') as f:
-        for commit in useful_commits:
-            f.write(format_commit(commit))
-
-
-def query_commits(url, headers, commits):
-    print('Querying {}'.format(url))
-    req = urllib2.Request(url, None, headers)
-    try:
-        response = urllib2.urlopen(req)
-        the_page = json.load(response)
-        for obj in the_page:
-            if obj['commit']['message'].startswith('* '):
-                newline_position = obj['commit']['message'].find('\n')
-                commits.append({'message': obj['commit']['message'][2:newline_position],
-                                'url': obj['commit']['url'],
-                                'sha': obj['sha'],
-                                'ellipsis': '...' if len(obj['commit']['message']) > 80 else ''})
-        if response.headers['link'] and len(commits) < 20:
-            first_link = response.headers['link'].split(',')[0]
-            if first_link.find('rel="next"') > -1:
-                link = first_link.split(';')[0]
-                query_commits(link[1:len(link) - 1], headers, commits)
-    except urllib2.HTTPError as err:
-        print('Got an error of {}'.format(err))
-    except Exception as ex:
-        print('Got general exception of {}: {}'.format(ex, traceback.format_exc()))
-    finally:
-        return commits
+        for commit in coms.splitlines():
+            f.write(format_commit(repo, commit))
 
 
 if __name__ == '__main__':
-    get_commits('https://api.github.com/repos/mattgodbolt/compiler-explorer/commits')
+    get_commits('https://github.com/mattgodbolt/compiler-explorer/')
