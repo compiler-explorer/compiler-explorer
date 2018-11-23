061e030d9f77f2cc998bdd1157a806211fce84df
Mitch Kennedy <mitchtdk@outlook.com>
2018-11-23 18:26:36 +1100

    Fixed typos in example programs.

--- examples/ada/default.adb
+++ examples/ada/default.adb
@@ -9,9 +9,9 @@ begin
     return num**2;
 end Square;
 
--- Ada 2012 also provides Expressiion Functions 
+-- Ada 2012 also provides Expression Functions
 -- (http://www.ada-auth.org/standards/12rm/html/RM-6-8.html)
 -- as a short hand for functions whose body consists of a 
--- single return statement. Hhowever they cannot be used as a
+-- single return statement. However they cannot be used as a
 -- complication unit.
 -- function Square(num : Integer) return Integer is (num**2);
