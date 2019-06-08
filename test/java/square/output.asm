940e0731d37f31a5e0902b6ba2cc57101a451302
Christian Vonrüti <christian@vonrueti.ch>
2019-06-08 02:53:55 +0200

    Tests for java support

---
+++ test/java/square/output.asm
@@ -0,0 +1,14 @@
+class Square {
+  Square();
+line 2:       0: aload_0
+line 2:       1: invokespecial #1                  // Method java/lang/Object."<init>":()V
+line 2:       4: return
+
+
+  static int square(int);
+line 4:       0: iload_0
+line 4:       1: iload_0
+line 4:       2: imul
+line 4:       3: ireturn
+
+}
---
+++ test/java/two-classes/input.java
@@ -0,0 +1,11 @@
+class ZFirstClass {
+    static int square(int num) {
+        return num * num;
+    }
+}
+
+class ASecondClass {
+    static int square(int num) {
+        return num * num;
+    }
+}
