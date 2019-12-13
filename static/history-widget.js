8968492b61da3b0a59faa580b556f2755127eee8
Partouf <partouf@gmail.com>
2019-12-13 15:51:25 +0100

    add history to localstorage + way to restore state

---
+++ static/history-widget.js
@@ -0,0 +1,90 @@
+// Copyright (c) 2019, Compiler Explorer Authors
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+"use strict";
+
+var
+    $ = require('jquery'),
+    _ = require('underscore'),
+    ga = require('analytics'),
+    history = require('./history');
+
+function History() {
+    this.modal = null;
+}
+
+History.prototype.initializeIfNeeded = function () {
+    if (this.modal === null) {
+        this.modal = $("#history");
+    }
+};
+
+History.prototype.populateFromLocalStorage = function () {
+    this.populate(
+        this.modal.find('.historiccode'),
+        _.map(history.list(), _.bind(function (data) {
+            var dt = new Date(data.dt);
+            return {
+                name: dt.toString(),
+                load: _.bind(function () {
+                    this.onLoad(data);
+                    this.modal.modal('hide');
+                }, this)
+            };
+        }, this)));
+};
+
+History.prototype.onLoad = function (data) {
+    console.log(data);
+};
+
+History.prototype.populate = function (root, list) {
+    root.find('li:not(.template)').remove();
+    var template = root.find('.template');
+    _.each(list, _.bind(function (elem) {
+        template
+            .clone()
+            .removeClass('template')
+            .appendTo(root)
+            .find('a')
+            .text(elem.name)
+            .click(elem.load);
+    }, this));
+};
+
+History.prototype.run = function (onLoad) {
+    this.initializeIfNeeded();
+    this.populateFromLocalStorage();
+    this.onLoad = onLoad;
+    this.modal.modal();
+    ga.proxy('send', {
+        hitType: 'event',
+        eventCategory: 'OpenModalPane',
+        eventAction: 'History'
+    });
+};
+
+module.exports = {
+    History: History
+};
---
+++ static/history.js
@@ -0,0 +1,93 @@
+// Copyright (c) 2019, Compiler Explorer Authors
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+"use strict";
+var
+    local = require('./local'),
+    _ = require('underscore');
+
+function extractEditorSources(content) {
+    var sources = [];
+
+    for (var i = 0; i < content.length; i++) {
+        var component = content[i];
+        if (component.content) {
+            var subsources = extractEditorSources(component.content);
+            if (subsources.length > 0) {
+                sources = sources.concat(subsources);
+            }
+        } else if (component.componentName === 'codeEditor') {
+            sources.push(component.componentState.source);
+        }
+    }
+
+    return sources;
+};
+
+function push(stringifiedConfig) {
+    var config = JSON.parse(stringifiedConfig);
+    var editors = extractEditorSources(config.content);
+    if (editors.length > 0) {
+        var stringifiedHistory = local.get('history');
+        var completeHistory = JSON.parse(stringifiedHistory ? stringifiedHistory : '[]');
+
+        var duplicateIdx = -1;
+        for (var i = 0; i < completeHistory.length; i++) {
+            var diff = _.difference(editors, completeHistory[i].code);
+            if (diff.length === 0) {
+                duplicateIdx = i;
+                break;
+            }
+        }
+
+        if (duplicateIdx === -1) {
+            while (completeHistory.length >= 30) {
+                completeHistory.shift();
+            }
+    
+            completeHistory.push({
+                dt: Date.now(),
+                code: editors,
+                config: config
+            });
+        } else {
+            var entry = completeHistory[duplicateIdx];
+            entry.dt = Date.now();
+        }
+
+        local.set('history', JSON.stringify(completeHistory));
+    }
+};
+
+function list() {
+    var stringifiedHistory = local.get('history');
+    var completeHistory = JSON.parse(stringifiedHistory ? stringifiedHistory : '[]');
+
+    return completeHistory;
+};
+
+module.exports = {
+    push,
+    list
+};
