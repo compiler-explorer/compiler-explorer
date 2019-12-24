34922876388b5c4155c100e1b79acc112c4eaf76
bastien penavayre <bastien.penavayre.ext@mappy.com>
2019-12-24 13:33:11 +0100

    Add support for Nim language
    - Add language
    - Adds Nim compiler
    - supports js, c, c++ outputs (objc not tested)
    - prevent --run option
    - Basic test for nim
    
    Todos:
    - monaco profile for Nim

---
+++ examples/nim/default.nim
@@ -0,0 +1,9 @@
+# Minimal example
+# Allowed commands:
+#    - "compile", "compileToC", "c" to build C assambly
+#    - "compileToCpp", "cpp", "cc" to build C++ assambly
+#    - "compileToOC", "objc" to build objective-C assambly
+#    - "js" to output js code
+#    - "check" to check code validity
+
+echo "hello world"
