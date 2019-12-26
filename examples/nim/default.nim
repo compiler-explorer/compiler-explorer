620d9f4b62189609a59bc726a8922aae9e9909d9
bastien penavayre <bastien.penavayre.ext@mappy.com>
2019-12-26 17:48:40 +0100

    Standardize nim example

--- examples/nim/default.nim
+++ examples/nim/default.nim
@@ -1,9 +1,3 @@
-# Minimal example
-# Allowed commands:
-#    - "compile", "compileToC", "c" to build C assambly
-#    - "compileToCpp", "cpp", "cc" to build C++ assambly
-#    - "compileToOC", "objc" to build objective-C assambly
-#    - "js" to output js code
-#    - "check" to check code validity
-
-echo "hello world"
+# Type your code here, or load an example.
+proc square(num: int): int =
+    return num * num
