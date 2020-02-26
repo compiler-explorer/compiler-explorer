042ccaeb2a4f07b4859d7494f99853557496e518
Matt Godbolt <matt@godbolt.org>
2020-02-26 08:32:23 -0600

    Fix up lint error

--- static/modes/cuda-mode.js
+++ static/modes/cuda-mode.js
@@ -43,7 +43,9 @@ function definition() {
     cuda.tokenPostfix = '.cu';
 
     // Keywords for CUDA
-    addKeywords(["__host__", "__global__", "__device__", "__shared__", "__noinline__", "__forceinline__", "__restrict__"]);
+    addKeywords([
+        "__host__", "__global__", "__device__", "__shared__", "__noinline__", "__forceinline__", "__restrict__"
+    ]);
 
     return cuda;
 }
