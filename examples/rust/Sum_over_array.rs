91a35e65d4990d05165849c274a7ba261d0a54da
Christopher Serr <christopher.serr@gmail.com>
2020-03-26 23:23:35 +0100

    Improve the Rust examples
    This commit
     - formats the Rust examples idiomatically
     - uses idiomatic code
     - uses only safe code without compromising on performance
     - makes them compile on stable

--- examples/rust/Sum_over_array.rs
+++ examples/rust/Sum_over_array.rs
@@ -1,13 +1,3 @@
-pub fn sum_array_loop(input: &[i32]) -> i32 {
-  let mut sum: i32 = 0;
-
-  for i in 0..input.len() {
-    sum += input[i];
-  }
-
-  sum
-}
-
-pub fn sum_array_iterator(input: &[i32]) -> i32 {
-  input.iter().sum()
+pub fn sum_array(input: &[i32]) -> i32 {
+    input.iter().sum()
 }
