5513f1bd0ed2d2242ada057cada57acd8df75ce6
Tony Yang <38984697+tonyyzy@users.noreply.github.com>
2020-05-10 19:18:42 +0100

    Fix Max_array.rs iter_mut is not necessary
    The previous example failed to compile as `y` was not borrowed as a mutable reference but used to produce a mutable iterator. Changing from `iter_mut()` to `iter()` resolved this issue.

--- examples/rust/Max_array.rs
+++ examples/rust/Max_array.rs
@@ -1,5 +1,5 @@
 pub fn max_array(x: &mut [f64; 65536], y: &[f64; 65536]) {
-    for (x, y) in x.iter_mut().zip(y.iter_mut()) {
+    for (x, y) in x.iter_mut().zip(y.iter()) {
         *x = if *y > *x { *y } else { *x };
     }
 }
