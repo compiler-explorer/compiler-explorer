21fea37af56e2bf15399946635f9efcb421818e2
Austin Morton <austinpmorton@gmail.com>
2020-08-27 07:47:47 -0400

    Fix #2150

--- static/panes/editor.js
+++ static/panes/editor.js
@@ -763,9 +763,26 @@ Editor.prototype.updateSource = function (newSource) {
 
     if (!this.awaitingInitialResults) {
         if (this.selection) {
-            this.editor.setSelection(this.selection);
-            this.editor.revealLinesInCenter(this.selection.startLineNumber,
-                this.selection.endLineNumber);
+            /*
+             * this setTimeout is a really crap workaround to fix #2150
+             * the TL;DR; is that we reach this point *before* GL has laid
+             * out the window, so we have no height
+             *
+             * If we revealLinesInCenter at this point the editor "does the right thing"
+             * and scrolls itself all the way to the line we requested.
+             *
+             * Unfortunately the editor thinks it is very small, so the "center"
+             * is the first line, and when the editor does resize eventually things are off.
+             *
+             * The workaround is to just delay things "long enough"
+             *
+             * This is bad and I feel bad.
+             */
+            setTimeout(_.bind(function () {
+                this.editor.setSelection(this.selection);
+                this.editor.revealLinesInCenter(this.selection.startLineNumber,
+                    this.selection.endLineNumber);
+            }, this), 500);
         }
         this.awaitingInitialResults = true;
     }
