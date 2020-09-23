ca437e15b6d2ff57a2c0cd6cd6dd2e6ecde74517
Austin Morton <apmorton@users.noreply.github.com>
2020-09-23 02:09:32 -0400

    Consolidate eslint configuration (#2224)

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const path = require('path'),
     OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin'),
     MonacoEditorWebpackPlugin = require('monaco-editor-webpack-plugin');
 
-const isDev = process.env.NODE_ENV !== "production";
+const isDev = process.env.NODE_ENV !== 'production';
 
 const distPath = path.resolve(__dirname, 'out', 'dist');
 const staticPath = path.join(distPath, 'static');
@@ -15,29 +15,29 @@ const staticPath = path.join(distPath, 'static');
 // Hack alert: due to a variety of issues, sometimes we need to change
 // the name here. Mostly it's things like webpack changes that affect
 // how minification is done, even though that's supposed not to matter.
-const webjackJsHack = ".v3.";
+const webjackJsHack = '.v3.';
 const plugins = [
     new MonacoEditorWebpackPlugin({
         languages: [ 'cpp', 'go', 'pascal', 'python', 'rust', 'swift' ],
-        filename: isDev ? '[name].worker.js' : `[name]${webjackJsHack}worker.[contenthash].js`
+        filename: isDev ? '[name].worker.js' : `[name]${webjackJsHack}worker.[contenthash].js`,
     }),
     new CopyWebpackPlugin([
         {
             from: 'node_modules/es6-shim/es6-shim.min.js',
-            to: staticPath
-        }
+            to: staticPath,
+        },
     ]),
     new webpack.ProvidePlugin({
         $: 'jquery',
-        jQuery: 'jquery'
+        jQuery: 'jquery',
     }),
     new MiniCssExtractPlugin({
-        filename: isDev ? '[name].css' : '[name].[contenthash].css'
+        filename: isDev ? '[name].css' : '[name].[contenthash].css',
     }),
     new ManifestPlugin({
         fileName: path.join(distPath, 'manifest.json'),
-        publicPath: ''
-    })
+        publicPath: '',
+    }),
 ];
 
 module.exports = {
@@ -48,13 +48,13 @@ module.exports = {
     },
     output: {
         filename: isDev ? '[name].js' : `[name]${webjackJsHack}[contenthash].js`,
-        path: staticPath
+        path: staticPath,
     },
     resolve: {
         alias: {
-            'monaco-editor$': 'monaco-editor/esm/vs/editor/editor.api'
+            'monaco-editor$': 'monaco-editor/esm/vs/editor/editor.api',
         },
-        modules: ['./static', './node_modules']
+        modules: ['./static', './node_modules'],
     },
     stats: 'normal',
     devtool: 'source-map',
@@ -63,28 +63,28 @@ module.exports = {
         splitChunks: {
             cacheGroups: {
                 vendors: {
-                    test: /[\\/]node_modules[\\/]/,
+                    test: /[/\\]node_modules[/\\]/,
                     name: 'vendor',
                     chunks: 'all',
                     priority: -10,
-                }
-            }
+                },
+            },
         },
         moduleIds: 'hashed',
         minimizer: [
             new OptimizeCssAssetsPlugin({
                 cssProcessorPluginOptions: {
                     preset: ['default', { discardComments: { removeAll: true } }],
-                }
+                },
             }),
             new TerserPlugin({
                 parallel: true,
                 sourceMap: true,
                 terserOptions: {
-                    ecma: 5
-                }
-            })
-        ]
+                    ecma: 5,
+                },
+            }),
+        ],
     },
     module: {
         rules: [
@@ -96,26 +96,26 @@ module.exports = {
                         loader: MiniCssExtractPlugin.loader,
                         options: {
                             publicPath: './',
-                            hmr: isDev
-                        }
+                            hmr: isDev,
+                        },
                     },
-                    'css-loader'
-                ]
+                    'css-loader',
+                ],
             },
             {
                 test: /\.css$/,
                 include: path.resolve(__dirname, 'static/themes/'),
-                loader: 'css-loader'
+                loader: 'css-loader',
             },
             {
                 test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-                loader: 'url-loader?limit=8192'
+                loader: 'url-loader?limit=8192',
             },
             {
                 test: /\.(html)$/,
-                loader: 'html-loader'
-            }
-        ]
+                loader: 'html-loader',
+            },
+        ],
     },
-    plugins: plugins
+    plugins: plugins,
 };
