ed7227fc3834cbaaf8e37fbad52438a79236a854
Partouf <partouf@gmail.com>
2020-09-24 21:43:49 +0200

    make libs button styles also work for darkmode

--- static/panes/compiler.js
+++ static/panes/compiler.js
@@ -1216,9 +1216,9 @@ Compiler.prototype.updateButtons = function () {
     this.executorButton.prop('disabled', !this.compiler.supportsExecute);
 
     if (this.currentState().libs.length > 0) {
-        this.libsButton.addClass('btn-success');
+        this.libsButton.addClass('btn-success').removeClass('btn-light');
     } else {
-        this.libsButton.removeClass('btn-success');
+        this.libsButton.removeClass('btn-success').addClass('btn-light');
     }
 
     this.enableToolButtons();
