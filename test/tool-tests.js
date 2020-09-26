9dcd208879767762ce752e3b9d802d0f900c3f09
Austin Morton <austinpmorton@gmail.com>
2020-09-26 19:04:48 -0400

    Fix lint warning

--- test/tool-tests.js
+++ test/tool-tests.js
@@ -22,8 +22,9 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-import { path } from './utils';
 import { CompilerDropinTool } from '../lib/tooling/compiler-dropin-tool';
+
+import { path } from './utils';
 
 describe('CompilerDropInTool', () => {
     it('Should support llvm based compilers', () => {
