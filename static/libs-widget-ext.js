f7729fa27ac13ae690338e7278020b5f83af391f
Partouf <partouf@gmail.com>
2020-09-27 01:05:37 +0200

    no real need for this

--- static/libs-widget-ext.js
+++ static/libs-widget-ext.js
@@ -22,8 +22,6 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-'use strict';
-
 var options = require('options'),
     _ = require('underscore'),
     local = require('./local'),
