fea050141bac66ba564c7813e38909c5f9c582c1
Danil Yarantsev <tiberiumk12@gmail.com>
2020-10-09 02:09:47 +0300

    Replace it with openArray

--- examples/nim/Max_array.nim
+++ examples/nim/Max_array.nim
@@ -1,5 +1,5 @@
 # For best results with compile with -d:danger --passC:"-march=native"
-proc maxArray(x: var seq[float], y: seq[float]) {.exportc.} = 
+proc maxArray(x: var openArray[float], y: openArray[float]) {.exportc.} = 
   for i in 0 ..< x.len:
     if y[i] > x[i]:
       x[i] = y[i]
--- examples/nim/Sum_over_array.nim
+++ examples/nim/Sum_over_array.nim
@@ -1,4 +1,4 @@
 # For best results compile with -d:danger --passC:"-march=native" 
-proc sumArray(data: seq[int]): int {.exportc.} = 
+proc sumArray(data: openArray[int]): int {.exportc.} = 
   for elem in data:
     result += elem
