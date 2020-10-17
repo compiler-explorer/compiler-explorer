f7e16371ecb80db15e848ec62bafc8fb3db5ff60
partouf <partouf@gmail.com>
2020-10-17 17:11:11 +0000

    test binary asm-parser with the c++ ignore func regex thing

--- test/filter-tests.js
+++ test/filter-tests.js
@@ -82,8 +82,23 @@ function processAsm(filename, filters) {
         parser = new VcAsmParser();
     else if (filename.includes('sass-'))
         parser = new SassAsmParser();
-    else
-        parser = new AsmParser();
+    else {
+        let options;
+        if (filters.binary) {
+            // assume it's c++?
+            // binaryHideFuncRe=^(__.*|_(init|start|fini)|(de)?register_tm_clones|call_gmon_start|frame_dummy|\.plt.*|_dl_relocate_static_pie)$
+            options = (prop, def) => {
+                if (prop === 'binaryHideFuncRe') {
+                    return '^(__.*|_(init|start|fini)|(de)?register_tm_clones|call_gmon_start|frame_dummy|.plt.*|_dl_relocate_static_pie)$';
+                } else {
+                    return def;
+                }
+            };
+        }
+
+        parser = new AsmParser(options);
+    }
+
     return parser.process(file, filters);
 }
 
