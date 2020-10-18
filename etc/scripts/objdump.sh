1fd62bb8ce15c013d0a72b346b2d8c591cdd50bf
Patrick Quist <partouf@gmail.com>
2020-10-18 18:05:58 +0200

    fix path

--- etc/scripts/objdump.sh
+++ etc/scripts/objdump.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 OBJDUMP=/opt/compiler-explorer/gcc-10.1.0/bin/objdump
-ASMPARSER=/opt/compiler-explorer/asm-parser/build/bin/asm-parser
+ASMPARSER=/opt/compiler-explorer/asm-parser/bin/asm-parser
 
 $OBJDUMP "$@" | $ASMPARSER -stdin -binary
