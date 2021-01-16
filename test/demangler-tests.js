9d487e913e99b15267aa44d62ac9ea0dfb6f8fed
Matt Godbolt <matt@godbolt.org>
2021-01-16 16:12:56 -0600

    Use a prefix tree instead of binary search to ensure longest match requirements

--- test/demangler-tests.js
+++ test/demangler-tests.js
@@ -23,6 +23,7 @@
 // POSSIBILITY OF SUCH DAMAGE.
 
 import { CppDemangler, Win32Demangler } from '../lib/demangler';
+import { PrefixTree } from '../lib/demangler/prefix-tree';
 import * as exec from '../lib/exec';
 import { SymbolStore } from '../lib/symbol-store';
 import * as utils from '../lib/utils';
@@ -281,4 +282,34 @@ describe('File demangling', () => {
             });
         }
     }
+});
+
+describe('Demangling replacements', () => {
+    const replacements = new PrefixTree();
+    replacements.add('a', 'short_a');
+    replacements.add('aa', 'long_a');
+    replacements.add('aa_shouldnotmatch', 'ERROR');
+    it('should replace a short match', () => {
+        replacements.replaceAll('a').should.eq('short_a');
+    });
+    it('should replace using the longest match', () => {
+        replacements.replaceAll('aa').should.eq('long_a');
+    });
+    it('should replace using both', () => {
+        replacements.replaceAll('aaa').should.eq('long_ashort_a');
+    });
+    it('should replace using both', () => {
+        replacements.replaceAll('a aa a aa').should.eq('short_a long_a short_a long_a');
+    });
+    it('should work with empty replacements', () => {
+        new PrefixTree().replaceAll('Testing 123').should.eq('Testing 123');
+    });
+    it('should leave unmatching text alone', () => {
+        replacements.replaceAll('Some text with none of the first letter of the ordered letter list')
+            .should.eq('Some text with none of the first letter of the ordered letter list');
+    });
+    it('should handle a mixture', () => {
+        replacements.replaceAll('Everyone loves an aardvark')
+            .should.eq('Everyone loves short_an long_ardvshort_ark');
+    });
 });
