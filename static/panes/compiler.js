a9e222f671ec660edd6057a71bd4a0756dae4f5b
RabsRincon <ruben@rinconblanco.es>
2021-03-19 21:13:23 +0100

    Add filtered report in compile info label

--- static/panes/compiler.js
+++ static/panes/compiler.js
@@ -563,7 +563,7 @@ Compiler.prototype.compile = function (bypassCache, newTools) {
         return;
     }
     this.needsCompile = false;
-    this.compileTimeLabel.text(' - Compiling...');
+    this.compileInfoLabel.text(' - Compiling...');
     var options = {
         userArguments: this.options,
         compilerOptions: {
@@ -797,18 +797,22 @@ Compiler.prototype.onCompileResponse = function (request, result, cached) {
     } else {
         this.outputBtn.prop('title', allText.replace(/\x1b\[[0-9;]*m(.\[K)?/g, ''));
     }
-    var timeLabelText = '';
+    var infoLabelText = '';
     if (cached) {
-        timeLabelText = ' - cached';
+        infoLabelText = ' - cached';
     } else if (wasRealReply) {
-        timeLabelText = ' - ' + timeTaken + 'ms';
+        infoLabelText = ' - ' + timeTaken + 'ms';
     }
 
     if (result.asmSize !== undefined) {
-        timeLabelText += ' (' + result.asmSize + 'B)';
+        infoLabelText += ' (' + result.asmSize + 'B)';
     }
 
-    this.compileTimeLabel.text(timeLabelText);
+    if (result.filteredCount > 0) {
+        infoLabelText += ' ~'+ result.filteredCount + (result.filteredCount === 1 ? ' line' : ' lines') + ' filtered';
+    }
+
+    this.compileInfoLabel.text(infoLabelText);
 
     this.postCompilationResult(request, result);
     this.eventHub.emit('compileResult', this.id, this.compiler, result, languages[this.currentLangId]);
@@ -1066,7 +1070,7 @@ Compiler.prototype.initButtons = function (state) {
     this.executorButton = this.domRoot.find('.create-executor');
     this.libsButton = this.domRoot.find('.btn.show-libs');
 
-    this.compileTimeLabel = this.domRoot.find('.compile-time');
+    this.compileInfoLabel = this.domRoot.find('.compile-info');
     this.compileClearCache = this.domRoot.find('.clear-cache');
 
     this.outputBtn = this.domRoot.find('.output-btn');
