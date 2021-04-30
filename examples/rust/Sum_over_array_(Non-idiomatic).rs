9d4871f9dba8a7c33b4baba01ae71e699543bb9a
redzic <yusufredzic@gmail.com>
2021-04-30 18:46:42 -0500

    Make 'non-idiomatic' example match the aligned version

--- examples/rust/Sum_over_array_(Non-idiomatic).rs
+++ examples/rust/Sum_over_array_(Non-idiomatic).rs
@@ -1,17 +1,13 @@
 // Compile with -C opt-level=3 -C target-cpu=native to see autovectorization
-// assumes input's length is a multiple of 64
-pub fn sum_array(input: &[i32]) -> i32 {
-    let len = input.len();
-    let input = input.as_ptr();
 
-    if len & 63 != 0 {
+// assumes input is aligned on 64-byte boundary and that
+// input's length is a multiple of 64.
+pub fn sum_array(input: &[i32]) -> i32 {
+    if input.len() & 63 != 0 {
         unsafe { std::hint::unreachable_unchecked() }
     }
 
-    let mut sum = 0;
-    for i in 0..len {
-        sum += unsafe { *input.add(i) };
-    }
-
-    sum
+    (0..input.len())
+        .map(|i| unsafe { *input.as_ptr().add(i) })
+        .sum()
 }
