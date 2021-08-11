b0ef781dcb1c277a72f8a00d562e22ab0a781ee5
Mats Larsen <me@supergrecko.com>
2021-08-11 02:08:23 +0200

    Fix nits from review

--- etc/scripts/docenizerJava.sh
+++ etc/scripts/docenizerJava.sh
@@ -1,5 +1,7 @@
 #/usr/bin/bash
 
-curl https://docs.oracle.com/javase/specs/jvms/se16/html/jvms-6.html -o `pwd`/vendor/jvms.html
+JVMS_PATH=$(pwd)/vendor/jvms.html
+
+[ -f "$JVMS_PATH" ] || curl https://docs.oracle.com/javase/specs/jvms/se16/html/jvms-6.html -o "$JVMS_PATH"
 
 node docenizer-java.js > ../../lib/handlers/asm-docs-java.js
