995d8ea8ad3bfb33f1ae8de71f491e8f95dc99ab
partouf <partouf@gmail.com>
2021-08-31 10:35:27 +0000

    extra label definition screws up logic

--- test/filter-tests.js
+++ test/filter-tests.js
@@ -41,8 +41,10 @@ function processAsm(filename, filters) {
         parser = new VcAsmParser();
     else if (filename.includes('sass-'))
         parser = new SassAsmParser();
-    else
+    else {
         parser = new AsmParser();
+        parser.binaryHideFuncRe = /^(__.*|_(init|start|fini)|(de)?register_tm_clones|call_gmon_start|frame_dummy|\.plt.*|_dl_relocate_static_pie)$/;
+    }
     return parser.process(file, filters);
 }
 
