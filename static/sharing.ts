compiler-explorer/compiler-explorer

commit 192af6070132dae0c4ad6e1fc0f2d31cb127c0b6

Sketch a possible solution for #2436 (#2510)
* Sketch a possible solution for #2436

* Do the dirty and just call setupSharing twice

* add embeds share, reuse embed dialog and get rid of old popup

* styling fixes

* rename dialog

* refactor and reintroduce ctrl+s implementation

* reintroduce analytics sending

Co-authored-by: partouf <partouf@gmail.com>

static/explorer.scss
@@ -755,3 +755,7 @@ html[data-theme=dark] {
     @import "themes/dark-theme";
     background-color: #333 !important;
 }
+
+.socialsharing {
+    max-width: 250px;
+}

static/main.js
@@ -526,9 +526,17 @@ function start() {
         setupButtons(options);
     }
 
-    sharing.initShareButton($('#share'), layout, function (config, extra) {
+    function storeCurrentConfig(config, extra) {
         window.history.pushState(null, null, extra);
         storedPaths[JSON.stringify(config)] = extra;
+    }
+
+    sharing.initShareButton($('#shareShort'), layout, storeCurrentConfig, 'Short');
+    sharing.initShareButton($('#shareFull'), layout, storeCurrentConfig, 'Full');
+    sharing.initShareButton($('#shareEmbed'), layout, storeCurrentConfig, 'Embed');
+
+    layout.eventHub.on('displaySharingPopover', function () {
+        $('#shareShort').trigger('click');
     });
 
     function setupAdd(thing, func) {

static/sharing.js
@@ -107,152 +107,113 @@ function updateShares(container, url) {
     });
 }
 
-function initShareButton(getLink, layout, noteNewState) {
-    var baseUrl = window.location.protocol + '//' + window.location.hostname;
-    var html = $('.template .urls').html();
+function initShareButton(getLink, layout, noteNewState, startingBind) {
     var currentNode = null;
     // Explicit because webstorm gets confused about the type of this variable.
     /***
      * Current URL bind
      * @type {string}
      */
-    var currentBind = '';
-    var title = getLink.prop('title'); // preserve before popover/tooltip breaks it
+    var currentBind = startingBind;
 
-    getLink.popover({
-        container: 'body',
-        content: html,
-        html: true,
-        placement: 'bottom',
-        trigger: 'manual',
-        sanitize: false,
-    }).click(function () {
-        getLink.popover('toggle');
-    }).on('inserted.bs.popover', function () {
+    var popoverModal = $('#sharelinkdialog');
+    var socialSharingElements = popoverModal.find('.socialsharing');
+    var root = $('.urls-container:visible');
+    var label = root.find('.current');
+    var permalink = $('.permalink');
+    var urls = {};
+
+    var embedsettings = $('#embedsettings');
+
+    function setCurrent(node) {
+        currentNode = node;
+        currentBind = node.data().bind;
+        if (currentBind === 'Embed') {
+            embedsettings.show();
+        } else {
+            embedsettings.hide();
+        }
+    }
+
+    function setSocialSharing(element, sharedUrl) {
+        if (options.sharingEnabled) {
+            updateShares(element, sharedUrl);
+            // Disable the links for every share item which does not support embed html as links
+            if (currentBind === 'Embed') {
+                element.children('.share-no-embeddable')
+                    .addClass('share-disabled')
+                    .prop('title', 'Embed links are not supported in this service')
+                    .on('click', false);
+            }
+        }
+    }
+
+    function onUpdate(socialSharing, config, bind, result) {
+        if (result.updateState) {
+            noteNewState(config, result.extra);
+        }
+        label.text(bind);
+        permalink.val(result.url);
+        setSocialSharing(socialSharing, result.url);
+    }
+
+    function getEmbeddedCacheLinkId() {
+        if ($('#sharelinkdialog input:checked').length === 0) return 'Embed';
+
+        return 'Embed|' + $('#sharelinkdialog input:checked').map(function () {
+            return $(this).prop('class');
+        })
+            .get()
+            .join();
+    }
+
+    function update() {
+        var socialSharing = socialSharingElements;
+        socialSharing.empty();
+        if (!currentBind) return;
+        permalink.prop('disabled', false);
+        var config = layout.toConfig();
+        var cacheLinkId = currentBind;
+        if (currentBind === 'Embed') {
+            cacheLinkId = getEmbeddedCacheLinkId();
+        }
+        if (!urls[cacheLinkId]) {
+            label.text(currentNode.text());
+            permalink.val('');
+            getLinks(config, currentBind, function (error, newUrl, extra, updateState) {
+                if (error || !newUrl) {
+                    permalink.prop('disabled', true);
+                    permalink.val(error || 'Error providing URL');
+                } else {
+                    urls[cacheLinkId] = {
+                        updateState: updateState,
+                        extra: extra,
+                        url: newUrl,
+                    };
+                    onUpdate(socialSharing, config, currentBind, urls[cacheLinkId]);
+                }
+            });
+        } else {
+            onUpdate(socialSharing, config, currentBind, urls[cacheLinkId]);
+        }
+    }
+
+    getLink.on('click', function () {
         ga.proxy('send', {
             hitType: 'event',
             eventCategory: 'OpenModalPane',
             eventAction: 'Sharing',
         });
-        var popoverElement = $($(this).data('bs.popover').tip);
-        var socialSharingElements = popoverElement.find('.socialsharing');
-        var root = $('.urls-container:visible');
-        var label = root.find('.current');
-        var permalink = $('.permalink');
-        var urls = {};
-        if (!currentNode) currentNode = $(root.find('.sources button')[0]);
-        if (!currentBind) currentBind = currentNode.data().bind;
 
-        function setCurrent(node) {
-            currentNode = node;
-            currentBind = node.data().bind;
-        }
-
-        function setSocialSharing(element, sharedUrl) {
-            if (options.sharingEnabled) {
-                updateShares(element, sharedUrl);
-                // Disable the links for every share item which does not support embed html as links
-                if (currentBind !== 'Full' && currentBind !== 'Short') {
-                    element.children('.share-no-embeddable')
-                        .addClass('share-disabled')
-                        .prop('title', 'Embed links are not supported in this service')
-                        .on('click', false);
-                }
-            }
-        }
-
-        function onUpdate(socialSharing, config, bind, result) {
-            if (result.updateState) {
-                noteNewState(config, result.extra);
-            }
-            label.text(bind);
-            permalink.val(result.url);
-            setSocialSharing(socialSharing, result.url);
-        }
-
-        function getEmbeddedCacheLinkId() {
-            if ($('#shareembedlink input:checked').length === 0) return 'Embed';
-
-            return 'Embed|' + $('#shareembedlink input:checked').map(function () {
-                return $(this).prop('class');
-            })
-                .get()
-                .join();
-        }
-
-        function update() {
-            var socialSharing = socialSharingElements;
-            socialSharing.empty();
-            if (!currentBind) return;
-            permalink.prop('disabled', false);
-            var config = layout.toConfig();
-            var cacheLinkId = currentBind;
-            if (currentBind === 'Embed') {
-                cacheLinkId = getEmbeddedCacheLinkId();
-            }
-            if (!urls[cacheLinkId]) {
-                label.text(currentNode.text());
-                permalink.val('');
-                getLinks(config, currentBind, function (error, newUrl, extra, updateState) {
-                    if (error || !newUrl) {
-                        permalink.prop('disabled', true);
-                        permalink.val(error || 'Error providing URL');
-                    } else {
-                        urls[cacheLinkId] = {
-                            updateState: updateState,
-                            extra: extra,
-                            url: newUrl,
-                        };
-                        onUpdate(socialSharing, config, currentBind, urls[cacheLinkId]);
-                    }
-                });
-            } else {
-                onUpdate(socialSharing, config, currentBind, urls[cacheLinkId]);
-            }
-        }
-
-        root.find('.sources button').on('click', function () {
-            setCurrent($(this));
-            update();
-        });
-
-        var embeddedButton = $('.shareembed');
-        embeddedButton.on('click', function () {
-            setCurrent(embeddedButton);
-            update();
-            getLink.popover('hide');
-        });
-
-        $('#embedsettings input').off('click').on('click', function () {
-            setCurrent(embeddedButton);
-            update();
-        });
-
+        setCurrent($(this));
         update();
-    }).prop('title', title);
-
-    // Dismiss the popover on escape.
-    $(document).on('keyup.editable', function (e) {
-        if (e.which === 27) {
-            getLink.popover('hide');
-        }
     });
 
-    // Dismiss on any click that isn't either in the opening element, inside
-    // the popover or on any alert
-    $(document).on('mouseup', function (e) {
-        var target = $(e.target);
-        if (!target.is(getLink) && getLink.has(target).length === 0 && target.closest('.popover').length === 0)
-            getLink.popover('hide');
-    });
-
-    // Opens the popup if asked to by the editor
-    layout.eventHub.on('displaySharingPopover', function () {
-        getLink.popover('show');
-    });
-
-    if (options.sharingEnabled) {
-        updateShares($('#socialshare'), baseUrl);
+    if (startingBind === 'Embed') {
+        embedsettings.find('input').on('click', function () {
+            setCurrent(getLink);
+            update();
+        });
     }
 }
 
@@ -322,7 +283,7 @@ function getLinks(config, currentBind, done) {
         default:
             if (currentBind.substr(0, 5) === 'Embed') {
                 var options = {};
-                $('#shareembedlink input:checked').each(function () {
+                $('#sharelinkdialog input:checked').each(function () {
                     options[$(this).prop('class')] = true;
                 });
                 done(null, getEmbeddedHtml(config, root, false, options), false);

views/index.pug
@@ -56,9 +56,23 @@ block prepend content
         if showSponsors
           li.nav-item.btn.btn-outline-primary#ces(data-toggle="modal") Sponsors
             include sponsor-icons.pug
-        li.nav-item
-          a.nav-link#share(href="javascript:;") Share
-            b.fas.fa-caret-down
+        li.nav-item.dropdown
+          a.nav-link.dropdown-toggle#share(href="javascript:;" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false") Share
+          div.dropdown-menu.dropdown-menu-right
+            if storageSolution !== "null"
+              a.dropdown-item#shareShort(href="javascript:;" data-target="#sharelinkdialog" data-toggle="modal" data-bind="Short")
+                span.dropdown-icon.fas.fa-cloud
+                | Short Link
+                | &nbsp;
+            a.dropdown-item#shareFull(href="javascript:;" data-target="#sharelinkdialog" data-toggle="modal" data-bind="Full")
+              span.dropdown-icon.fas.fa-store-slash
+              | Full Link
+              | &nbsp;
+            a.dropdown-item#shareEmbed(href="javascript:;" data-target="#sharelinkdialog" data-toggle="modal" data-bind="Embed")
+              span.dropdown-icon.fas.fa-window-restore
+              | Embed in iframe
+              | &nbsp;
+
         li.nav-item.dropdown
           a.nav-link.dropdown-toggle#otherDropdown(href="javascript:;" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false") Other
           div.dropdown-menu.dropdown-menu-right(aria-labelledby="otherDropdown")

views/popups.pug
@@ -223,30 +223,34 @@
 
 #notifications
 
-#shareembedlink.modal.fade.gl_keep(tabindex="-1" role="dialog")
+#sharelinkdialog.modal.fade.gl_keep(tabindex="-1" role="dialog")
   .modal-dialog.modal-lg
     .modal-content
       .modal-header
-        h5.modal-title Share embedded
+        h5.modal-title Share
         button.close(type="button" data-dismiss="modal" aria-hidden="true" aria-label="Close")
           span(aria-hidden="true")
             | &times;
       .modal-body
         .card
           .card-body
-            .input-group-append
-              input.form-control.input-sm.permalink(type="text" placeholder="Loading" readonly size="1024" style="font-size: 10px")
-              button.input-group-btn.btn.btn-outline-secondary.btn-sm.clippy(type="button" data-clipboard-target="#shareembedlink .permalink" title="Copy to clipboard")
-                span.fa.fa-clipboard
-            #embedsettings.form-group(role="group")
-              .checkbox
-                label
-                  input.readOnly(type="checkbox")
-                  | Read Only
-              .checkbox
-                label
-                  input.hideEditorToolbars(type="checkbox")
-                  | Hide Editor Toolbars
+            .container
+              .input-group-append
+                input.form-control.input-sm.permalink(type="text" placeholder="Loading" readonly size="1024" style="font-size: 10px")
+                button.input-group-btn.btn.btn-outline-secondary.btn-sm.clippy(type="button" data-clipboard-target="#sharelinkdialog .permalink" title="Copy to clipboard")
+                  span.fa.fa-clipboard
+              #embedsettings.form-group(role="group")
+                .checkbox
+                  label
+                    input.readOnly(type="checkbox")
+                    | Read Only
+                .checkbox
+                  label
+                    input.hideEditorToolbars(type="checkbox")
+                    | Hide Editor Toolbars
+              .spacer &nbsp;
+              if sharingEnabled
+                .socialsharing
 
 #history.modal.fade.gl_keep(tabindex="-1" role="dialog")
   .modal-dialog.modal-lg

views/templates.pug
@@ -1,30 +1,4 @@
 .gl_keep.template
-  .urls
-    .card
-      .card-body
-        .urls-container
-          .btn-group.btn-group-sm
-            .input-group.input-group-sm
-              .input-group-prepend
-                button.btn.btn-sm.btn-light.btn-outline-secondary.dropdown-toggle(data-toggle="dropdown" type="button" aria-haspopup="true" aria-expanded="false")
-                  span.current Short
-                ul.dropdown-menu.sources
-                  if storageSolution !== "null"
-                    button.dropdown-item.btn.btn-light(data-bind="Short")
-                      | Short
-                  button.dropdown-item.btn.btn-light(data-bind="Full")
-                    | Full
-              input.form-control.permalink(type="text" placeholder="Loading" readonly size="1024")
-              .input-group-append
-                button.btn.btn-sm.btn-light.btn-outline-secondary.clippy(type="button" data-clipboard-target=".popover .permalink" title="Copy to clipboard")
-                  span.fa.fa-clipboard
-        div
-          | &nbsp;
-        button.btn.btn-sm.btn-light.btn-outline-secondary.shareembed(data-target="#shareembedlink" data-toggle="modal" data-bind="Embed")
-          | Embedded
-      if sharingEnabled
-        .card-footer
-          .socialsharing
 
   #codeEditor
     .top-bar.btn-toolbar.bg-light(role="toolbar" aria-label="Editor toolbar" name="editor-btn-toolbar")

commit 9f1373be08cf93bfc639e3e9636cc392020f4dd7

Reenables social shares for whole site
Fixes a bug introduced in #2510
(In which I totally deleted working code by mistake...)

static/explorer.scss
@@ -520,10 +520,6 @@ kbd {
     margin-right: 6px;
 }
 
-#socialshare {
-    margin-left: -3px;
-}
-
 .linked-compiler-output-line {
     cursor: pointer;
 }

static/sharing.ts
@@ -206,6 +206,10 @@ export class Sharing {
             shareFullCopyToClipBtn.hide();
             shareEmbedCopyToClipBtn.hide();
         }
+
+        if (options.sharingEnabled) {
+            Sharing.updateShares($('#socialshare'), window.location.protocol + '//' + window.location.hostname);
+        }
     }
 
     private onClipButtonPressed(event: ClickEvent, type: LinkType): void {