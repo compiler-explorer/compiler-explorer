448494365951f85153c0f56390f6173d1defbe60
Robert Andrzejuk <robert-andrzejuk@users.noreply.github.com>
2021-09-10 12:41:53 +0200

    Update styles available in clang-format (#2926)
    * Update styles availabe in clang-format
    
    * Update compiler-explorer.amazon.properties

--- static/settings.js
+++ static/settings.js
@@ -231,7 +231,7 @@ function setupSettings(root, settings, onChange, subLangId) {
 
     add(root.find('.newEditorLastLang'), 'newEditorLastLang', true, Checkbox);
 
-    var formats = ['Google', 'LLVM', 'Mozilla', 'Chromium', 'WebKit'];
+    var formats = ['Google', 'LLVM', 'Mozilla', 'Chromium', 'WebKit', 'Microsoft', 'GNU'];
     add(root.find('.formatBase'), 'formatBase', formats[0], Select,
         _.map(formats, function (format) {
             return {label: format, desc: format};
