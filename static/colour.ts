70726a9d442b4b3f95024c2317776dedbeac5873
MarkusJx <28785953+markusjx@users.noreply.github.com>
2021-10-03 19:36:00 +0200

    Remove unnecessary 'use strict'
    Signed-off-by: Markus <28785953+MarkusJx@users.noreply.github.com>

--- static/colour.ts
+++ static/colour.ts
@@ -22,8 +22,6 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-'use strict';
-
 import _ from 'underscore';
 import * as monaco from 'monaco-editor';
 
