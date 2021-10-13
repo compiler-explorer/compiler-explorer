fbdfa628236eb90727c7c56c37352f96f4c5e6a2
Mats Larsen <me@supergrecko.com>
2021-10-13 22:47:32 +0200

    Create helper utility for creating non-deep-cloning copies of modes

---
+++ static/modes/modes.ts
@@ -0,0 +1,43 @@
+// Copyright (c) 2021, Compiler Explorer Authors
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+import _ from 'underscore';
+import { LanguageDefinitionProducer } from './modes.interfaces';
+
+type LanguageDefinition<T> = ReturnType<LanguageDefinitionProducer<T>>;
+
+/**
+ * Helper function for creating a language mode which extends a different
+ * language mode.
+ *
+ * This is a higher order function returning a lambda which will create create
+ * the new mode.
+ *
+ * Warning: this function does not do deep copies, so do not modify properties
+ * on the base parameter in the callback, do local copies there.
+ */
+export const createLanguageMode = <S, T>(
+    base: LanguageDefinitionProducer<T>,
+    producer: (base: LanguageDefinition<T>) => LanguageDefinition<S>
+): LanguageDefinitionProducer<T & S> => () => _.extend({}, base, producer(base()));
