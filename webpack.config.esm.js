5bfdc47d933cd8c84faaca6a77e351b649a75bad
Masoud Shokohi <mass.shock57@gmail.com>
2021-12-04 23:07:03 +0330

    Update and rename webpack.config.esm.js to webpack.config.esm.js

--- webpack.config.esm.js
+++ webpack.config.esm.js
@@ -1,5 +1,5 @@
 // Copyright (c) 2020, Compiler Explorer Authors
-// All rights reserved.
+// All rights reserved. 
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
