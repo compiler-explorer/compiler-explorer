01ee97e503ccfcf1cfd3db484896d1a335d07f3b
partouf <partouf@gmail.com>
2022-01-08 23:11:00 +0000

    i should be fired

--- static/global.ts
+++ static/global.ts
@@ -22,7 +22,7 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-import { IFrontendTesting } from './frontend-testing.interfaces';
+import { IFrontendTesting } from './tests/frontend-testing.interfaces';
 import { Options } from './options.interfaces';
 
 type CompilerExplorerOptions = Record<string, unknown> & Options
