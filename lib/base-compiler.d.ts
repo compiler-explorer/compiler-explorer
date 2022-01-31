f5f61003cded2325d4802d4a6bf63c1b7daa47d0
partouf <partouf@gmail.com>
2022-01-31 22:17:13 +0100

    lintfixes

--- lib/base-compiler.d.ts
+++ lib/base-compiler.d.ts
@@ -1,13 +1,12 @@
-
 export declare class BaseCompiler {
-    constructor(compilerInfo: any, env: any);
-    public compiler: any;
-    public lang: any;
+    constructor(compilerInfo, env);
+    public compiler;
+    public lang;
     public outputFilebase: string;
     public compilerProps: (key: string) => string;
     public getOutputFilename(path: string, filename: string): string;
-    public exec(filepath: string, args: string[], execOptions: any): any;
-    public parseCompilationOutput(result: any, filename: string);
-    public getDefaultExecOptions(): any;
-    public runCompiler(compiler: string, args: string[], filename: string, execOptions: any);
+    public exec(filepath: string, args: string[], execOptions);
+    public parseCompilationOutput(result, filename: string);
+    public getDefaultExecOptions();
+    public runCompiler(compiler: string, args: string[], filename: string, execOptions);
 }
