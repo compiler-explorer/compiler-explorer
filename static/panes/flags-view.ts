9c289f1a64de277f5e5b348097dd15c0002e0f5b
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2022-04-01 12:10:45 -0400

    Updated copyright

--- static/panes/flags-view.ts
+++ static/panes/flags-view.ts
@@ -1,4 +1,4 @@
-// Copyright (c) 2021, Tom Ritter
+// Copyright (c) 2022, Tom Ritter and Compiler Explorer Authors
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
