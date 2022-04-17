debf276b893be0800e05c677d4623666c5dbf350
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2022-04-17 16:31:09 -0400

    correct null assertion

--- static/formatter-registry.ts
+++ static/formatter-registry.ts
@@ -45,7 +45,7 @@ const doFormatRequest = async (options: FormattingRequest) => {
     const body = await res.json();
     if (res.status === 200 && body.exit === 0) {
         // API sent 200 and we have a valid response
-        return body.answer!;
+        return body.answer as string;
     }
     // We had an error (either HTTP request error, or API error)
     // Figure out which it is, show it to the user, and reject the promise
