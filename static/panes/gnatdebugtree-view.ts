bac8a451dbf127e49d66c1a87224bbf39c72a228
Mats Larsen <me@supergrecko.com>
2022-04-24 13:56:42 +0200

    Fix formatting

--- static/panes/gnatdebugtree-view.ts
+++ static/panes/gnatdebugtree-view.ts
@@ -24,15 +24,15 @@
 
 import _ from 'underscore';
 import * as monaco from 'monaco-editor';
-import { Container } from 'golden-layout';
+import {Container} from 'golden-layout';
 
-import { MonacoPane } from './pane';
-import { GnatDebugTreeState } from './gnatdebugtree-view.interfaces';
-import { MonacoPaneState } from './pane.interfaces';
+import {MonacoPane} from './pane';
+import {GnatDebugTreeState} from './gnatdebugtree-view.interfaces';
+import {MonacoPaneState} from './pane.interfaces';
 
-import { ga } from '../analytics';
-import { extendConfig } from '../monaco-config';
-import { Hub } from '../hub';
+import {ga} from '../analytics';
+import {extendConfig} from '../monaco-config';
+import {Hub} from '../hub';
 
 export class GnatDebugTree extends MonacoPane<monaco.editor.IStandaloneCodeEditor, GnatDebugTreeState> {
     constructor(hub: Hub, container: Container, state: GnatDebugTreeState & MonacoPaneState) {
@@ -47,12 +47,15 @@ export class GnatDebugTree extends MonacoPane<monaco.editor.IStandaloneCodeEdito
     }
 
     override createEditor(editorRoot: HTMLElement): monaco.editor.IStandaloneCodeEditor {
-        return monaco.editor.create(editorRoot, extendConfig({
-            language: 'plainText',
-            readOnly: true,
-            glyphMargin: true,
-            lineNumbersMinChars: 3,
-        }));
+        return monaco.editor.create(
+            editorRoot,
+            extendConfig({
+                language: 'plainText',
+                readOnly: true,
+                glyphMargin: true,
+                lineNumbersMinChars: 3,
+            })
+        );
     }
 
     override registerOpeningAnalyticsEvent(): void {
@@ -68,8 +71,8 @@ export class GnatDebugTree extends MonacoPane<monaco.editor.IStandaloneCodeEdito
     }
 
     override registerCallbacks(): void {
-        const throttleFunction = _.throttle((event) => this.onDidChangeCursorSelection(event), 500);
-        this.editor.onDidChangeCursorSelection((event) => throttleFunction(event));
+        const throttleFunction = _.throttle(event => this.onDidChangeCursorSelection(event), 500);
+        this.editor.onDidChangeCursorSelection(event => throttleFunction(event));
         this.eventHub.emit('gnatDebugTreeViewOpened', this.compilerInfo.compilerId);
         this.eventHub.emit('requestSettings');
     }
@@ -83,8 +86,7 @@ export class GnatDebugTree extends MonacoPane<monaco.editor.IStandaloneCodeEdito
         }
     }
 
-    override onCompiler(compilerId: number, compiler: any, options: any, editorId?: number,
-        treeId?: number): void {
+    override onCompiler(compilerId: number, compiler: any, options: any, editorId?: number, treeId?: number): void {
         if (this.compilerInfo.compilerId === compilerId) {
             this.compilerInfo.compilerName = compiler ? compiler.name : '';
             this.compilerInfo.editorId = editorId;
@@ -97,15 +99,14 @@ export class GnatDebugTree extends MonacoPane<monaco.editor.IStandaloneCodeEdito
     }
 
     showGnatDebugTreeResults(result: any[]): void {
-        this.editor.getModel()?.setValue(result.length
-            ? _.pluck(result, 'text').join('\n')
-            : '<No GNAT Debug Tree generated>');
+        this.editor
+            .getModel()
+            ?.setValue(result.length ? _.pluck(result, 'text').join('\n') : '<No GNAT Debug Tree generated>');
 
         if (!this.isAwaitingInitialResults) {
             if (this.selection) {
                 this.editor.setSelection(this.selection);
-                this.editor.revealLinesInCenter(this.selection.selectionStartLineNumber,
-                    this.selection.endLineNumber);
+                this.editor.revealLinesInCenter(this.selection.selectionStartLineNumber, this.selection.endLineNumber);
             }
             this.isAwaitingInitialResults = true;
         }
--- static/panes/rustmir-view.ts
+++ static/panes/rustmir-view.ts
@@ -24,15 +24,15 @@
 
 import _ from 'underscore';
 import * as monaco from 'monaco-editor';
-import { Container } from 'golden-layout';
+import {Container} from 'golden-layout';
 
-import { MonacoPane } from './pane';
-import { MonacoPaneState } from './pane.interfaces';
-import { RustMirState } from './rustmir-view.interfaces';
+import {MonacoPane} from './pane';
+import {MonacoPaneState} from './pane.interfaces';
+import {RustMirState} from './rustmir-view.interfaces';
 
-import { ga } from '../analytics';
-import { extendConfig } from '../monaco-config';
-import { Hub } from '../hub';
+import {ga} from '../analytics';
+import {extendConfig} from '../monaco-config';
+import {Hub} from '../hub';
 
 export class RustMir extends MonacoPane<monaco.editor.IStandaloneCodeEditor, RustMirState> {
     constructor(hub: Hub, container: Container, state: RustMirState & MonacoPaneState) {
@@ -47,12 +47,15 @@ export class RustMir extends MonacoPane<monaco.editor.IStandaloneCodeEditor, Rus
     }
 
     override createEditor(editorRoot: HTMLElement): monaco.editor.IStandaloneCodeEditor {
-        return monaco.editor.create(editorRoot, extendConfig({
-            language: 'rust',
-            readOnly: true,
-            glyphMargin: true,
-            lineNumbersMinChars: 3,
-        }));
+        return monaco.editor.create(
+            editorRoot,
+            extendConfig({
+                language: 'rust',
+                readOnly: true,
+                glyphMargin: true,
+                lineNumbersMinChars: 3,
+            })
+        );
     }
 
     override registerOpeningAnalyticsEvent(): void {
@@ -68,8 +71,8 @@ export class RustMir extends MonacoPane<monaco.editor.IStandaloneCodeEditor, Rus
     }
 
     override registerCallbacks(): void {
-        const throttleFunction = _.throttle((event) => this.onDidChangeCursorSelection(event), 500);
-        this.editor.onDidChangeCursorSelection((event) => throttleFunction(event));
+        const throttleFunction = _.throttle(event => this.onDidChangeCursorSelection(event), 500);
+        this.editor.onDidChangeCursorSelection(event => throttleFunction(event));
         this.eventHub.emit('rustMirViewOpened', this.compilerInfo.compilerId);
         this.eventHub.emit('requestSettings');
     }
@@ -83,8 +86,7 @@ export class RustMir extends MonacoPane<monaco.editor.IStandaloneCodeEditor, Rus
         }
     }
 
-    override onCompiler(compilerId: number, compiler: any, options: any, editorId?: number,
-        treeId?: number): void {
+    override onCompiler(compilerId: number, compiler: any, options: any, editorId?: number, treeId?: number): void {
         if (this.compilerInfo.compilerId === compilerId) {
             this.compilerInfo.compilerName = compiler ? compiler.name : '';
             this.compilerInfo.editorId = editorId;
@@ -97,15 +99,14 @@ export class RustMir extends MonacoPane<monaco.editor.IStandaloneCodeEditor, Rus
     }
 
     showRustMirResults(result: any[]): void {
-        this.editor.getModel()?.setValue(result.length
-            ? _.pluck(result, 'text').join('\n')
-            : '<No Rust MIR generated>');
+        this.editor
+            .getModel()
+            ?.setValue(result.length ? _.pluck(result, 'text').join('\n') : '<No Rust MIR generated>');
 
         if (!this.isAwaitingInitialResults) {
             if (this.selection) {
                 this.editor.setSelection(this.selection);
-                this.editor.revealLinesInCenter(this.selection.selectionStartLineNumber,
-                    this.selection.endLineNumber);
+                this.editor.revealLinesInCenter(this.selection.selectionStartLineNumber, this.selection.endLineNumber);
             }
             this.isAwaitingInitialResults = true;
         }
