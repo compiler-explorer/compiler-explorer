4f543df58cbf7c798b9698de4e87e8e655f0c4c0
Matt Godbolt <matt@godbolt.org>
2022-05-10 17:36:23 -0500

    Add clarifying comment, and fix exposed difference :)

--- etc/scripts/parsed_pug_file.js
+++ etc/scripts/parsed_pug_file.js
@@ -6,10 +6,11 @@ import path from 'path';
 // If you edit either cookies.pug or privacy.pug be aware this will trigger a popup on the users' next visit.
 // Knowing the last versions here helps us be aware when this happens. If you get an error here and you _haven't_
 // knowingly edited either policy, contact the CE team. If you have edited the cookies and know that this expected,
-// just update the hash here.
+// just update the hash here. Note that you will need to check in the changes to the policy document first, then
+// run `make webpack`, and then paste in those values, as the timestamp of the checkin is part of the hash.
 const expectedHashes = {
-    cookies: '90cd74e38dd0f12e',
-    privacy: 'bdf83664aafb58cb',
+    cookies: '7581921381afeed5',
+    privacy: 'a0540b2506625656',
 };
 
 function execGit(command) {
