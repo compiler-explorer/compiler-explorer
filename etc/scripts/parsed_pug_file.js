3796bbec695eeb423705c4dbd2cea833ce494387
Matt Godbolt <matt@godbolt.org>
2022-05-11 11:30:19 -0500

    Revert "Fix policy links (#3644)"
    This reverts commit b66520ce6f290e95f9982dbd9053223c145ee4b6.
    
    Seems to be not deterministic: https://github.com/compiler-explorer/compiler-explorer/runs/6390821301?check_suite_focus=true

--- etc/scripts/parsed_pug_file.js
+++ etc/scripts/parsed_pug_file.js
@@ -1,17 +1,6 @@
 import {execSync} from 'child_process';
 import {getHashDigest} from 'loader-utils';
 import * as pug from 'pug';
-import path from 'path';
-
-// If you edit either cookies.pug or privacy.pug be aware this will trigger a popup on the users' next visit.
-// Knowing the last versions here helps us be aware when this happens. If you get an error here and you _haven't_
-// knowingly edited either policy, contact the CE team. If you have edited the cookies and know that this expected,
-// just update the hash here. Note that you will need to check in the changes to the policy document first, then
-// run `make webpack`, and then paste in those values, as the timestamp of the checkin is part of the hash.
-const expectedHashes = {
-    cookies: '7581921381afeed5',
-    privacy: 'a0540b2506625656',
-};
 
 function execGit(command) {
     const gitResult = execSync(command);
@@ -28,24 +17,13 @@ const gitChanges = execGit('git log --date=local --after="3 months ago" "--grep=
     .map(match => match.groups);
 
 export default function(content) {
-    const filePath = this.resourcePath;
-    const filename = path.basename(filePath, '.pug');
-    const lastTime = execGit(`git log -1 --format=%cd "${filePath}"`).trimEnd();
-    const lastCommit = execGit(`git log -1 --format=%h "${filePath}"`).trimEnd();
-    const compiled = pug.compile(content.toString(), {filename: filePath});
+    const filename = this.resourcePath;
+    const lastTime = execGit(`git log -1 --format=%cd "${filename}"`).trimEnd();
+    const lastCommit = execGit(`git log -1 --format=%h "${filename}"`).trimEnd();
+    const compiled = pug.compile(content.toString(), {filename});
     const source = compiled({gitChanges, lastTime, lastCommit});
-    const hashDigest = getHashDigest(source, 'sha256', 'hex', 16);
-    const expectedHash = expectedHashes[filename];
-    if (expectedHash !== undefined && expectedHash !== hashDigest) {
-        this.emitError(
-            new Error(
-                `Hash for file '${filePath}' changed from '${expectedHash}' to '${hashDigest}'` +
-                    ` - if expected, update the definition in parsed_pug.js`,
-            ),
-        );
-    }
     const result = {
-        hash: hashDigest,
+        hash: getHashDigest(source, 'sha256', 'hex', 16),
         text: source.toString(),
     };
     return `export default ${JSON.stringify(result)};`;
