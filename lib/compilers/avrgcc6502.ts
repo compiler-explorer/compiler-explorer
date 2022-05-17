a84f8f3bafb654438fddbc50c1a8e2e1c8bd9c2c
partouf <partouf@gmail.com>
2022-05-17 02:26:31 +0200

    add new x86-to-6502 compiler using avr-gcc, xa and avrlibstdcpp

---
+++ lib/compilers/avrgcc6502.ts
@@ -0,0 +1,77 @@
+// Copyright (c) 2022, Compiler Explorer Authors
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+import path from 'path';
+
+import {BaseCompiler} from '../base-compiler';
+
+export class AvrGcc6502Compiler extends BaseCompiler {
+    private readonly avrgccpath: string;
+    private readonly xapath: string;
+    private readonly avrlibstdcpppath: string;
+
+    static get key() {
+        return 'avrgcc6502';
+    }
+
+    constructor(compilerInfo, env) {
+        super(compilerInfo, env);
+
+        this.avrgccpath = this.compilerProps(`compiler.${this.compiler.id}.avrgccpath`);
+        this.xapath = this.compilerProps(`compiler.${this.compiler.id}.xapath`);
+        this.avrlibstdcpppath = this.compilerProps(`compiler.${this.compiler.id}.avrlibstdcpppath`);
+        this.outputFilebase = 'example';
+    }
+
+    public override getOutputFilename(dirPath, outputFilebase, key) {
+        let filename;
+        if (key && key.backendOptions && key.backendOptions.customOutputFilename) {
+            filename = key.backendOptions.customOutputFilename;
+        } else {
+            filename = `${outputFilebase}.6502.asm`;
+        }
+
+        if (dirPath) {
+            return path.join(dirPath, filename);
+        } else {
+            return filename;
+        }
+    }
+
+    protected override optionsForFilter(filters: object, outputFilename: string): string[] {
+        return [`-I${this.avrlibstdcpppath}`];
+    }
+
+    public override async runCompiler(compiler, options, inputFilename, execOptions) {
+        if (!execOptions) {
+            execOptions = this.getDefaultExecOptions();
+        }
+
+        execOptions.env = {
+            PATH: path.join(this.avrgccpath, 'bin') + path.delimiter + this.xapath,
+        };
+
+        return await super.runCompiler(compiler, options, inputFilename, execOptions);
+    }
+}
