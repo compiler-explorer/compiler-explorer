3f81bb67227830621b06d28cb8a10ade6ffafaf8
partouf <partouf@gmail.com>
2022-06-06 15:11:21 +0200

    props should be optional

--- types/compilation/compilation.interfaces.ts
+++ types/compilation/compilation.interfaces.ts
@@ -27,8 +27,8 @@ import {ResultLine} from '../resultline/resultline.interfaces';
 
 export type CompilationResult = {
     code: number;
-    buildResult: unknown;
-    inputFilename: string;
+    buildResult?: unknown;
+    inputFilename?: string;
     asm?: ResultLine[];
     stdout: ResultLine[];
     stderr: ResultLine[];
