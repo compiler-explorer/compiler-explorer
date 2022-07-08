65989618c537757aa1ba6765ac286c5fa2cc969c
Ross Smyth <crsmyth@mtu.edu>
2022-07-08 03:54:49 -0400

    Look more in to why LLVM IR won't display and clean up the directory modification

--- lib/compilers/pony.ts
+++ lib/compilers/pony.ts
@@ -3,20 +3,20 @@ import path from 'path';
 import {ParseFilters} from '../../types/features/filters.interfaces';
 import {BaseCompiler} from '../base-compiler';
 import {CompilationResult, ExecutionOptions} from '../../types/compilation/compilation.interfaces';
+import {logger} from '../logger';
+import _ from 'underscore';
 
 export class PonyCompiler extends BaseCompiler {
     static get key() {
         return 'pony';
     }
 
-    /* 
-  constructor(info: any, env: any) {
-    super(info, env);
+    constructor(info: any, env: any) {
+        super(info, env);
 
-    this.compiler.supportsIrView = true;
-    this.compiler.irArg = ['--pass', 'ir'];
-  } 
-  */
+        this.compiler.supportsIrView = true;
+        this.compiler.irArg = ['--pass', 'ir'];
+    }
 
     override optionsForFilter(filters: ParseFilters, outputFilename: any, userOptions?: any): string[] {
         let options = ['-d', '-b', path.parse(outputFilename).name];
@@ -37,6 +37,21 @@ export class PonyCompiler extends BaseCompiler {
         return source;
     }
 
+    override async generateIR(inputFilename: any, options: any[], filters: ParseFilters) {
+        const newOptions = _.filter(options, option => !['--pass', 'asm'].includes(option)).concat(this.compiler.irArg);
+
+        const execOptions = this.getDefaultExecOptions();
+        // A higher max output is needed for when the user includes headers
+        execOptions.maxOutput = 1024 * 1024 * 1024;
+
+        const output = await this.runCompiler(this.compiler.exe, newOptions, this.filename(inputFilename), execOptions);
+        if (output.code !== 0) {
+            return [{text: 'Failed to run compiler to get IR code'}];
+        }
+        const ir = await this.processIrOutput(output, filters);
+        return ir.asm;
+    }
+
     override async runCompiler(
         compiler: string,
         options: string[],
@@ -53,8 +68,7 @@ export class PonyCompiler extends BaseCompiler {
 
         // Pony operates upon the directory as a whole, not files it seems
         // So we must set the input to the directory rather than a file.
-        const input_i = options.indexOf(inputFilename);
-        options[input_i] = path.dirname(options[input_i]);
+        options = _.map(options, arg => (arg.includes(inputFilename) ? path.dirname(arg) : arg));
 
         const result = await this.exec(compiler, options, execOptions);
         result.inputFilename = inputFilename;
