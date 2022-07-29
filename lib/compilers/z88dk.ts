249ac495393cda49ec786e2ab671ce926fdfc0fa
partouf <partouf@gmail.com>
2022-07-29 03:40:10 +0200

    fixes to compilation and disassembly

--- lib/compilers/z88dk.ts
+++ lib/compilers/z88dk.ts
@@ -25,20 +25,71 @@
 import path from 'path';
 
 import {ExecutionOptions} from '../../types/compilation/compilation.interfaces';
+import {ParseFilters} from '../../types/features/filters.interfaces';
 import {BaseCompiler} from '../base-compiler';
+import {AsmParserZ88dk} from '../parsers/asm-parser-z88dk';
 
 export class z88dkCompiler extends BaseCompiler {
     static get key() {
         return 'z88dk';
     }
 
-    protected override optionsForFilter(filters: object, outputFilename: string): string[] {
-        return [];
+    constructor(compilerInfo, env) {
+        super(compilerInfo, env);
+        this.outputFilebase = 'example';
+        this.asm = new AsmParserZ88dk(this.compilerProps);
+    }
+
+    public override getOutputFilename(dirPath: string, outputFilebase: string, key?: any): string {
+        let filename;
+        if (key && key.backendOptions && key.backendOptions.customOutputFilename) {
+            filename = key.backendOptions.customOutputFilename;
+        } else if (key && key.filters.binary) {
+            filename = `${outputFilebase}.s`;
+        } else {
+            filename = `${outputFilebase}.c.asm`;
+        }
+
+        if (dirPath) {
+            return path.join(dirPath, filename);
+        } else {
+            return filename;
+        }
+    }
+
+    public override orderArguments(
+        options,
+        inputFilename,
+        libIncludes,
+        libOptions,
+        libPaths,
+        libLinks,
+        userOptions,
+        staticLibLinks,
+    ) {
+        return userOptions.concat(
+            options,
+            [this.filename(inputFilename)],
+            libIncludes,
+            libOptions,
+            libPaths,
+            libLinks,
+            staticLibLinks,
+        );
+    }
+
+    protected override optionsForFilter(filters: ParseFilters, outputFilename: string): string[] {
+        if (!filters.binary) {
+            return ['-S'];
+        } else {
+            return ['-o', outputFilename];
+        }
     }
 
     override getDefaultExecOptions(): ExecutionOptions {
         const opts = super.getDefaultExecOptions();
         opts.env.ZCCCFG = path.join(path.dirname(this.compiler.exe), '../share/z88dk/lib/config');
+        opts.env.PATH = process.env.PATH + path.delimiter + path.dirname(this.compiler.exe);
 
         return opts;
     }
