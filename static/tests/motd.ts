45dc44093c1a1ca71cc53c7a9591f9828ead32ad
RabsRincon <ruben@rinconblanco.es>
2022-08-13 00:09:18 +0200

    Change motd tests to use +00:00 instead of Z
    That's how python will encode them, so use that

--- static/tests/motd.ts
+++ static/tests/motd.ts
@@ -94,11 +94,11 @@ class MotdTests implements ITestable {
         );
 
         MotdTests.assertAdWithDateNow(
-            Date.parse('2022-01-08T00:00:00Z'),
+            Date.parse('2022-01-08T00:00:00+00:00'),
             {
                 filter: [],
                 html: '',
-                valid_from: '2022-01-01T00:00:00Z',
+                valid_from: '2022-01-01T00:00:00+00:00',
             },
             'langForTest',
             true,
@@ -106,11 +106,11 @@ class MotdTests implements ITestable {
         );
 
         MotdTests.assertAdWithDateNow(
-            Date.parse('2022-01-08T00:00:00Z'),
+            Date.parse('2022-01-08T00:00:00+00:00'),
             {
                 filter: [],
                 html: '',
-                valid_from: '2022-01-16T00:00:00Z',
+                valid_from: '2022-01-16T00:00:00+00:00',
             },
             'langForTest',
             false,
@@ -118,11 +118,11 @@ class MotdTests implements ITestable {
         );
 
         MotdTests.assertAdWithDateNow(
-            Date.parse('2022-01-08T00:00:00Z'),
+            Date.parse('2022-01-08T00:00:00+00:00'),
             {
                 filter: [],
                 html: '',
-                valid_until: '2022-01-16T00:00:00Z',
+                valid_until: '2022-01-16T00:00:00+00:00',
             },
             'langForTest',
             true,
@@ -130,11 +130,11 @@ class MotdTests implements ITestable {
         );
 
         MotdTests.assertAdWithDateNow(
-            Date.parse('2022-01-08T00:00:00Z'),
+            Date.parse('2022-01-08T00:00:00+00:00'),
             {
                 filter: [],
                 html: '',
-                valid_from: '2022-01-16T00:00:00Z',
+                valid_from: '2022-01-16T00:00:00+00:00',
             },
             'langForTest',
             false,
@@ -142,12 +142,12 @@ class MotdTests implements ITestable {
         );
 
         MotdTests.assertAdWithDateNow(
-            Date.parse('2022-01-08T00:00:00Z'),
+            Date.parse('2022-01-08T00:00:00+00:00'),
             {
                 filter: [],
                 html: '',
-                valid_from: '2022-01-01T00:00:00Z',
-                valid_until: '2022-01-16T00:00:00Z',
+                valid_from: '2022-01-01T00:00:00+00:00',
+                valid_until: '2022-01-16T00:00:00+00:00',
             },
             'langForTest',
             true,
@@ -155,12 +155,12 @@ class MotdTests implements ITestable {
         );
 
         MotdTests.assertAdWithDateNow(
-            Date.parse('2022-01-08T00:00:00Z'),
+            Date.parse('2022-01-08T00:00:00+00:00'),
             {
                 filter: [],
                 html: '',
-                valid_from: '2022-01-10T00:00:00Z',
-                valid_until: '2022-01-16T00:00:00Z',
+                valid_from: '2022-01-10T00:00:00+00:00',
+                valid_until: '2022-01-16T00:00:00+00:00',
             },
             'langForTest',
             false,
@@ -168,12 +168,12 @@ class MotdTests implements ITestable {
         );
 
         MotdTests.assertAdWithDateNow(
-            Date.parse('2022-01-20T00:00:00Z'),
+            Date.parse('2022-01-20T00:00:00+00:00'),
             {
                 filter: [],
                 html: '',
-                valid_from: '2022-01-10T00:00:00Z',
-                valid_until: '2022-01-16T00:00:00Z',
+                valid_from: '2022-01-10T00:00:00+00:00',
+                valid_until: '2022-01-16T00:00:00+00:00',
             },
             'langForTest',
             false,
@@ -181,12 +181,12 @@ class MotdTests implements ITestable {
         );
 
         MotdTests.assertAdWithDateNow(
-            Date.parse('2022-01-08T00:00:00Z'),
+            Date.parse('2022-01-08T00:00:00+00:00'),
             {
                 filter: [],
                 html: '',
-                valid_from: '2022-01-16T00:00:00Z',
-                valid_until: '2022-01-10T00:00:00Z',
+                valid_from: '2022-01-16T00:00:00+00:00',
+                valid_until: '2022-01-10T00:00:00+00:00',
             },
             'langForTest',
             false,
@@ -194,12 +194,12 @@ class MotdTests implements ITestable {
         );
 
         MotdTests.assertAdWithDateNow(
-            Date.parse('2022-01-08T00:00:00Z'),
+            Date.parse('2022-01-08T00:00:00+00:00'),
             {
                 filter: ['fakeLang'],
                 html: '',
-                valid_from: '2022-01-01T00:00:00Z',
-                valid_until: '2022-01-16T00:00:00Z',
+                valid_from: '2022-01-01T00:00:00+00:00',
+                valid_until: '2022-01-16T00:00:00+00:00',
             },
             'langForTest',
             false,
@@ -207,12 +207,12 @@ class MotdTests implements ITestable {
         );
 
         MotdTests.assertAdWithDateNow(
-            Date.parse('2022-01-08T00:00:00Z'),
+            Date.parse('2022-01-08T00:00:00+00:00'),
             {
                 filter: ['langForTest'],
                 html: '',
-                valid_from: '2022-01-01T00:00:00Z',
-                valid_until: '2022-01-16T00:00:00Z',
+                valid_from: '2022-01-01T00:00:00+00:00',
+                valid_until: '2022-01-16T00:00:00+00:00',
             },
             'langForTest',
             true,
@@ -220,12 +220,12 @@ class MotdTests implements ITestable {
         );
 
         MotdTests.assertAdWithDateNow(
-            Date.parse('2022-01-08T00:00:00Z'),
+            Date.parse('2022-01-08T00:00:00+00:00'),
             {
                 filter: ['langForTest'],
                 html: '',
-                valid_from: '2022-01-08T00:00:00Z',
-                valid_until: '2022-01-16T00:00:00Z',
+                valid_from: '2022-01-08T00:00:00+00:00',
+                valid_until: '2022-01-16T00:00:00+00:00',
             },
             'langForTest',
             true,
@@ -233,12 +233,12 @@ class MotdTests implements ITestable {
         );
 
         MotdTests.assertAdWithDateNow(
-            Date.parse('2022-01-08T00:00:00Z'),
+            Date.parse('2022-01-08T00:00:00+00:00'),
             {
                 filter: ['langForTest'],
                 html: '',
-                valid_from: '2022-01-01T00:00:00Z',
-                valid_until: '2022-01-18T00:00:00Z',
+                valid_from: '2022-01-01T00:00:00+00:00',
+                valid_until: '2022-01-18T00:00:00+00:00',
             },
             'langForTest',
             true,
