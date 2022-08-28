d46c416b3280ce6a1e820598ddc7fc3ad1181070
RabsRincon <ruben@rinconblanco.es>
2022-08-29 01:04:08 +0200

    Add compiler check for every prop & typo check

--- etc/scripts/util/propschecktest.py
+++ etc/scripts/util/propschecktest.py
@@ -1,75 +1,91 @@
+import sys
+import os
 import unittest
 
 from propscheck import process_file, Line
 
 
+def sline(number, text):
+    return str(Line(number, text))
+
+
 class PropsCheckTests(unittest.TestCase):
-    def run_test(self, file, expected_key, expected_contents):
-        result = process_file(file)
+    def run_test(self, filename, expected_key, expected_contents):
+        base_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+        test_case_file = os.path.join(base_path, 'test', 'cases', f"{filename}.properties")
+        result = process_file(test_case_file)
         self.assertEqual(result[expected_key], expected_contents)
 
     def test_bad_compilers(self):
-        self.run_test("./test/cases/bad_compilers.properties", "bad_compilers", {"b"})
+        self.run_test("bad_compilers", "bad_compilers", {"b"})
 
     def test_bad_compilers_alias(self):
-        self.run_test("./test/cases/bad_compilers_aliases.properties", "bad_compilers", {"c"})
+        self.run_test("bad_compilers_aliases", "bad_compilers", {"c"})
 
     def test_bad_compilers_disabled(self):
-        self.run_test("./test/cases/bad_compilers_disabled.properties", "bad_compilers", set())
+        self.run_test("bad_compilers_disabled", "bad_compilers", set())
 
     def test_bad_groups(self):
-        self.run_test("./test/cases/bad_groups.properties", "bad_groups", {"b"})
+        self.run_test("bad_groups", "bad_groups", {"b"})
 
     def test_bad_formatters(self):
-        self.run_test("./test/cases/bad_formatters.properties", "bad_formatters", {"b"})
+        self.run_test("bad_formatters", "bad_formatters", {"b"})
 
     def test_bad_libs_ids(self):
-        self.run_test("./test/cases/bad_libs_ids.properties", "bad_libs_ids", {"b"})
-        self.run_test("./test/cases/bad_libs_ids.properties", "bad_libs_versions", set())
+        self.run_test("bad_libs_ids", "bad_libs_ids", {"b"})
+        self.run_test("bad_libs_ids", "bad_libs_versions", set())
 
     def test_bad_libs_versions(self):
-        self.run_test("./test/cases/bad_libs_versions.properties", "bad_libs_versions", {"a a2"})
+        self.run_test("bad_libs_versions", "bad_libs_versions", {"a a2"})
 
     def test_bad_tools(self):
-        self.run_test("./test/cases/bad_tools.properties", "bad_tools", {"b"})
+        self.run_test("bad_tools", "bad_tools", {"b"})
 
     def test_bad_default(self):
-        self.run_test("./test/cases/bad_default.properties", "bad_default", {"b"})
+        self.run_test("bad_default", "bad_default", {"b"})
 
     def test_empty_separators(self):
-        self.run_test("./test/cases/empty_separators.properties", "empty_separators", {
-            str(Line(1, "compilers=a::b")),
-            str(Line(1, "compilers=a::b")),
-            str(Line(2, "compilers=::a:b")),
-            str(Line(3, "compilers=a:b::")),
-            str(Line(4, "compilers=::")),
-            str(Line(5, "compilers=::a")),
-            str(Line(6, "compilers=a::")),
-            str(Line(7, "compilers=:a")),
-            str(Line(8, "compilers=a:"))
+        self.run_test("empty_separators", "empty_separators", {
+            sline(1, "compilers=a::b"),
+            sline(1, "compilers=a::b"),
+            sline(2, "compilers=::a:b"),
+            sline(3, "compilers=a:b::"),
+            sline(4, "compilers=::"),
+            sline(5, "compilers=::a"),
+            sline(6, "compilers=a::"),
+            sline(7, "compilers=:a"),
+            sline(8, "compilers=a:")
         })
 
     def test_duplicate_lines(self):
-        self.run_test("./test/cases/duplicate_lines.properties", "duplicate_lines",
-                      {str(Line(5, "duplicated.prop=true"))})
+        self.run_test("duplicate_lines", "duplicate_lines",
+                      {sline(5, "duplicated.prop=true")})
 
     def test_duplicated_compiler(self):
-        self.run_test("./test/cases/bad_duplicated_compiler.properties", "duplicated_compiler_references",
+        self.run_test("bad_duplicated_compiler", "duplicated_compiler_references",
                       {"duplicatedname"})
 
     def test_duplicated_group(self):
-        self.run_test("./test/cases/bad_duplicated_group.properties", "duplicated_group_references",
+        self.run_test("bad_duplicated_group", "duplicated_group_references",
                       {"dupgroup"})
 
     def test_suspicious_path(self):
-        self.run_test("./test/cases/suspicious_path.properties", "suspicious_path",
+        self.run_test("suspicious_path", "suspicious_path",
                       {"/wrong/path/bin/gcc"})
 
     def test_good_file(self):
-        result = process_file('../../config/c++.amazon.properties')
+        base_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+        test_case_file = os.path.join(base_path, '..', '..', 'config', 'c++.amazon.properties')
+        result = process_file(test_case_file)
         for k in result:
             if k != "filename":
                 self.assertEqual(result[k], set(), f"{k} has output in known good file")
+
+    def test_bad_compiler_ids(self):
+        self.run_test("bad_compiler_ids", "bad_compiler_ids", {"bb"})
+
+    def test_typo_compilers(self):
+        self.run_test("typo_compilers", "typo_compilers", {sline(3, 'compilers.a.name=A')})
 
 
 if __name__ == '__main__':
