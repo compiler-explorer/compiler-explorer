e4909784105f64f9855f49477b93c56c70d3cf46
Markus <28785953+MarkusJx@users.noreply.github.com>
2022-10-12 17:07:04 +0200

    Convert device-view.js to typescript (#4105)

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -17,6 +17,6 @@
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
+    // `on` is used to hook into various events Cypress emits
+    // `config` is the resolved Cypress config
+};
