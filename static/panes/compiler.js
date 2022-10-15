cc8fd8fdd399ff3c9d79d2a66204e5f0887af883
Rubén Rincón Blanco <ruben@rinconblanco.es>
2022-10-15 17:38:41 +0200

    Properly allow for multiple opt pass panes (#4155)

--- static/panes/compiler.js
+++ static/panes/compiler.js
@@ -1672,7 +1672,6 @@ Compiler.prototype.onIrViewClosed = function (id) {
 
 Compiler.prototype.onLLVMOptPipelineViewOpened = function (id) {
     if (this.id === id) {
-        this.llvmOptPipelineButton.prop('disabled', true);
         this.llvmOptPipelineViewOpen = true;
         this.compile();
     }
@@ -1680,7 +1679,6 @@ Compiler.prototype.onLLVMOptPipelineViewOpened = function (id) {
 
 Compiler.prototype.onLLVMOptPipelineViewClosed = function (id) {
     if (this.id === id) {
-        this.llvmOptPipelineButton.prop('disabled', false);
         this.llvmOptPipelineViewOpen = false;
     }
 };
@@ -2237,7 +2235,8 @@ Compiler.prototype.updateButtons = function () {
     this.ppButton.prop('disabled', this.ppViewOpen);
     this.astButton.prop('disabled', this.astViewOpen);
     this.irButton.prop('disabled', this.irViewOpen);
-    this.llvmOptPipelineButton.prop('disabled', this.llvmOptPipelineViewOpen);
+    // As per #4112, it's useful to have this available more than once: Don't disable it when it opens
+    // this.llvmOptPipelineButton.prop('disabled', this.llvmOptPipelineViewOpen);
     this.deviceButton.prop('disabled', this.deviceViewOpen);
     this.rustMirButton.prop('disabled', this.rustMirViewOpen);
     this.haskellCoreButton.prop('disabled', this.haskellCoreViewOpen);
@@ -2256,8 +2255,8 @@ Compiler.prototype.updateButtons = function () {
     this.ppButton.toggle(!!this.compiler.supportsPpView);
     this.astButton.toggle(!!this.compiler.supportsAstView);
     this.irButton.toggle(!!this.compiler.supportsIrView);
-    // As per #4112, it's useful to have this available more than once: Don't disable it when it opens
-    //this.llvmOptPipelineButton.toggle(!!this.compiler.supportsLLVMOptPipelineView);
+
+    this.llvmOptPipelineButton.toggle(!!this.compiler.supportsLLVMOptPipelineView);
     this.deviceButton.toggle(!!this.compiler.supportsDeviceAsmView);
     this.rustMirButton.toggle(!!this.compiler.supportsRustMirView);
     this.rustMacroExpButton.toggle(!!this.compiler.supportsRustMacroExpView);
