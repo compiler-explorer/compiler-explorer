dfea3ca00cef503232f4881a4d5f726a912df7ca
Matt Godbolt <matt@godbolt.org>
2022-11-10 17:48:47 -0600

    Better phrasing

--- lib/sponsors.ts
+++ lib/sponsors.ts
@@ -59,41 +59,53 @@ function squareSumFromMean(values: number[]): number {
     return values.reduce((x, y) => x + (y - mean) * (y - mean), 0);
 }
 
-function sponsorIconSetsOk(sponsorAppearanceCount: Map<Sponsor, number>, totalAppearances: number): boolean {
-    const byEvery: Map<number, number[]> = new Map();
+function standardDeviation(values: number[]): number {
+    return values.length < 2 ? 0 : Math.sqrt(squareSumFromMean(values) / (values.length - 1));
+}
+
+// A sponsor icon set is ok if:
+// - each sponsor is shown at least every "topIconShowEvery"
+// - the standard deviation for the number of showings between sponsors at the same "show every' is not too high: that
+//   is we fairly distribute showings of sponsors at the same level
+function sponsorIconSetsOk(
+    sponsorAppearanceCount: Map<Sponsor, number>,
+    totalAppearances: number,
+    maxStandardDeviation: number,
+): boolean {
+    const countsByShowEvery: Map<number, number[]> = new Map();
     for (const [icon, count] of sponsorAppearanceCount.entries()) {
         const seenEvery = count > 0 ? totalAppearances / count : Infinity;
         if (seenEvery > icon.topIconShowEvery) {
             return false;
         }
-        const others = byEvery.get(icon.topIconShowEvery) || [];
+        const others = countsByShowEvery.get(icon.topIconShowEvery) || [];
         others.push(seenEvery);
-        byEvery.set(icon.topIconShowEvery, others);
+        countsByShowEvery.set(icon.topIconShowEvery, others);
     }
-    const maxStandardDeviation = Math.max(
-        ...[...byEvery.values()].map(vals =>
-            vals.length < 2 ? 0 : Math.sqrt(squareSumFromMean(vals) / (vals.length - 1)),
-        ),
-    );
-    return maxStandardDeviation < 0.25; // TODO made up number
+    return Math.max(...[...countsByShowEvery.values()].map(standardDeviation)) <= maxStandardDeviation;
 }
 
-export function makeIconSets(icons: Sponsor[], maxIcons: number, maxIters = 100): Sponsor[][] {
+export function makeIconSets(
+    icons: Sponsor[],
+    maxIcons: number,
+    maxIters = 100,
+    maxStandardDeviation = 0.5,
+): Sponsor[][] {
     const result: Sponsor[][] = [];
     const sponsorAppearanceCount: Map<Sponsor, number> = new Map();
     for (const icon of icons) sponsorAppearanceCount.set(icon, 0);
-    while (!sponsorIconSetsOk(sponsorAppearanceCount, result.length)) {
+    while (!sponsorIconSetsOk(sponsorAppearanceCount, result.length, maxStandardDeviation)) {
         if (result.length > maxIters) {
             throw new Error(`Unable to find a solution in ${maxIters}`);
         }
         const toPick = icons.map(icon => {
             return {
                 icon: icon,
-                // TODO this is subtly different from the seenEvery calc in setsOk; is that ok?
-                error: (sponsorAppearanceCount.get(icon) || 0) - result.length / icon.topIconShowEvery,
+                // Number of times we'd expect to see this, divided by number of times we saw it
+                error: result.length / icon.topIconShowEvery / (sponsorAppearanceCount.get(icon) || 0.00001),
             };
         });
-        toPick.sort((lhs, rhs) => lhs.error - rhs.error);
+        toPick.sort((lhs, rhs) => rhs.error - lhs.error);
         const chosen = toPick
             .slice(0, maxIcons)
             .map(x => x.icon)
