867d24c9c3a2068337ab8404918e6eb679e6691b
Matt Godbolt <matt@godbolt.org>
2022-11-27 10:39:05 -0600

    Fix typo

--- examples/csharp/default.cs
+++ examples/csharp/default.cs
@@ -3,5 +3,5 @@ using System;
 class Program
 {
     static int Square(int num) => num * num;
-    static void Main() => Console.WriteLine(Sqaure(42));
+    static void Main() => Console.WriteLine(Square(42));
 }
