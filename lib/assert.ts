779600897fe67b70966c277422c799e1a31eaa94
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2022-12-14 23:54:43 -0500

    Playing around with diagnostics

--- lib/assert.ts
+++ lib/assert.ts
@@ -22,13 +22,153 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-import * as stacktrace from 'stack-trace';
+import * as fs from 'fs-extra';
+
+/* eslint-disable unicorn/no-abusive-eslint-disable */
+/* eslint-disable */
+namespace stacktrace {
+    export function get(belowFn) {
+        const oldLimit = Error.stackTraceLimit;
+        Error.stackTraceLimit = Infinity;
+
+        const dummyObject: any = {};
+
+        const v8Handler = Error.prepareStackTrace;
+        Error.prepareStackTrace = function (dummyObject, v8StackTrace) {
+            return v8StackTrace;
+        };
+        Error.captureStackTrace(dummyObject, belowFn || get);
+
+        const v8StackTrace = dummyObject.stack;
+        Error.prepareStackTrace = v8Handler;
+        Error.stackTraceLimit = oldLimit;
+
+        return v8StackTrace;
+    }
+
+    export function parse(err) {
+        if (!err.stack) {
+            return [];
+        }
+
+        const lines = err.stack.split('\n').slice(1);
+        return lines
+            .map(function (line) {
+                if (line.match(/^\s*[-]{4,}$/)) {
+                    return createParsedCallSite({
+                        fileName: line,
+                        lineNumber: null,
+                        functionName: null,
+                        typeName: null,
+                        methodName: null,
+                        columnNumber: null,
+                        native: null,
+                    });
+                }
+
+                const lineMatch = line.match(/at (?:(.+?)\s+\()?(?:(.+?):(\d+)(?::(\d+))?|([^)]+))\)?/);
+                if (!lineMatch) {
+                    return;
+                }
+
+                let object: string | null = null;
+                let method: string | null = null;
+                let functionName: string | null = null;
+                let typeName: string | null = null;
+                let methodName: string | null = null;
+                let isNative = lineMatch[5] === 'native';
+
+                if (lineMatch[1]) {
+                    functionName = lineMatch[1];
+                    let methodStart = functionName!.lastIndexOf('.');
+                    if (functionName![methodStart - 1] == '.') methodStart--;
+                    if (methodStart > 0) {
+                        object = functionName!.substr(0, methodStart);
+                        method = functionName!.substr(methodStart + 1);
+                        const objectEnd = object.indexOf('.Module');
+                        if (objectEnd > 0) {
+                            functionName = functionName!.substr(objectEnd + 1);
+                            object = object.substr(0, objectEnd);
+                        }
+                    }
+                }
+
+                if (method) {
+                    typeName = object;
+                    methodName = method;
+                }
+
+                if (method === '<anonymous>') {
+                    methodName = null;
+                    functionName = null;
+                }
+
+                const properties = {
+                    fileName: lineMatch[2] || null,
+                    lineNumber: parseInt(lineMatch[3], 10) || null,
+                    functionName: functionName,
+                    typeName: typeName,
+                    methodName: methodName,
+                    columnNumber: parseInt(lineMatch[4], 10) || null,
+                    native: isNative,
+                };
+
+                return createParsedCallSite(properties);
+            })
+            .filter(function (callSite) {
+                return !!callSite;
+            });
+    }
+
+    function CallSite(this: any, properties) {
+        for (const property in properties) {
+            this[property] = properties[property];
+        }
+    }
+
+    const strProperties = [
+        'this',
+        'typeName',
+        'functionName',
+        'methodName',
+        'fileName',
+        'lineNumber',
+        'columnNumber',
+        'function',
+        'evalOrigin',
+    ];
+
+    const boolProperties = ['topLevel', 'eval', 'native', 'constructor'];
+
+    strProperties.forEach(function (property) {
+        CallSite.prototype[property] = null;
+        CallSite.prototype['get' + property[0].toUpperCase() + property.substr(1)] = function () {
+            return this[property];
+        };
+    });
+
+    boolProperties.forEach(function (property) {
+        CallSite.prototype[property] = false;
+        CallSite.prototype['is' + property[0].toUpperCase() + property.substr(1)] = function () {
+            return this[property];
+        };
+    });
+
+    function createParsedCallSite(properties) {
+        return new CallSite(properties);
+    }
+}
+/* eslint-enable */
+/* eslint-enable unicorn/no-abusive-eslint-disable */
 
 export function assert<C>(c: C, message?: string): asserts c {
     if (!c) {
-        const e = new Error('Assertion failed' + (message ? ': ' + message : ''));
-        console.log('--->', stacktrace.parse(e)); // eslint-disable-line no-console
-        throw e;
+        const e = new Error(); // eslint-disable-line unicorn/error-message
+        const trace = stacktrace.parse(e);
+        // eslint-disable-next-line import/namespace
+        const file = fs.readFileSync(trace[1].fileName, 'utf-8');
+        const lines = file.split('\n');
+        throw new Error('Assertion failed' + (message ? ': ' + message : '') + '\n\n' + lines[trace[1].lineNumber - 1]);
     }
 }
 
