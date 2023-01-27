784393bbc78342197ac26ad8e9b236f55bcc74bb
Matt Godbolt <matt@godbolt.org>
2023-01-27 09:14:31 -0600

    Convert asm-raw to typescript (#4647)

--- lib/parsers/asm-raw.ts
+++ lib/parsers/asm-raw.ts
@@ -22,11 +22,14 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
+import {AsmResultLink, ParsedAsmResult, ParsedAsmResultLine} from '../../types/asmresult/asmresult.interfaces';
+import {ParseFiltersAndOutputOptions} from '../../types/features/filters.interfaces';
+
 import {AsmRegex} from './asmregex';
 
 export class AsmRaw extends AsmRegex {
-    processBinaryAsm(asm, filters) {
-        const result = [];
+    processBinaryAsm(asm: string, filters: ParseFiltersAndOutputOptions): ParsedAsmResult {
+        const result: ParsedAsmResultLine[] = [];
         const asmLines = asm.split('\n');
         const asmOpcodeRe = /^\s*([\da-f]+):\s*(([\da-f]{2} ?)+)\s*(.*)/;
         const labelRe = /^([\da-f]+)\s+<([^>]+)>:$/;
@@ -55,7 +58,7 @@ export class AsmRaw extends AsmRegex {
                 const address = parseInt(match[1], 16);
                 const opcodes = match[2].split(' ').filter(Boolean);
                 const disassembly = ' ' + AsmRegex.filterAsmLine(match[4], filters);
-                let links = null;
+                let links: AsmResultLink[] | undefined;
                 const destMatch = line.match(destRe);
                 if (destMatch) {
                     links = [
