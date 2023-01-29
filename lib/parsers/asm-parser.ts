0bc5e57ac1d50428bf043fd7ea152b5f354c41a2
Matt Godbolt <matt@godbolt.org>
2023-01-29 17:22:17 -0600

    Convert asmamdgpu to ts (#4653)

--- lib/parsers/asm-parser.ts
+++ lib/parsers/asm-parser.ts
@@ -31,7 +31,9 @@ import {
     ParsedAsmResultLine,
 } from '../../types/asmresult/asmresult.interfaces';
 import {ParseFiltersAndOutputOptions} from '../../types/features/filters.interfaces';
-import {logger} from '../logger';
+import {assert} from '../assert';
+import {isString} from '../common-utils';
+import {PropertyGetter} from '../properties.interfaces';
 import * as utils from '../utils';
 
 import {IAsmParser} from './asm-parser.interfaces';
@@ -80,7 +82,8 @@ export class AsmParser extends AsmRegex implements IAsmParser {
     stdInLooking: RegExp;
     endBlock: RegExp;
     blockComments: RegExp;
-    constructor(compilerProps) {
+
+    constructor(compilerProps?: PropertyGetter) {
         super();
 
         this.labelFindNonMips = /[.A-Z_a-z][\w$.]*/g;
@@ -112,6 +115,7 @@ export class AsmParser extends AsmRegex implements IAsmParser {
         if (compilerProps) {
             const binaryHideFuncReValue = compilerProps('binaryHideFuncRe');
             if (binaryHideFuncReValue) {
+                assert(isString(binaryHideFuncReValue));
                 this.binaryHideFuncRe = new RegExp(binaryHideFuncReValue);
             }
 
