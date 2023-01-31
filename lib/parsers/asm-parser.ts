fc94ccd5a71b6291f678e878932f11dcfe1928bc
Matt Godbolt <matt@godbolt.org>
2023-01-30 22:13:41 -0600

    Ts-ify dart (#4665)

--- lib/parsers/asm-parser.ts
+++ lib/parsers/asm-parser.ts
@@ -609,7 +609,7 @@ export class AsmParser extends AsmRegex implements IAsmParser {
         return !this.binaryHideFuncRe.test(func);
     }
 
-    processBinaryAsm(asmResult, filters): ParsedAsmResult {
+    processBinaryAsm(asmResult: string, filters: ParseFiltersAndOutputOptions): ParsedAsmResult {
         const startTime = process.hrtime.bigint();
         const asm: ParsedAsmResultLine[] = [];
         const labelDefinitions: Record<string, number> = {};
@@ -647,6 +647,7 @@ export class AsmParser extends AsmRegex implements IAsmParser {
             }
             let match = line.match(this.lineRe);
             if (match) {
+                assert(match.groups);
                 if (dontMaskFilenames) {
                     source = {
                         file: utils.maskRootdir(match[1]),
@@ -696,6 +697,7 @@ export class AsmParser extends AsmRegex implements IAsmParser {
 
             match = line.match(this.asmOpcodeRe);
             if (match) {
+                assert(match.groups);
                 const address = parseInt(match.groups.address, 16);
                 const opcodes = (match.groups.opcodes || '').split(' ').filter(x => !!x);
                 const disassembly = ' ' + AsmRegex.filterAsmLine(match.groups.disasm, filters);
@@ -722,6 +724,7 @@ export class AsmParser extends AsmRegex implements IAsmParser {
 
             match = line.match(this.relocationRe);
             if (match) {
+                assert(match.groups);
                 const address = parseInt(match.groups.address, 16);
                 const relocname = match.groups.relocname;
                 const relocdata = match.groups.relocdata;
