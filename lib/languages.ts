626285fef9ef234f59c974278e6aef11b339ca28
Marc Poulhiès <dkm@kataplop.net>
2023-02-03 21:44:54 +0100

    Initial support for Modula-2 language
    With the upcoming gcc 13 release and it's shiny Modula-2 frontend, we
    are introducing Modula-2 support in Compiler Explorer :)
    
    Currently, only the gcc-snapshot build supports modula-2.
    
    fixes #4688
    
    Signed-off-by: Marc Poulhiès <dkm@kataplop.net>

--- lib/languages.ts
+++ lib/languages.ts
@@ -418,6 +418,17 @@ const definitions: Record<LanguageKey, LanguageDefinition> = {
         previewFilter: null,
         monacoDisassembly: null,
     },
+    modula2: {
+        name: 'modula2',
+        monaco: 'modula2',
+        extensions: ['.mod'],
+        alias: [],
+        logoUrl: null,
+        logoUrlDark: null,
+        formatter: null,
+        previewFilter: null,
+        monacoDisassembly: null,
+    },
     nim: {
         name: 'Nim',
         monaco: 'nim',
