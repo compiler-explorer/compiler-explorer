3ab93a7abdcb389bd2c599e8628ebec013dff5e2
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2023-02-10 12:38:51 -0500

    Matt suggestions

--- lib/handlers/api.ts
+++ lib/handlers/api.ts
@@ -174,7 +174,7 @@ export class ApiHandler {
     handleLanguages(req: express.Request, res: express.Response) {
         const availableLanguages = this.usedLangIds.map(val => {
             const lang = this.languages[val];
-            const newLangObj: Language & {defaultCompiler?: string} = Object.assign({}, lang);
+            const newLangObj: Language = Object.assign({}, lang);
             if (this.options) {
                 newLangObj.defaultCompiler = this.options.options.defaultCompiler[unwrap(lang).id];
             }
