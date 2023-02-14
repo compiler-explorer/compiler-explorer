7e0e955831d8958a119b899878f1bb1899867888
Matt Godbolt <matt@godbolt.org>
2023-02-13 21:53:23 -0600

    TSify analysis tests

--- test/analysis-tests.ts
+++ test/analysis-tests.ts
@@ -22,29 +22,33 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-import {LLVMmcaTool} from '../lib/compilers/llvm-mca';
+import {CompilationEnvironment} from '../lib/compilation-env';
+import {AnalysisTool, LLVMmcaTool} from '../lib/compilers';
+import {LanguageKey} from '../types/languages.interfaces';
 
-import {makeCompilationEnvironment} from './utils';
+import {makeCompilationEnvironment, makeFakeCompilerInfo, makeFakeParseFiltersAndOutputOptions} from './utils';
 
 const languages = {
-    analysis: {id: 'analysis'},
+    analysis: {id: 'analysis' as LanguageKey},
 };
 
 describe('LLVM-mca tool definition', () => {
-    let ce, a;
+    let ce: CompilationEnvironment;
+    let a: LLVMmcaTool;
 
     before(() => {
         ce = makeCompilationEnvironment({languages});
-        const info = {
-            exe: null,
+        const info = makeFakeCompilerInfo({
+            id: 'id',
+            name: 'name',
             remote: true,
             lang: languages.analysis.id,
-        };
+        });
         a = new LLVMmcaTool(info, ce);
     });
 
     it('should have most filters disabled', () => {
-        a.compiler.disabledFilters.should.be.deep.equal(['labels', 'directives', 'commentOnly', 'trim']);
+        a.getInfo().disabledFilters.should.deep.equal(['labels', 'directives', 'commentOnly', 'trim']);
     });
 
     it('should default to most filters off', () => {
@@ -61,18 +65,22 @@ describe('LLVM-mca tool definition', () => {
     });
 
     it('should support "-o output-file" by default', () => {
-        const opts = a.optionsForFilter({commentOnly: false, labels: true}, 'output.txt');
-        opts.should.be.deep.equal(['-o', 'output.txt']);
+        const opts = a.optionsForFilter(
+            makeFakeParseFiltersAndOutputOptions({
+                commentOnly: false,
+                labels: true,
+            }),
+            'output.txt',
+        );
+        opts.should.deep.equal(['-o', 'output.txt']);
     });
 
     it('should split if disabledFilters is a string', () => {
-        const info = {
-            exe: null,
+        const info = makeFakeCompilerInfo({
             remote: true,
             lang: 'analysis',
-            disabledFilters: 'labels,directives',
-        };
-        const a = new LLVMmcaTool(info, ce);
-        a.compiler.disabledFilters.should.be.deep.equal(['labels', 'directives']);
+            disabledFilters: 'labels,directives' as any,
+        });
+        new AnalysisTool(info, ce).getInfo().disabledFilters.should.deep.equal(['labels', 'directives']);
     });
 });
