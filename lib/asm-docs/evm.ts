f28684c3f22e010d964e67090affe752a7cc80f4
Mats Larsen <me@supergrecko.com>
2023-02-25 19:01:24 +0100

    fix: update imports for asm-docs directory

--- lib/asm-docs/evm.ts
+++ lib/asm-docs/evm.ts
@@ -22,8 +22,8 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-import {AssemblyInstructionInfo, BaseAssemblyDocumentationProvider} from './base';
-import {getAsmOpcode} from './generated/asm-docs-evm';
+import {AssemblyInstructionInfo, BaseAssemblyDocumentationProvider} from './base.js';
+import {getAsmOpcode} from './generated/asm-docs-evm.js';
 
 export class EvmDocumentationProvider extends BaseAssemblyDocumentationProvider {
     public static get key() {
