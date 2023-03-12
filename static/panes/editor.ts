ca4ab7148b6a873ca6787c4b675c13fead797cbb
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2023-03-12 19:40:12 -0400

    Give an error when no editor language is specified (#4864)
    Following discussion at #4817 and with partouf/matt. An editor's
    state.lang being undefined should probably always be an error since
    it'll be susceptible to the same default language shenanigans.
    
    Closes #4817

--- static/panes/editor.ts
+++ static/panes/editor.ts
@@ -114,6 +114,13 @@ export class Editor extends MonacoPane<monaco.editor.IStandaloneCodeEditor, Edit
         this.alertSystem = new Alert();
         this.alertSystem.prefixMessage = 'Editor #' + this.id;
 
+        if ((state.lang as any) === undefined && Object.keys(languages).length > 0) {
+            // Primarily a diagnostic for urls created outside CE. Addresses #4817.
+            this.alertSystem.alert('State Error', 'No language specified for editor', {isError: true});
+        } else if (!(state.lang in languages) && Object.keys(languages).length > 0) {
+            this.alertSystem.alert('State Error', 'Unknown language specified for editor', {isError: true});
+        }
+
         if (this.currentLanguage) this.onLanguageChange(this.currentLanguage.id, true);
 
         if (state.source !== undefined) {
