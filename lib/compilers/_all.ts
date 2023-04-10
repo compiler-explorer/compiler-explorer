c617cc21e0c5f8259f0ef472cf4bfd3dd17758e9
David Hewitt <davidmhewitt@gmail.com>
2023-04-10 09:29:49 +0100

    Add the Vala programming language

--- lib/compilers/_all.ts
+++ lib/compilers/_all.ts
@@ -104,6 +104,7 @@ export {TinyCCompiler} from './tinyc.js';
 export {ToitCompiler} from './toit.js';
 export {TurboCCompiler} from './turboc.js';
 export {TypeScriptNativeCompiler} from './typescript-native.js';
+export {ValaCompiler} from './vala.js';
 export {VBCompiler} from './dotnet.js';
 export {V8Compiler} from './v8.js';
 export {Win32Compiler} from './win32.js';
