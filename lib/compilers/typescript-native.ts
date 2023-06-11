562ba2f8aa44f07536adb61588a3076634277f0b
ASDAlexander77 <duzhar@googlemail.com>
2023-06-11 23:30:09 +0100

    added asm output

--- lib/compilers/typescript-native.ts
+++ lib/compilers/typescript-native.ts
@@ -23,14 +23,15 @@
 // POSSIBILITY OF SUCH DAMAGE.
 
 import Semver from 'semver';
+import path from 'path';
 
-import type {CompilationResult, ExecutionOptions} from '../../types/compilation/compilation.interfaces.js';
 import type {PreliminaryCompilerInfo} from '../../types/compiler.interfaces.js';
 import type {ParseFiltersAndOutputOptions} from '../../types/features/filters.interfaces.js';
 import {BaseCompiler} from '../base-compiler.js';
 import {asSafeVer} from '../utils.js';
 
 import {TypeScriptNativeParser} from './argument-parsers.js';
+import { ExecutionOptions } from '../../types/compilation/compilation.interfaces.js';
 
 export class TypeScriptNativeCompiler extends BaseCompiler {
     static get key() {
@@ -40,24 +41,65 @@ export class TypeScriptNativeCompiler extends BaseCompiler {
     tscJit: string;
     tscSharedLib: string;
     tscNewOutput: boolean;
+    tscAsmOutput: boolean;
 
     constructor(compilerInfo: PreliminaryCompilerInfo, env) {
         super(compilerInfo, env);
 
-        this.compiler.supportsIntel = false;
-        this.compiler.supportsIrView = true;
-
         this.tscJit = this.compiler.exe;
         this.tscSharedLib = this.compilerProps<string>(`compiler.${this.compiler.id}.sharedlibs`);
         this.tscNewOutput = Semver.gt(asSafeVer(this.compiler.semver || '0.0.0'), '0.0.32', true);
+        this.tscAsmOutput = Semver.gt(asSafeVer(this.compiler.semver || '0.0.0'), '0.0.34', true);
+
+        this.compiler.irArg = ['--emit=llvm'];
+        this.compiler.supportsIntel = this.tscAsmOutput;
+        this.compiler.supportsIrView = true;
     }
 
     override getSharedLibraryPathsAsArguments() {
         return [];
     }
 
-    override optionsForFilter(filters: ParseFiltersAndOutputOptions, outputFilename: string) {
-        return [this.filename(outputFilename)];
+    override optionsForFilter(
+        filters: ParseFiltersAndOutputOptions,
+        outputFilename: string,
+        userOptions?: string[],
+    ): string[] {
+        return [];
+    }    
+
+    override optionsForBackend(backendOptions: Record<string, any>, outputFilename: string): string[] {
+        let addOpts: string[] = [];
+
+        addOpts.push('--emit=asm');
+
+        if (!this.tscSharedLib) {
+            addOpts.push('-nogc');
+        }
+
+        if (this.tscNewOutput) {
+            addOpts.push('-o=' + this.filename(outputFilename));
+        }
+
+        return addOpts;
+    }    
+
+    override getIrOutputFilename(inputFilename: string, filters: ParseFiltersAndOutputOptions): string {
+        const outputFilename = this.getOutputFilename(path.dirname(inputFilename), this.outputFilebase);
+        // As per #4054, if we are asked for binary mode, the output will be in the .s file, no .ll will be emited
+        if (!filters.binary) {
+            return outputFilename.replace('.s', '.ll');
+        }
+        return outputFilename;
+    }
+
+    override async generateIR(inputFilename: string, options: string[], filters: ParseFiltersAndOutputOptions) {
+        const newOptions = [ ...options.filter(e => !e.startsWith('--emit=') && !e.startsWith('-o=')) ];
+        if (this.tscNewOutput) {
+            newOptions.push('-o=' + this.getIrOutputFilename(inputFilename, filters));
+        }
+
+        return await super.generateIR(inputFilename, newOptions, filters);
     }
 
     override async handleInterpreting(key, executeParameters) {
@@ -70,50 +112,17 @@ export class TypeScriptNativeCompiler extends BaseCompiler {
         return await super.handleInterpreting(key, executeParameters);
     }
 
-    override async runCompiler(
-        compiler: string,
-        options: string[],
-        inputFilename: string,
-        execOptions: ExecutionOptions,
-    ): Promise<CompilationResult> {
-        // These options make Clang produce an IR
-        const newOptions = ['--emit=mlir-llvm', inputFilename];
-
-        if (!this.tscSharedLib) {
-            newOptions.push('-nogc');
-        }
-
-        const output = await this.runCompilerRawOutput(
-            this.tscJit,
-            newOptions,
-            this.filename(inputFilename),
-            execOptions,
-        );
-        if (output.code !== 0) {
-            return {
-                code: output.code,
-                timedOut: false,
-                stdout: [],
-                stderr: [
-                    {
-                        text: 'Failed to run compiler to get MLIR code',
-                    },
-                ],
-            };
-        }
-
-        return {code: 0, timedOut: false, stdout: [], stderr: []};
-    }
-
+    /*
     override async generateIR(inputFilename: string, options: string[], filters: ParseFiltersAndOutputOptions) {
         // These options make Clang produce an IR
-        let newOptions = ['--emit=llvm', inputFilename];
-        if (this.tscNewOutput) {
-            newOptions = ['--emit=llvm', '-o=-', inputFilename];
-        }
+        const newOptions = ['--emit=llvm'];
 
         if (!this.tscSharedLib) {
             newOptions.push('-nogc');
+        }
+
+        if (this.tscNewOutput) {
+            return await super.generateIR(inputFilename, options, filters);
         }
 
         const execOptions = this.getDefaultExecOptions();
@@ -137,6 +146,7 @@ export class TypeScriptNativeCompiler extends BaseCompiler {
         const ir = await this.llvmIr.process(this.tscNewOutput ? output.stdout : output.stderr, filters);
         return ir.asm;
     }
+    */
 
     override isCfgCompiler() {
         return true;
