9f982a6e61affaf19d408c64b362b060dee47788
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2023-06-17 17:51:03 -0400

    Try to capture more details about a bug

--- lib/base-compiler.ts
+++ lib/base-compiler.ts
@@ -103,6 +103,7 @@ import {
     ConfiguredOverrides,
 } from '../types/compilation/compiler-overrides.interfaces.js';
 import {LLVMIrBackendOptions} from '../types/compilation/ir.interfaces.js';
+import {SentryCapture} from './sentry.js';
 
 const compilationTimeHistogram = new PromClient.Histogram({
     name: 'ce_base_compiler_compilation_duration_seconds',
@@ -2728,14 +2729,18 @@ export class BaseCompiler implements ICompiler {
 
         if (this.compiler.demangler) {
             const result = JSON.stringify(output, null, 4);
+            let demangleResult: UnprocessedExecResult | null = null;
             try {
-                const demangleResult = await this.exec(
+                demangleResult = await this.exec(
                     this.compiler.demangler,
                     [...this.compiler.demanglerArgs, '-n', '-p'],
                     {input: result},
                 );
                 return JSON.parse(demangleResult.stdout);
             } catch (exception) {
+                // TODO(jeremy-rifkin): Temp triage for
+                // https://github.com/compiler-explorer/compiler-explorer/issues/2984
+                SentryCapture(demangleResult, 'during opt demangle parsing');
                 // swallow exception and return non-demangled output
                 logger.warn(`Caught exception ${exception} during opt demangle parsing`);
             }
