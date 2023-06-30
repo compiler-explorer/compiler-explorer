e13a84f96c6cc68d8ff7b239fc0934730e105aca
Yin Hu (Eva) <yinhueva@gmail.com>
2023-06-29 17:29:11 -0700

    Delete optionsForFilter()
    We know that we need to edit "cflat.ts", but we don't know how to override "getOutputFilename()" and "runCompiler()".

--- lib/compilers/cflat.ts
+++ lib/compilers/cflat.ts
@@ -1,11 +1,7 @@
 import {BaseCompiler} from '../base-compiler.js';
-import type {ParseFiltersAndOutputOptions} from '../../types/features/filters.interfaces.js';
 
 export class CflatCompiler extends BaseCompiler {
     static get key() {
         return 'cflat';
-    }
-    override optionsForFilter(filters: ParseFiltersAndOutputOptions, outputFilename: string) {
-        return ['compile-only', '-g', '-l', 'pthread', '--emit-asm'];
     }
 }
