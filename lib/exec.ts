8e4d8714fc84b5265ee25e2fd2b87bb952157563
Guo Ci <zguoci@gmail.com>
2023-07-03 15:07:26 -0400

    resolve #4330 (#5196)
    #4330

--- lib/exec.ts
+++ lib/exec.ts
@@ -417,11 +417,7 @@ export function startWineInit() {
         }
 
         logger.info(`Killing any pre-existing wine-server`);
-        let result = child_process.exec(`${server} -k || true`, {env: env});
-        logger.info(`Result: ${result}`);
-        logger.info(`Waiting for any pre-existing server to stop...`);
-        result = child_process.exec(`${server} -w`, {env: env});
-        logger.info(`Result: ${result}`);
+        child_process.exec(`${server} -k || true`, {env: env});
 
         // We run a long-lived cmd process, to:
         // * test that WINE works
