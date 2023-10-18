d72be292d87513e288fb822b16b2a08b2c4e4156
nickpdemarco <nickpdemarco@gmail.com>
2023-10-18 10:28:22 -0400

    Remove old svg, comment

--- lib/compilers/hylo.ts
+++ lib/compilers/hylo.ts
@@ -2,14 +2,6 @@ import {BaseCompiler} from '../base-compiler.js';
 import type {PreliminaryCompilerInfo} from '../../types/compiler.interfaces.js';
 import type {ParseFiltersAndOutputOptions} from '../../types/features/filters.interfaces.js';
 
-// TODO: remove this comment
-// For reference, the basic behaviour of BaseCompiler is:
-// - make a random temporary folder
-// - save example.extension to the new folder, the full path to this is the inputFilename
-// - the outputFilename is determined by the getOutputFilename() method
-// - execute the compiler.exe with the arguments from OptionsForFilter() and adding inputFilename
-// - be aware that the language class is only instanced once, so storing state is not possible
-
 export class HyloCompiler extends BaseCompiler {
     static get key() {
         return 'hylo';
