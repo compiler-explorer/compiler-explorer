a0cb7f11aeeca933dda11947b3bf01ba85903f1c
nickpdemarco <nickpdemarco@gmail.com>
2023-10-18 16:34:27 -0400

    Get execution working by setting library paths to [], be more precise about when we emit intel

--- lib/compilers/hylo.ts
+++ lib/compilers/hylo.ts
@@ -1,6 +1,7 @@
 import {BaseCompiler} from '../base-compiler.js';
 import type {PreliminaryCompilerInfo} from '../../types/compiler.interfaces.js';
 import type {ParseFiltersAndOutputOptions} from '../../types/features/filters.interfaces.js';
+import {CompileChildLibraries} from '../../types/compilation/compilation.interfaces.js';
 
 export class HyloCompiler extends BaseCompiler {
     static get key() {
@@ -13,12 +14,20 @@ export class HyloCompiler extends BaseCompiler {
         // this.compiler.supportsIrView = true;
     }
 
+    override getSharedLibraryPathsAsArguments(
+        libraries: CompileChildLibraries[],
+        libDownloadPath?: string,
+        toolchainPath?: string,
+    ) {
+        return [];
+    }
+
     override optionsForFilter(
         filters: ParseFiltersAndOutputOptions,
         outputFilename: string,
         userOptions?: string[],
     ): string[] {
-        if (filters.intel) {
+        if (this.compiler.intelAsm && filters.intel && !filters.binary) {
             return ['--emit', 'intel-asm', '-o', this.filename(outputFilename)];
         } else {
             return ['-o', this.filename(outputFilename)];
