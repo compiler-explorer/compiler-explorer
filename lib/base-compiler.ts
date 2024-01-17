eddcea81719b76e7c9b7f16217fb50203c6b7e7a
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2024-01-16 20:09:11 -0600

    Fix issue with llvm language detection for cfg generation, fixes #6014

--- lib/base-compiler.ts
+++ lib/base-compiler.ts
@@ -2923,7 +2923,10 @@ export class BaseCompiler implements ICompiler {
             // TODO rephrase this so we don't need to reassign
             result = filters.demangle ? await this.postProcessAsm(result, filters) : result;
             if (this.compiler.supportsCfg && backendOptions.produceCfg && backendOptions.produceCfg.asm) {
-                const isLlvmIr = (options && options.includes('-emit-llvm')) || this.llvmIr.isLlvmIr(result.asm);
+                const isLlvmIr =
+                    this.compiler.instructionSet === 'llvm' ||
+                    (options && options.includes('-emit-llvm')) ||
+                    this.llvmIr.isLlvmIr(result.asm);
                 result.cfg = cfg.generateStructure(this.compiler, result.asm, isLlvmIr);
             }
         }
