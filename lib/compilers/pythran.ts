03956645162dd91c144d0d5715f29b5f37d12203
Marc Poulhiès <dkm@kataplop.net>
2024-02-28 18:52:12 +0100

    fix pythran wrt ldpath
    refs #6079
    
    Signed-off-by: Marc Poulhiès <dkm@kataplop.net>

--- lib/compilers/pythran.ts
+++ lib/compilers/pythran.ts
@@ -1,4 +1,7 @@
 import path from 'path';
+
+import {CompileChildLibraries} from '../../types/compilation/compilation.interfaces.js';
+
 import {BaseCompiler} from '../base-compiler.js';
 import type {PreliminaryCompilerInfo} from '../../types/compiler.interfaces.js';
 import type {ParseFiltersAndOutputOptions} from '../../types/features/filters.interfaces.js';
@@ -15,15 +18,22 @@ export class PythranCompiler extends BaseCompiler {
         this.cpp_compiler_root = this.compilerProps<string>(`compiler.${this.compiler.id}.cpp_compiler_root`);
     }
 
-    override getDefaultExecOptions() {
-        const execOptions = super.getDefaultExecOptions();
+    override getSharedLibraryPaths(libraries: CompileChildLibraries[], dirPath?: string): string[] {
+        let ldpath = super.getSharedLibraryPaths(libraries, dirPath);
         if (this.cpp_compiler_root) {
-            execOptions.env.PATH = path.join(this.cpp_compiler_root, 'bin');
-            const ld_library_path = [
+            ldpath = ldpath.concat(
                 path.join(this.cpp_compiler_root, 'lib'),
                 path.join(this.cpp_compiler_root, 'lib64'),
-            ];
-            execOptions.env.LD_LIBRARY_PATH = ld_library_path.join(':');
+            );
+        }
+        return ldpath;
+    }
+
+    override getDefaultExecOptions() {
+        const execOptions = super.getDefaultExecOptions();
+
+        if (this.cpp_compiler_root) {
+            execOptions.env.PATH = execOptions.env.PATH + ':' + path.join(this.cpp_compiler_root, 'bin');
         }
 
         return execOptions;
