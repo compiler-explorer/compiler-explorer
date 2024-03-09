1aed5a0abc8b0c6877783c409f8a5409d8632ed6
Matt Godbolt <matt@godbolt.org>
2024-03-08 21:38:32 -0600

    Getting there

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vitest/config';
 export default defineConfig({
     test: {
         include: ['test/**/*.ts'],
+        exclude: ['test/_*.ts', 'test/utils.ts'],
         setupFiles: ['/test/_setup-fake-aws.ts', '/test/_setup-log.ts'],
     },
 });
