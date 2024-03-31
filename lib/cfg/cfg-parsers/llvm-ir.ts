4f006dd688c4a311fbac8634f85d436e832e4c79
partouf <partouf@gmail.com>
2024-03-31 21:41:18 +0200

    oops

--- lib/cfg/cfg-parsers/llvm-ir.ts
+++ lib/cfg/cfg-parsers/llvm-ir.ts
@@ -265,13 +265,13 @@ export class LlvmIrCfgParser extends BaseCFGParser {
                     edges.push(
                         {
                             from: bb.nameId,
-                            to: labels.at[labels.length - 2],
+                            to: labels[labels.length - 2],
                             arrows: 'to',
                             color: 'green',
                         },
                         {
                             from: bb.nameId,
-                            to: labels.at[labels.length - 1],
+                            to: labels[labels.length - 1],
                             arrows: 'to',
                             color: 'grey',
                         },
