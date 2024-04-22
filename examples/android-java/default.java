2ab24a106590e584b5d7ba05231cde33ffd16961
Kevin Jeon <kevinjeon@google.com>
2024-04-22 14:24:29 -0400

    Add profile support for dex2oat.
    dex2oat supports profile-guided compilation. In addition to taking dex
    code as an input, it can take a profile as another input, to guide the
    compilation. The profile determines which methods need to be compiled,
    how calls can be inlined, and so on.
    
    Because Compiler Explorer doesn't support multiple input files, the
    profile is added as a comment block in the Java/Kotlin source code. A
    post-processor extracts the profile from the comment block into a
    separate file, in order to input it to dex2oat.

--- examples/android-java/default.java
+++ examples/android-java/default.java
@@ -5,3 +5,10 @@ class Square {
     }
 }
 
+// Set `enabled` to `true` to enable profile-guided compilation for dex2oat.
+// (For advanced use only!)
+/* ---------- begin profile (enabled=false) ----------
+HSPLSquare;-><init>()V
+HSPLSquare;->square(I)I
+LSquare;
+---------- end profile ---------- */
--- examples/android-kotlin/default.kt
+++ examples/android-kotlin/default.kt
@@ -1,2 +1,9 @@
 // Type your code here, or load an example.
 fun square(num: Int): Int = num * num
+
+// Set `enabled` to `true` to enable profile-guided compilation for dex2oat.
+// (For advanced use only!)
+/* ---------- begin profile (enabled=false) ----------
+HSPLExampleKt;->square(I)I
+LExampleKt;
+---------- end profile ---------- */
