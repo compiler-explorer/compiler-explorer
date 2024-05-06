f7381bd04dac3d7cb2b80ec82accbdd9fcc44696
Ofek Shilon <ofekshilon@gmail.com>
2024-05-06 22:04:55 +0300

    Readability improvement (thanks partouf)

--- lib/cfg/cfg-parsers/llvm-ir.ts
+++ lib/cfg/cfg-parsers/llvm-ir.ts
@@ -62,9 +62,10 @@ export class LlvmIrCfgParser extends BaseCFGParser {
         while (i < asmArr.length) {
             if (this.functionDefinition.test(asmArr[i].text)) {
                 const start = i;
-                while (i < asmArr.length - 1 && asmArr[i].text !== '}') {
+                do {
                     i++;
-                }
+                } while (i < asmArr.length && asmArr[i].text !== '}');
+
                 // start is the function define, end is the closing brace
                 result.push({
                     start,
