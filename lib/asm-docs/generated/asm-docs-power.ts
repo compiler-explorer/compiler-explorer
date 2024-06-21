452330af393570d32bda9242cc3d140915a580d8
Nicholas Hubbard <nhubbard@users.noreply.github.com>
2024-06-21 11:39:37 -0500

    feature/power-asm: Add initial ASM docs for POWER ISA; instructions starting with letter A are covered

---
+++ lib/asm-docs/generated/asm-docs-power.ts
@@ -0,0 +1,199 @@
+import { AssemblyInstructionInfo } from "../base.js";
+
+// Based on OpenPOWER ISA v3.1C
+export function getAsmOpcode(opcode: string | undefined): AssemblyInstructionInfo | undefined {
+    if (!opcode) return;
+    switch (opcode.toUpperCase()) {
+        case "ABS":
+        case "ABS.":
+        case "ABSO":
+        case "ABSO.":
+            return {
+                "html": `
+                    <p>The <strong>abs</strong> instruction places the absolute value of the contents of general-purpose register (GPR) <em>RA</em> into the target GPR <em>RT</em>.</p>
+                    <p>If GPR <em>RA</em> contains the most negative number ('8000 0000'), the result of the instruction is the most negative number, and the instruction will set the Overflow bit in the Fixed-Point Exception Register to 1 if the OE bit is set to 1.</p>
+                    <p>The <strong>abs</strong> instruction has four syntax forms. Each syntax form has a different effect on Condition Register Field 0 and the Fixed-Point Exception Register.</p>
+                    <p>The four syntax forms of the <strong>abs</strong> instruction always affect the Carry bit (CA) in the Fixed-Point Exception Register. If the syntax form sets the Overflow Exception (OE) bit to 1, the instruction affects the Summary Overflow (SO) and Overflow (OV) bits in the Fixed-Point Exception Register. If the syntax form sets the Record (Rc) bit to 1, the instruction affects the Less Than (LT) zero, Greater Than (GT) zero, Equal To (EQ) zero, and Summary Overflow (SO) bits in Condition Register Field 0.</p>
+                `,
+                "tooltip": "Absolute",
+                "url": "https://www.ibm.com/docs/en/aix/7.3?topic=set-abs-absolute-instruction"
+            };
+        case "ADD":
+        case "ADD.":
+        case "ADDO":
+        case "ADDO.":
+        case "CAX":
+        case "CAX.":
+        case "CAXO":
+        case "CAXO.":
+            return {
+                "html": `
+                    <p>The <strong>add</strong> and <strong>cax</strong> instructions place the sum of the contents of general-purpose register (GPR) <em>RA</em> and GPR <em>RB</em> into the target GPR <em>RT</em>.</p>
+                    <p>The <strong>add</strong> and <strong>cax</strong> instructions have four syntax forms. Each syntax form has a different effect on Condition Register Field 0 and the Fixed-Point Exception Register.</p>
+                    <p>The four syntax forms of the <strong>add</strong> instruction and the four syntax forms of the <strong>cax</strong> instruction never affect the Carry bit (CA) in the Fixed-Point Exception Register. If the syntax form sets the Overflow Exception (OE) bit to 1, the instruction affects the Summary Overflow (SO) and Overflow (OV) bits in the Fixed-Point Exception Register. If the syntax form sets the Record (Rc) bit to 1, the instruction affects the Less Than (LT) zero, Greater Than (GT) zero, Equal To (EQ) zero, and Summary Overflow (SO) bits in Condition Register Field 0.</p>
+                `,
+                "tooltip": "Add or Compute Address",
+                "url": "https://www.ibm.com/docs/en/aix/7.3?topic=set-add-add-cax-compute-address-instruction"
+            };
+        case "ADDC":
+        case "ADDC.":
+        case "ADDCO":
+        case "ADDCO.":
+        case "A":
+        case "A.":
+        case "AO":
+        case "AO.":
+            return {
+                "html": `
+                    <p>The <strong>addc</strong> and <strong>a</strong> instructions place the sum of the contents of general-purpose register (GPR) <em>RA</em> and GPR <em>RB</em> into the target GPR <em>RT</em>.</p>
+                    <p>The <strong>addc</strong> instruction has four syntax forms. Each syntax form has a different effect on Condition Register Field 0 and the Fixed-Point Exception Register.</p>
+                    <p>The <strong>a</strong> instruction has four syntax forms. Each syntax form has a different effect on Condition Register Field 0 and the Fixed-Point Exception Register.</p>
+                    <p>The four syntax forms of the <strong>addc</strong> instruction and the four syntax forms of the <strong>a</strong> instruction always affect the Carry bit (CA) in the Fixed-Point Exception Register. If the syntax form sets the Overflow Exception (OE) bit to 1, the instruction affects the Summary Overflow (SO) and Overflow (OV) bits in the Fixed-Point Exception Register. If the syntax form sets the Record (Rc) bit to 1, the instruction affects the Less Than (LT) zero, Greater Than (GT) zero, Equal To (EQ) zero, and Summary Overflow (SO) bits in Condition Register Field 0.</p>
+                `,
+                "tooltip": "Add Carrying",
+                "url": "https://www.ibm.com/docs/en/aix/7.3?topic=set-addc-add-carrying-instruction"
+            };
+        case "ADDE":
+        case "ADDE.":
+        case "ADDEO":
+        case "ADDEO.":
+        case "AE":
+        case "AE.":
+        case "AEO":
+        case "AEO.":
+            return {
+                "html": `
+                    <p>The <strong>adde</strong> and <strong>ae</strong> instructions place the sum of the contents of general-purpose register (GPR) <em>RA</em>, GPR <em>RB</em>, and the Carry bit into the target GPR <em>RT</em>.</p>
+                    <p>The <strong>adde</strong> instruction has four syntax forms. Each syntax form has a different effect on Condition Register Field 0 and the Fixed-Point Exception Register.</p>
+                    <p>The <strong>ae</strong> instruction has four syntax forms. Each syntax form has a different effect on Condition Register Field 0 and the Fixed-Point Exception Register.</p>
+                    <p>The four syntax forms of the <strong>adde</strong> instruction and the four syntax forms of the <strong>ae</strong> instruction always affect the Carry bit (CA) in the Fixed-Point Exception Register. If the syntax form sets the Overflow Exception (OE) bit to 1, the instruction affects the Summary Overflow (SO) and Overflow (OV) bits in the Fixed-Point Exception Register. If the syntax form sets the Record (Rc) bit to 1, the instruction affects the Less Than (LT) zero, Greater Than (GT) zero, Equal To (EQ) zero, and Summary Overflow (SO) bits in Condition Register Field 0.</p>
+                `,
+                "tooltip": "Add Extended",
+                "url": "https://www.ibm.com/docs/en/aix/7.3?topic=set-adde-ae-add-extended-instruction"
+            };
+        case "ADDI":
+        case "CAL":
+            return {
+                "html": `
+                    <p>The <strong>addi</strong> and <strong>cal</strong> instructions place the sum of the contents of general-purpose register (GPR) <em>RA</em> and the 16-bit two's complement integer <em>SI</em> or <em>D</em>, sign-extended to 32 bits, into the target GPR <em>RT</em>. If GPR <em>RA</em> is GPR 0, then <em>SI</em> or <em>D</em> is stored into the target GPR <em>RT</em>.</p>
+                    <p>The <strong>addi</strong> and <strong>cal</strong> instructions have one syntax form and do not affect Condition Register Field 0 or the Fixed-Point Exception Register.</p>
+                `,
+                "tooltip": "Add Immediate or Compute Address Lower",
+                "url": "https://www.ibm.com/docs/en/aix/7.3?topic=is-addi-add-immediate-cal-compute-address-lower-instruction"
+            };
+        case "ADDIC":
+        case "AI":
+            return {
+                "html": `
+                    <p>The <strong>addic</strong> and <strong>ai</strong> instructions place the sum of the contents of general-purpose register (GPR) <em>RA</em> and a 16-bit signed integer, <em>SI</em>, into target GPR <em>RT</em>.</p>
+                    <p>The 16-bit integer provided as immediate data is sign-extended to 32 bits prior to carrying out the addition operation.</p>
+                    <p>The <strong>addic</strong> and <strong>ai</strong> instructions have one syntax form and can set the Carry bit of the Fixed-Point Exception Register; these instructions never affect Condition Register Field 0.</p>
+                `,
+                "tooltip": "Add Immediate Carrying",
+                "url": "https://www.ibm.com/docs/en/aix/7.3?topic=set-addic-ai-add-immediate-carrying-instruction"
+            };
+        case "ADDIC.":
+        case "AI.":
+            return {
+                "html": `
+                    <p>The <strong>addic.</strong> and <strong>ai.</strong> instructions place the sum of the contents of general-purpose register (GPR) <em>RA</em> and a 16-bit signed integer, <em>SI</em>, into the target GPR <em>RT</em>.</p>
+                    <p>The 16-bit integer <em>SI</em> provided as immediate data is sign-extended to 32 bits prior to carrying out the addition operation.</p>
+                    <p>The <strong>addic.</strong> and <strong>ai.</strong> instructions have one syntax form and can set the Carry Bit of the Fixed-Point Exception Register. These instructions also affect Condition Register Field 0.</p>
+                `,
+                "tooltip": "Add Immediate Carrying and Record",
+                "url": "https://www.ibm.com/docs/en/aix/7.3?topic=set-addic-ai-add-immediate-carrying-record-instruction"
+            };
+        case "ADDIS":
+        case "CAU":
+            return {
+                "html": `
+                    <p>The <strong>addis</strong> and <strong>cau</strong> instructions place the sum of the contents of general-purpose register (GPR) <em>RA</em> and the concatenation of a 16-bit unsigned integer, <em>SI</em> or <em>UI,</em> and x'0000' into the target GPR <em>RT</em>. If GPR <em>RA</em> is GPR 0, then the sum of the concatenation of 0, <em>SI</em> or <em>UI</em>, and x'0000' is stored into the target GPR <em>RT</em>.</p>
+                    <p>The <strong>addis</strong> and <strong>cau</strong> instructions do not affect Condition Register Field 0 or the Fixed-Point Exception Register.  The <strong>cau</strong> instruction has one syntax form. The <strong>addis</strong> instruction has two syntax forms; however, the second form is only valid when the R_TOCU relocation type is used in the <em>D</em> expression. The R_TOCU relocation type can be specified explicitly with the <strong>@u</strong> relocation specifier or implicitly by using a <strong>QualName</strong> parameter with a TE storage-mapping class.</p>
+                    <blockquote><span>“</span><strong>Note:</strong> The immediate value for the <strong>cau</strong> instruction is a 16-bit unsigned integer, whereas the immediate value for the <strong>addis</strong> instruction is a 16-bit signed integer. This difference is a result of extending the architecture to 64 bits.<span>”</span></blockquote>
+                    <p>The assembler does a 0 to 65535 value-range check for the <em>UI</em> field, and a -32768 to 32767 value-range check for the <em>SI</em> field.</p>
+                    <p>To keep the source compatibility of the <strong>addis</strong> and <strong>cau</strong> instructions, the assembler expands the value-range check for the <strong>addis</strong> instruction to -65536 to 65535. The sign bit is ignored and the assembler only ensures that the immediate value fits into 16 bits. This expansion does not affect the behavior of a 32-bit implementation or 32-bit mode in a 64-bit implementation.</p>
+                    <p>The <strong>addis</strong> instruction has different semantics in 32-bit mode than it does in 64-bit mode. If bit 32 is set, it propagates through the upper 32 bits of the 64-bit general-purpose register. Use caution when using the <strong>addis</strong> instruction to construct an unsigned integer. The <strong>addis</strong> instruction with an unsigned integer in 32-bit may not be directly ported to 64-bit mode. The code sequence needed to construct an unsigned integer in 64-bit mode is significantly different from that needed in 32-bit mode.</p>
+                `,
+                "tooltip": "Add Immediate Shifted",
+                "url": "https://www.ibm.com/docs/en/aix/7.3?topic=set-addis-cau-add-immediate-shifted-instruction"
+            };
+        case "ADDME":
+        case "ADDME.":
+        case "ADDMEO":
+        case "ADDMEO.":
+        case "AME":
+        case "AME.":
+        case "AMEO":
+        case "AMEO.":
+            return {
+                "html": `
+                    <p>The <strong>addme</strong> and <strong>ame</strong> instructions place the sum of the contents of general-purpose register (GPR) <em>RA</em>, the Carry bit of the Fixed-Point Exception Register, and -1 (0xFFFF FFFF<samp>)</samp> into the target GPR <em>RT</em>.</p>
+                    <p>The <strong>addme</strong> instruction has four syntax forms. Each syntax form has a different effect on Condition Register Field 0 and the Fixed-Point Exception Register.</p>
+                    <p>The <strong>ame</strong> instruction has four syntax forms. Each syntax form has a different effect on Condition Register Field 0 and the Fixed-Point Exception Register.</p>
+                    <p>The four syntax forms of the <strong>addme</strong> instruction and the four syntax forms of the <strong>ame</strong> instruction always affect the Carry bit (CA) in the Fixed-Point Exception Register. If the syntax form sets the Overflow Exception (OE) bit to 1, the instruction affects the Summary Overflow (SO) and Overflow (OV) bits in the Fixed-Point Exception Register. If the syntax form sets the Record (Rc) bit to 1, the instruction affects the Less Than (LT) zero, Greater Than (GT) zero, Equal To (EQ) zero, and Summary Overflow (SO) bits in Condition Register Field 0.</p>
+                `,
+                "tooltip": "Add to Minus One Extended",
+                "url": "https://www.ibm.com/docs/en/aix/7.3?topic=set-addme-ame-add-minus-one-extended-instruction"
+            };
+        case "ADDZE":
+        case "ADDZE.":
+        case "ADDZEO":
+        case "ADDZEO.":
+        case "AZE":
+        case "AZE.":
+        case "AZEO":
+        case "AZEO.":
+            return {
+                "html": `
+                    <p>The <strong>addze</strong> and <strong>aze</strong> instructions add the contents of general-purpose register (GPR) <em>RA</em>, the Carry bit, and 0x0000 0000 and place the result into the target GPR <em>RT</em>.</p>
+                    <p>The <strong>addze</strong> instruction has four syntax forms. Each syntax form has a different effect on Condition Register Field 0 and the Fixed-Point Exception Register.</p>
+                    <p>The <strong>aze</strong> instruction has four syntax forms. Each syntax form has a different effect on Condition Register Field 0 and the Fixed-Point Exception Register.</p>
+                    <p>The four syntax forms of the <strong>addze</strong> instruction and the four syntax forms of the <strong>aze</strong> instruction always affect the Carry bit (CA) in the Fixed-Point Exception Register. If the syntax form sets the Overflow Exception (OE) bit to 1, the instruction affects the Summary Overflow (SO) and Overflow (OV) bits in the Fixed-Point Exception Register. If the syntax form sets the Record (Rc) bit to 1, the instruction affects the Less Than (LT) zero, Greater Than (GT) zero, Equal To (EQ) zero, and Summary Overflow (SO) bits in Condition Register Field 0.</p>
+                `,
+                "tooltip": "Add to Zero Extended",
+                "url": "https://www.ibm.com/docs/en/aix/7.3?topic=set-addze-aze-add-zero-extended-instruction"
+            };
+        case "AND":
+        case "AND.":
+            return {
+                "html": `
+                    <p>The <strong>and</strong> instruction logically ANDs the contents of general-purpose register (GPR) <em>RS</em> with the contents of GPR <em>RB</em> and places the result into the target GPR <em>RA</em>.</p>
+                    <p>The <strong>and</strong> instruction has two syntax forms. Each syntax form has a different effect on Condition Register Field 0.</p>
+                    <p>The two syntax forms of the <strong>and</strong> instruction never affect the Fixed-Point Exception Register. If the syntax form sets the Record (Rc) bit to 1, the instruction affects the Less Than (LT) zero, Greater Than (GT) zero, Equal To (EQ) zero, and Summary Overflow (SO) bits in Condition Register Field 0.</p>
+                `,
+                "tooltip": "AND",
+                "url": "https://www.ibm.com/docs/en/aix/7.3?topic=set-instruction"
+            };
+        case "ANDC":
+        case "ANDC.":
+            return {
+                "html": `
+                    <p>The <strong>andc</strong> instruction logically ANDs the contents of general-purpose register (GPR) <em>RS</em> with the complement of the contents of GPR <em>RB</em> and places the result into GPR <em>RA</em>.</p>
+                    <p>The <strong>andc</strong> instruction has two syntax forms. Each syntax form has a different effect on Condition Register Field 0.</p>
+                    <p>The two syntax forms of the <strong>andc</strong> instruction never affect the Fixed-Point Exception Register. If the syntax form sets the Record (Rc) bit to 1, the instruction affects the Less Than (LT) zero, Greater Than (GT) zero, Equal To (EQ) zero, and Summary Overflow (SO) bits in Condition Register Field 0.</p>
+                `,
+                "tooltip": "AND with Complement",
+                "url": "https://www.ibm.com/docs/en/aix/7.3?topic=set-andc-complement-instruction"
+            };
+        case "ANDI.":
+        case "ANDIL.":
+            return {
+                "html": `
+                    <p>The <strong>andi.</strong> and <strong>andil.</strong> instructions logically AND the contents of general-purpose register (GPR) <em>RS</em> with the concatenation of x'0000' and a 16-bit unsigned integer, <em>UI,</em> and place the result in GPR <em>RA</em>.</p>
+                    <p>The <strong>andi.</strong> and <strong>andil.</strong> instructions have one syntax form and never affect the Fixed-Point Exception Register. The <strong>andi.</strong> and <strong>andil.</strong> instructions copies the Summary Overflow (SO) bit from the Fixed-Point Exception Register into Condition Register Field 0 and sets one of the Less Than (LT), Greater Than (GT), or Equal To (EQ) bits of Condition Register Field 0.</p>
+                `,
+                "tooltip": "AND Immediate",
+                "url": "https://www.ibm.com/docs/en/aix/7.3?topic=set-andi-andil-immediate-instruction"
+            };
+        case "ANDIS.":
+        case "ANDIU.":
+            return {
+                "html": `
+                    <p>The <strong>andis.</strong> and <strong>andiu.</strong> instructions logically AND the contents of general-purpose register (GPR) <em>RS</em> with the concatenation of a 16-bit unsigned integer, <em>UI,</em> and x'0000' and then place the result into the target GPR <em>RA</em>.</p>
+                    <p>The <strong>andis.</strong> and <strong>andiu.</strong> instructions have one syntax form and never affect the Fixed-Point Exception Register. The <strong>andis.</strong> and <strong>andiu.</strong> instructions set the Less Than (LT) zero, Greater Than (GT) zero, Equal To (EQ) zero, or Summary Overflow (SO) bit in Condition Register Field 0.</p>
+                `,
+                "tooltip": "AND Immediate Shifted",
+                "url": "https://www.ibm.com/docs/en/aix/7.3?topic=set-andis-andiu-immediate-shifted-instruction"
+            }
+    }
+};
