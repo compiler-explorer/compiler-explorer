be19b4c12e6e6f505bbefe43a15b05e1a1213534
Partouf <partouf@gmail.com>
2024-07-30 03:48:18 +0200

    retrieve remote archs from api

--- lib/execution/execution-query.ts
+++ lib/execution/execution-query.ts
@@ -1,12 +1,31 @@
 import {BuildResult} from '../../types/compilation/compilation.interfaces.js';
-import {ExecutionSpecialty} from '../../types/execution/execution-triple.js';
+import {BaseExecutionTriple, ExecutionSpecialty} from '../../types/execution/execution-triple.js';
 
 import {ExecutionTriple} from './execution-triple.js';
 
+async function retrieveAllRemoteExecutionArchs(): Promise<string[]> {
+    // eslint-disable-next-line n/no-unsupported-features/node-builtins
+    const response = await fetch('https://api.compiler-explorer.com/get_remote_execution_archs');
+
+    return await response.json();
+}
+
+const _available_remote_execution_archs: Promise<BaseExecutionTriple[]> = new Promise(resolve => {
+    retrieveAllRemoteExecutionArchs().then(archs => {
+        const triples: BaseExecutionTriple[] = [];
+        for (const arch of archs) {
+            const triple = new BaseExecutionTriple();
+            triple.parse(arch);
+            triples.push(triple);
+        }
+        resolve(triples);
+    });
+});
+
 export class RemoteExecutionQuery {
     static async isPossible(triple: ExecutionTriple): Promise<boolean> {
-        // todo: should request this from a database or something
-        return triple.getInstructionSet() === 'aarch64';
+        const triples = await _available_remote_execution_archs;
+        return !!triples.find(remote => remote.toString() === triple.toString());
     }
 
     static guessExecutionTripleForBuildresult(result: BuildResult): ExecutionTriple {
