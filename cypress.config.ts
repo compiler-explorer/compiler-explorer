baaf398b5c907c3b5cf68efa7c1827e892ca0a61
Partouf <partouf@gmail.com>
2024-09-12 19:45:32 +0200

    fix frontend tests by making viewport a bit bigger so everything fits

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,8 @@ import {defineConfig} from 'cypress';
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
+    viewportWidth: 1000,
+    viewportHeight: 700,
     e2e: {
         baseUrl: 'http://127.0.0.1:10240/',
         supportFile: 'cypress/support/utils.ts',
