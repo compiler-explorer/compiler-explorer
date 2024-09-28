7125de43ebd07c2ab0a65fcfcafee979d0b16abf
Sirui Mu <msrlancern@gmail.com>
2024-09-28 15:05:48 +0800

    Add ClangIR compilation options
    This patch adds two options to the clangir pane that affects the clangir
    compilation pipeline:
    
    * Flat CIR enables the emission of flat clangir CFG;
    * -fclangir-mem2reg enables the mem2reg pass on the flat clangir CFG.

--- lib/base-compiler.ts
+++ lib/base-compiler.ts
@@ -33,6 +33,7 @@ import _ from 'underscore';
 import {unique} from '../shared/common-utils.js';
 import {PPOptions} from '../static/panes/pp-view.interfaces.js';
 import {ParsedAsmResultLine} from '../types/asmresult/asmresult.interfaces.js';
+import {ClangirBackendOptions} from '../types/compilation/clangir.interfaces.js';
 import {
     BuildResult,
     BuildStep,
@@ -1390,13 +1391,27 @@ export class BaseCompiler implements ICompiler {
         return utils.changeExtension(inputFilename, '.cir');
     }
 
-    async generateClangir(inputFilename: string, options: string[]): Promise<ResultLine[]> {
+    async generateClangir(
+        inputFilename: string,
+        options: string[],
+        clangirOptions: ClangirBackendOptions,
+    ): Promise<ResultLine[]> {
         const outputFilename = this.getClangirOutputFilename(inputFilename);
 
-        let newOptions = [...options];
+        const newOptions = [...options];
+        if (clangirOptions.flattenCFG) {
+            newOptions.push('-Xclang', '-emit-cir-flat');
+        } else {
+            newOptions.push('-Xclang', '-emit-cir');
+        }
+
+        if (clangirOptions.mem2reg) {
+            newOptions.push('-Xclang', '-fclangir-mem2reg');
+        }
+
         // Replace `-o <name>.s` with `-o <name>.cir`
         newOptions.splice(options.indexOf('-o'), 2);
-        newOptions = newOptions.concat(['-Xclang', '-emit-cir', '-o', outputFilename]);
+        newOptions.push('-o', outputFilename);
 
         const execOptions = this.getDefaultExecOptions();
         const output = await this.runCompiler(this.compiler.exe, newOptions, this.filename(inputFilename), execOptions);
@@ -2310,7 +2325,7 @@ export class BaseCompiler implements ICompiler {
                       filters,
                   )
                 : undefined,
-            makeClangir ? this.generateClangir(inputFilename, options) : undefined,
+            makeClangir ? this.generateClangir(inputFilename, options, backendOptions.produceClangir) : undefined,
             makeOptPipeline
                 ? this.generateOptPipeline(inputFilename, options, filters, backendOptions.produceOptPipeline)
                 : undefined,
--- static/panes/clangir-view.ts
+++ static/panes/clangir-view.ts
@@ -36,13 +36,22 @@ import {extendConfig} from '../monaco-config.js';
 import {Hub} from '../hub.js';
 import {CompilationResult} from '../compilation/compilation.interfaces.js';
 import {CompilerInfo} from '../compiler.interfaces.js';
+import {Toggles} from '../widgets/toggles.js';
+import {ClangirBackendOptions} from '../../types/compilation/clangir.interfaces.js';
 
 export class Clangir extends MonacoPane<monaco.editor.IStandaloneCodeEditor, ClangirState> {
+    private options: Toggles;
+    private lastOptions: ClangirBackendOptions = {
+        flattenCFG: false,
+        mem2reg: false,
+    };
+
     constructor(hub: Hub, container: Container, state: ClangirState & MonacoPaneState) {
         super(hub, container, state);
         if (state.clangirOutput) {
             this.showClangirResults(state.clangirOutput);
         }
+        this.onOptionsChange(true);
     }
 
     override getInitialHTML(): string {
@@ -77,11 +86,43 @@ export class Clangir extends MonacoPane<monaco.editor.IStandaloneCodeEditor, Cla
         return 'ClangIR Viewer';
     }
 
+    override registerButtons(state: ClangirState): void {
+        super.registerButtons(state);
+        this.options = new Toggles(this.domRoot.find('.options'), state as unknown as Record<string, boolean>);
+        this.options.on('change', this.onOptionsChange.bind(this));
+    }
+
     override registerCallbacks(): void {
         const throttleFunction = _.throttle(event => this.onDidChangeCursorSelection(event), 500);
         this.editor.onDidChangeCursorSelection(event => throttleFunction(event));
         this.eventHub.emit('clangirViewOpened', this.compilerInfo.compilerId);
         this.eventHub.emit('requestSettings');
+    }
+
+    override getCurrentState(): MonacoPaneState {
+        return {
+            ...super.getCurrentState(),
+            ...this.options.get(),
+        };
+    }
+
+    onOptionsChange(force = false) {
+        const options = this.options.get();
+        const newOptions: ClangirBackendOptions = {
+            flattenCFG: options['flatten-cfg'],
+            mem2reg: options['mem2reg'],
+        };
+        let changed = false;
+        for (const k in newOptions) {
+            if (newOptions[k] !== this.lastOptions[k]) {
+                changed = true;
+                break;
+            }
+        }
+        this.lastOptions = newOptions;
+        if (changed || force) {
+            this.eventHub.emit('clangirViewOptionsUpdated', this.compilerInfo.compilerId, newOptions, true);
+        }
     }
 
     override onCompileResult(compilerId: number, compiler: CompilerInfo, result: CompilationResult): void {
