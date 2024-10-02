375d3c156c6e633a480b9f68aa42e31889f8418c
goto40 <pierre.bayerl@googlemail.com>
2024-10-02 07:20:53 +0200

    fix: reviewer comment

--- test/handlers/route-api-test.ts
+++ test/handlers/route-api-test.ts
@@ -1,4 +1,4 @@
-// Copyright (c) 2024, Pierre Bayerl
+// Copyright (c) 2024, Compiler Explorer Authors
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
