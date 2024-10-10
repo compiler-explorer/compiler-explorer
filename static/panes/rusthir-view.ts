11fdef2302d6e5f482074eb24c4a492b7a2ad6bb
Ofek Shilon <ofekshilon@gmail.com>
2024-10-10 11:54:17 +0300

    More tsification, mostly frontend

--- static/panes/rusthir-view.ts
+++ static/panes/rusthir-view.ts
@@ -69,7 +69,10 @@ export class RustHir extends MonacoPane<monaco.editor.IStandaloneCodeEditor, Rus
     }
 
     override registerCallbacks(): void {
-        const throttleFunction = _.throttle(event => this.onDidChangeCursorSelection(event), 500);
+        const throttleFunction = _.throttle(
+            (event: monaco.editor.ICursorSelectionChangedEvent) => this.onDidChangeCursorSelection(event),
+            500,
+        );
         this.editor.onDidChangeCursorSelection(event => throttleFunction(event));
         this.eventHub.emit('rustHirViewOpened', this.compilerInfo.compilerId);
         this.eventHub.emit('requestSettings');
