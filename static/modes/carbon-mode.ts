4dee78c1404b93585eb8c138095129f6580d0c2a
Ofek Shilon <ofekshilon@gmail.com>
2024-10-12 01:08:28 +0300

    Tsify

--- static/modes/carbon-mode.ts
+++ static/modes/carbon-mode.ts
@@ -23,6 +23,8 @@
 // POSSIBILITY OF SUCH DAMAGE.
 
 import * as monaco from 'monaco-editor';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore  "Could not find a declaration file"
 import * as cpp from 'monaco-editor/esm/vs/basic-languages/cpp/cpp';
 
 function definition(): monaco.languages.IMonarchLanguage {
--- types/compilation/compilation.interfaces.ts
+++ types/compilation/compilation.interfaces.ts
@@ -145,6 +145,11 @@ export type CompilationRequest = {
     bypassCache?: BypassCache;
 };
 
+export type PPOutput = {
+    numberOfLinesFiltered: number;
+    output: string;
+};
+
 export type CompilationResult = {
     code: number;
     timedOut: boolean;
@@ -171,10 +176,7 @@ export type CompilationResult = {
     languageId?: string;
     result?: CompilationResult; // cmake inner result
 
-    ppOutput?: {
-        numberOfLinesFiltered: number;
-        output: string;
-    };
+    ppOutput?: PPOutput;
 
     optOutput?: OptCodeEntry[];
     optPath?: string;
