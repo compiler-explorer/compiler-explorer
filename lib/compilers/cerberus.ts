ac20b87aded869f3694fb2bb2a1f61a368830ddf
Vadim Zaliva <lord@crocodile.org>
2024-10-16 17:14:14 -0700

    rebasing to latest version

--- lib/compilers/cerberus.ts
+++ lib/compilers/cerberus.ts
@@ -24,7 +24,11 @@
 
 import path from 'node:path';
 
-import {ParsedAsmResult} from '../../types/asmresult/asmresult.interfaces.js';
+import Parser from 'tree-sitter';
+import coreLanguage from 'tree-sitter-core';
+import {isNull} from 'underscore';
+
+import {AsmResultSource, ParsedAsmResultLine} from '../../types/asmresult/asmresult.interfaces.js';
 import {BypassCache, CacheKey, ExecutionOptions} from '../../types/compilation/compilation.interfaces.js';
 import type {PreliminaryCompilerInfo} from '../../types/compiler.interfaces.js';
 import {ExecutableExecutionOptions} from '../../types/execution/execution.interfaces.js';
@@ -34,6 +38,54 @@ import {BaseCompiler} from '../base-compiler.js';
 import {CompilationEnvironment} from '../compilation-env.js';
 import {logger} from '../logger.js';
 import * as utils from '../utils.js';
+
+type Range = {
+    start: Parser.Point;
+    end: Parser.Point;
+};
+
+type LocationRange = {
+    location: Range;
+    cursor: Range | Parser.Point | null;
+};
+
+type ASTwithLocations = {
+    root: Parser.SyntaxNode;
+    locations: Map<number, LocationRange>;
+};
+
+type SyntaxNodeWithLocation = {
+    node: Parser.SyntaxNode;
+    loc: LocationRange | undefined;
+};
+
+/*
+function point_to_string(point: Parser.Point): string {
+    return `${point.row}:${point.column}`;
+}
+
+function range_to_string(range: Range): string {
+    if (range === undefined || range === null)
+        return 'undefined'
+    else
+        return `${point_to_string(range.start)}-${point_to_string(range.end)}`;
+}
+
+function location_range_to_string(loc: LocationRange|undefined): string {
+    if (loc === undefined || loc === null)
+        return 'undefined';
+    else {
+        const location = range_to_string(loc.location);
+        if (loc.cursor === null)
+            return location;
+        else
+            if (loc.cursor instanceof Object && 'start' in loc.cursor && 'end' in loc.cursor)
+                return `${location} [${range_to_string(loc.cursor)}]`;
+            else
+                return `${location} [${point_to_string(loc.cursor)}]`;
+    }
+}
+*/
 
 export class CerberusCompiler extends BaseCompiler {
     static get key() {
@@ -75,7 +127,7 @@ export class CerberusCompiler extends BaseCompiler {
             customCwd: (result.dirPath as string) || path.dirname(outputFilename),
         };
 
-        const args = ['--pp=core', outputFilename];
+        const args = ['--pp_flags=loc', '--pp=core', outputFilename];
 
         const objResult = await this.exec(this.compiler.objdumper, args, execOptions);
         if (objResult.code === 0) {
@@ -117,14 +169,129 @@ export class CerberusCompiler extends BaseCompiler {
         };
     }
 
-    override async processAsm(result): Promise<ParsedAsmResult> {
+    private parse_position(node: Parser.SyntaxNode): Parser.Point | null {
+        if (node.type !== 'position') return null;
+
+        const filenameNode = node.childForFieldName('filename');
+        if (!filenameNode || filenameNode.text !== 'example.c') return null;
+
+        const rowNode = node.childForFieldName('line');
+        const columnNode = node.childForFieldName('column');
+        if (!rowNode || !columnNode) return null;
+
+        const row = parseInt(rowNode.text, 10);
+        const column = parseInt(columnNode.text, 10);
+        if (isNaN(row) || isNaN(column)) return null;
+
+        return {row: row, column: column};
+    }
+
+    /* Attempt to parse 'location' node.
+       Returns null if the node does not correspond to location range.        
+     */
+    private parse_location(node: Parser.SyntaxNode): LocationRange | null {
+        if (node.firstNamedChild === null || node.firstNamedChild.type !== 'location_range') return null;
+
+        const startNode = node.firstNamedChild.childForFieldName('start');
+        const endNode = node.firstNamedChild.childForFieldName('end');
+        if (!startNode || !endNode) return null;
+
+        const start = this.parse_position(startNode);
+        const end = this.parse_position(endNode);
+        if (isNull(start) || isNull(end)) return null;
+
+        const startCursorNode = node.firstNamedChild.childForFieldName('start_cursor');
+        if (!startCursorNode) return {location: {start: start, end: end}, cursor: null};
+        const start_cursor = this.parse_position(startCursorNode);
+        if (!start_cursor) return {location: {start: start, end: end}, cursor: null};
+
+        const endCursorNode = node.firstNamedChild.childForFieldName('end_cursor');
+        if (!endCursorNode) return {location: {start: start, end: end}, cursor: start_cursor};
+        const end_cursor = this.parse_position(endCursorNode);
+        if (end_cursor) {
+            return {location: {start: start, end: end}, cursor: {start: start_cursor, end: end_cursor}};
+        } else {
+            return {location: {start: start, end: end}, cursor: start_cursor};
+        }
+    }
+
+    private annotate_ast(node: Parser.SyntaxNode, locmap: Map<number, LocationRange>): void {
+        let loc: LocationRange | null = null;
+        for (const n of node.children) {
+            if (n.type === 'location') {
+                loc = this.parse_location(n);
+            } else {
+                if (loc !== null && n.isNamed) {
+                    //console.log(`ANNOTATING ${n.id} ${n.type} at ${point_to_string(n.startPosition)}-${point_to_string(n.endPosition)} with location ${location_range_to_string(loc)}`);
+                    locmap[n.id] = loc;
+                    loc = null;
+                }
+                this.annotate_ast(n, locmap);
+            }
+        }
+    }
+
+    private findNodesByRange(ast: ASTwithLocations, range: Range): SyntaxNodeWithLocation[] {
+        const matchingNodes: SyntaxNodeWithLocation[] = [];
+
+        function search(n: Parser.SyntaxNode): void {
+            if (
+                (n.startPosition.row < range.start.row ||
+                    (n.startPosition.row === range.start.row && n.startPosition.column <= range.start.column)) &&
+                (n.endPosition.row > range.end.row ||
+                    (n.endPosition.row === range.end.row && n.endPosition.column >= range.end.column))
+            ) {
+                const loc = ast.locations[n.id];
+                matchingNodes.push({node: n, loc: loc});
+            }
+
+            for (const child of n.children) {
+                search(child);
+            }
+        }
+
+        search(ast.root);
+        return matchingNodes.reverse();
+    }
+
+    override async processAsm(result) {
         // Handle "error" documents.
         if (!result.asm.includes('\n') && result.asm[0] === '<') {
             return {asm: [{text: result.asm, source: null}]};
         }
 
-        const lines = result.asm.split('\n');
-        const plines = lines.map((l: string) => ({text: l}));
+        const core = result.asm.replaceAll(/\n{3,}/g, '\n\n');
+        const parser = new Parser();
+        parser.setLanguage(coreLanguage);
+
+        const tree = parser.parse(core);
+        const ast: ASTwithLocations = {root: tree.rootNode, locations: new Map<number, LocationRange>()};
+        this.annotate_ast(ast.root, ast.locations);
+
+        const lines = core.split('\n');
+        const plines: ParsedAsmResultLine[] = lines.map((l: string, n: number) => {
+            const ltrimmed = l.replace(/^\s*{-# .+ #-}\s*/, '');
+            const start_col = l.length - ltrimmed.length;
+            const rtrimmed = ltrimmed.trimEnd();
+            const r: Range = {start: {row: n, column: start_col}, end: {row: n, column: start_col + rtrimmed.length}};
+            const matchingNodes = this.findNodesByRange(ast, r);
+
+            const coreNode = matchingNodes.find(x => x.loc !== undefined);
+            // the second disjunct is redundant and added to make typechecker happy
+            if (coreNode === undefined || coreNode.loc === undefined) {
+                //console.log(`No node with location for ${range_to_string(r)}`);
+                return {text: l};
+            } else {
+                const loc = coreNode.loc;
+                //console.log(`Found ${coreNode.node.id} ${coreNode.node.type} at ${point_to_string(coreNode.node.startPosition)}-${point_to_string(coreNode.node.endPosition)} for ${range_to_string(r)} with location ${location_range_to_string(coreNode.loc)}`);
+                const src: AsmResultSource = {
+                    file: null,
+                    line: loc.location.start.row,
+                    column: loc.location.start.column,
+                };
+                return {text: l, source: src};
+            }
+        });
         return {
             asm: plines,
             languageId: 'core',
