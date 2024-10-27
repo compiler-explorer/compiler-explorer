7a3a00e27382dafbb3130b81cfdaf8930800989e
Ofek <ofekshilon@gmail.com>
2024-10-27 09:09:44 +0200

    Extra safety around executeParameters.args (#7028)
    Hopefully fixes #7027

--- types/compilation/compilation.interfaces.ts
+++ types/compilation/compilation.interfaces.ts
@@ -49,6 +49,7 @@ export type ActiveTool = {
 };
 
 export type UnparsedExecutionParams = {
+    // TODO: narrow to string[]
     args?: string | string[];
     stdin?: string;
     runtimeTools?: ConfiguredRuntimeTools;
