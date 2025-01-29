650e706c4d2c1c7e2b00319d57c85c14f093a02f
Waqar Ahmed <waqar.17a@gmail.com>
2025-01-29 19:40:07 +0500

    Unset context.prevLabel when a label gets filtered (#7315)
    Otherwise subsequent unused label's directives end up being included in
    the processed asm even though the label itself got filtered

--- lib/parsers/asm-parser.ts
+++ lib/parsers/asm-parser.ts
@@ -646,6 +646,7 @@ export class AsmParser extends AsmRegex implements IAsmParser {
                 ) {
                     // It's an unused label.
                     if (filters.labels) {
+                        context.prevLabel = '';
                         continue;
                     }
                 } else {
