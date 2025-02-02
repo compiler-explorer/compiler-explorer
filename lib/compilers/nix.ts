390016bcb798fcc5d86101672d8cb0ff6cec596b
Mats Jun Larsen <mats@jun.codes>
2025-02-03 03:12:43 +0900

    Rebase branch on Biome fixes

--- lib/compilers/nix.ts
+++ lib/compilers/nix.ts
@@ -21,7 +21,7 @@
 // CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
-import path from 'path';
+import path from 'node:path';
 import fs from 'fs-extra';
 
 import {ExecutionOptions} from '../../types/compilation/compilation.interfaces.js';
