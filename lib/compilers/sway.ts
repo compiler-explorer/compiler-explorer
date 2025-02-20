69214880c8a1d2702eb1b657fd9fe4908e51251d
JoshuaBatty <joshpbatty@gmail.com>
2025-02-20 15:28:41 +1100

    feedback

--- lib/compilers/sway.ts
+++ lib/compilers/sway.ts
@@ -1,4 +1,28 @@
-import path from 'path';
+// Copyright (c) 2025, Compiler Explorer Authors
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+import path from 'node:path';
 
 import fsExtra from 'fs-extra';
 
@@ -177,7 +201,7 @@ export class SwayCompiler extends BaseCompiler {
                         .split('\n')
                         .slice(1)
                         .join('\n')
-                        .match(/(script|library|contract|predicate)\s*{[^]*?^}/m)?.[0] || '';
+                        .match(/(script|library|contract|predicate)\s*{.*?^}/ms)?.[0] || '';
                 irLines = relevantIr.split('\n').map(line => ({text: line}));
             }
         }
@@ -210,7 +234,7 @@ export class SwayCompiler extends BaseCompiler {
 const FORC_TOML_CONTENT = `[project]
 entry = "main.sw"
 license = "Apache-2.0"
-name = "godbolt"
+name = "compiler-explorer"
 
 [dependencies]
 std = { git = "https://github.com/FuelLabs/sway", tag = "v0.66.7" }
