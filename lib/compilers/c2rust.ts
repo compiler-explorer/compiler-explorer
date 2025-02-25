d0c91b07f8dc7123fe13f87ab1d1cfef498477bf
Nicole LeGare <legaren@immunant.com>
2025-02-25 23:35:11 +0000

    Remove unnecessary function overrides

--- lib/compilers/c2rust.ts
+++ lib/compilers/c2rust.ts
@@ -1,10 +1,16 @@
-import path from 'node:path';
-
 import type {ParseFiltersAndOutputOptions} from '../../types/features/filters.interfaces.js';
 import {BaseCompiler} from '../base-compiler.js';
 
 import {C2RustParser} from './argument-parsers.js';
 
+// TODO: By default the comments filter is enable and seems to be stripping out
+// the first line of the Rust output, likely because it's seeing the `#` for the
+// attribute on the first line as a comment. I tried using `getDefaultFilters`
+// to set all the filters to false, but that doesn't seem to disable the
+// comments filter by default. There's probably a different way to address this
+// issue, e.g. fixing the output language such that the filter doesn't see `#`
+// as a comment, but it's already set to Rust so I'm not sure what else needs to
+// be done there.
 export class C2RustCompiler extends BaseCompiler {
     static get key() {
         return 'c2rust';
@@ -14,39 +20,11 @@ export class C2RustCompiler extends BaseCompiler {
         return ['transpile'];
     }
 
-    override getOutputFilename(dirPath: string) {
-        return path.join(dirPath, 'example.rs');
-    }
-
     override getArgumentParserClass() {
         return C2RustParser;
     }
 
     override getCompilerResultLanguageId(filters?: ParseFiltersAndOutputOptions): string | undefined {
         return 'rust';
-    }
-
-    // TODO: By default the comments filter is enable and seems to be stripping
-    // out the first line of the Rust output, likely because it's seeing the `#`
-    // for the attribute on the first line as a comment. This function is trying
-    // to disable that by setting all the filters to false, but that doesn't
-    // seem to disable the comments filter by default. There's probably a
-    // different way to address this issue, e.g. fixing the output language such
-    // that the filter doesn't see `#` as a comment.
-    override getDefaultFilters(): ParseFiltersAndOutputOptions {
-        return {
-            binary: false,
-            execute: false,
-            demangle: false,
-            intel: false,
-            commentOnly: false,
-            directives: false,
-            labels: false,
-            optOutput: false,
-            libraryCode: false,
-            trim: false,
-            binaryObject: false,
-            debugCalls: false,
-        };
     }
 }
