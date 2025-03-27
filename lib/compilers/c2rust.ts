90aee8f04db6f0725e80eaeb2918861809307a19
Matt Godbolt <matt@godbolt.org>
2025-03-27 12:15:39 -0500

    Implement a no-op parser for handling rust

--- lib/compilers/c2rust.ts
+++ lib/compilers/c2rust.ts
@@ -3,19 +3,44 @@ import path from 'node:path';
 import type {ParseFiltersAndOutputOptions} from '../../types/features/filters.interfaces.js';
 import {BaseCompiler} from '../base-compiler.js';
 
+import {ParsedAsmResult} from '../../types/asmresult/asmresult.interfaces.js';
+import {PreliminaryCompilerInfo} from '../../types/compiler.interfaces.js';
+import {CompilationEnvironment} from '../compilation-env.js';
+import {IAsmParser} from '../parsers/asm-parser.interfaces.js';
+import {AsmRegex} from '../parsers/asmregex.js';
+import * as utils from '../utils.js';
 import {C2RustParser} from './argument-parsers.js';
 
-// TODO: By default the comments filter is enable and seems to be stripping out
-// the first line of the Rust output, likely because it's seeing the `#` for the
-// attribute on the first line as a comment. I tried using `getDefaultFilters`
-// to set all the filters to false, but that doesn't seem to disable the
-// comments filter by default. There's probably a different way to address this
-// issue, e.g. fixing the output language such that the filter doesn't see `#`
-// as a comment, but it's already set to Rust so I'm not sure what else needs to
-// be done there.
+// Simple parser that only does bare minimum, handling whitespace. In particular, Rust
+// comments `#[]` etc are important, and as this isn't even slightly "assembly" we don't
+// try and do anything like "dead code removal".
+class NoOpParser implements IAsmParser {
+    process(asmResult: string, filters: ParseFiltersAndOutputOptions): ParsedAsmResult {
+        let asmLines = utils.splitLines(asmResult);
+        if (filters.preProcessLines !== undefined) {
+            asmLines = filters.preProcessLines(asmLines);
+        }
+        const asm = asmLines.map(line => {
+            line = utils.expandTabs(line);
+
+            const text = AsmRegex.filterAsmLine(line, filters);
+
+            return {text, source: null, labels: []};
+        });
+
+        return {asm};
+    }
+}
+
 export class C2RustCompiler extends BaseCompiler {
     static get key() {
         return 'c2rust';
+    }
+
+    constructor(compilerInfo: PreliminaryCompilerInfo, env: CompilationEnvironment) {
+        super(compilerInfo, env);
+
+        this.asm = new NoOpParser();
     }
 
     override optionsForFilter(filters: ParseFiltersAndOutputOptions, outputFilename: string) {
