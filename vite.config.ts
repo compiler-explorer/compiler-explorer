47e628a5f6314379b9cc9b5900c477d5bfb82bf9
Mats Jun Larsen <mats@jun.codes>
2025-04-16 14:59:01 +0900

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from "vite"
 export default defineConfig({
     build: {
         manifest: true,
+        rollupOptions: {
+            input: {
+                "main.js": "./static/main.ts",
+            }
+        }
     },
 })
