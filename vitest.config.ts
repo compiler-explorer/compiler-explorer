76083bd96ded40f18c24b20754a7162d1c5e9192
Matt Godbolt <matt@godbolt.org>
2025-06-18 14:47:05 -0500

    Work in progress on making unit testable frontend tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,8 +6,23 @@ export default defineConfig({
             provider: 'v8',
             reporter: ['text', 'json', 'html'],
         },
-        include: ['test/**/*.ts'],
-        exclude: ['test/_*.ts', 'test/utils.ts'],
-        setupFiles: ['/test/_setup-fake-aws.ts', '/test/_setup-log.ts'],
+        projects: [
+            {
+                test: {
+                    name: 'unit',
+                    include: ['test/**/*.ts'],
+                    exclude: ['test/_*.ts', 'test/utils.ts', 'test/static'],
+                    setupFiles: ['/test/_setup-fake-aws.ts', '/test/_setup-log.ts'],
+                },
+            },
+            {
+                test: {
+                    name: 'frontend unit',
+                    include: ['test/static/**/*.ts'],
+                    setupFiles: ['/test/_setup-fake-aws.ts', '/test/_setup-log.ts', '/test/static/_setup-dom.ts'],
+                    environment: 'jsdom',
+                },
+            },
+        ],
     },
 });
