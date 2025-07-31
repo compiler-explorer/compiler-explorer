bdd776634f144126673e1348a200aa60f14d1038
Shawn Zhong <github@shawnzhong.com>
2025-07-31 06:36:48 -0700

    [Triton] Add Triton 3.4.0 and tests (#7974)
    - Add Triton 3.4.0 released July 30th (c.f.
    https://github.com/compiler-explorer/infra/pull/1727). It came sooner
    than I expected.
    - Add `test_triton_wrapper.py` for testing the correctness of wrapper.

--- etc/scripts/triton_wrapper.py
+++ etc/scripts/triton_wrapper.py
@@ -1,6 +1,29 @@
+# Copyright (c) 2025, Compiler Explorer Authors
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright notice,
+#       this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
 import argparse
 import importlib.util
-import inspect
 import json
 import os
 from pathlib import Path
@@ -123,8 +146,8 @@ def setup_triton(
     2. Even if Triton adds such support, older versions of Triton (e.g., v2.3.x) still
        requirs such patching to work.
 
-    This function is a collection of hacks. It has been tested to work with Triton
-    2.3.0, 2.3.1, 3.0.0, 3.1.0, 3.2.0, 3.3.0, 3.3.1.
+    This function is a collection of hacks. It has been tested to work with Triton versions:
+    2.3.0, 2.3.1, 3.0.0, 3.1.0, 3.2.0, 3.3.0, 3.3.1, 3.4.0.
     """
 
     os.environ["TRITON_ALWAYS_COMPILE"] = "1"
