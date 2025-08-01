7dc05e1d4e61d325223429b80147a02f217fdf5b
Matt Godbolt <matt@godbolt.org>
2025-07-31 19:29:13 -0500

    More changes from feedback

--- etc/scripts/parsed-pug/parsed_pug_file.js
+++ etc/scripts/parsed-pug/parsed_pug_file.js
@@ -9,7 +9,7 @@ const path = require('path');
 // just update the hash here.
 const expectedHashes = {
     cookies: '08712179739d3679',
-    privacy: '31cfc0507c33c53c',
+    privacy: '074dd09a246ad6fe',
 };
 
 function _execGit(command) {
