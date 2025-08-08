b0c0c64f2bf70a830282b290152f2dfb26fe909b
A. R. Shajii <ars@ars.me>
2025-08-08 14:14:53 -0400

    Add support for asm output

--- lib/compilers/codon.ts
+++ lib/compilers/codon.ts
@@ -1,5 +1,6 @@
 import type {PreliminaryCompilerInfo} from '../../types/compiler.interfaces.js';
 import type {ParseFiltersAndOutputOptions} from '../../types/features/filters.interfaces.js';
+import type {SelectedLibraryVersion} from '../../types/libraries/libraries.interfaces.js';
 import {BaseCompiler} from '../base-compiler.js';
 import {CompilationEnvironment} from '../compilation-env.js';
 
@@ -17,10 +18,20 @@ export class CodonCompiler extends BaseCompiler {
         outputFilename: string,
         userOptions?: string[],
     ): string[] {
-        return ['build', '-llvm', '-o', this.filename(outputFilename)];
+        filters.binary = !(userOptions?.includes('-llvm') || userOptions?.includes('--llvm'));
+        return ['build', '-o', this.filename(outputFilename)];
+    }
+
+    override getSharedLibraryPathsAsArguments(
+        libraries: SelectedLibraryVersion[],
+        libDownloadPath: string | undefined,
+        toolchainPath: string | undefined,
+        dirPath: string,
+    ): string[] {
+        return [];
     }
 
     override getCompilerResultLanguageId(filters?: ParseFiltersAndOutputOptions): string | undefined {
-        return 'llvm-ir';
+        return filters?.binary ? 'asm' : 'llvm-ir';
     }
 }
