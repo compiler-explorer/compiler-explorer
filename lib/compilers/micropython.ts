905f240a453e4f4f8cc09f391cb86a82a83a0008
Anson Mansfield <anson.mansfield@gmail.com>
2025-11-06 15:52:11 -0500

    lib/compilers/micropython: Add MicroPython compiler.
    Signed-off-by: Anson Mansfield <anson.mansfield@gmail.com>

---
+++ lib/compilers/micropython.ts
@@ -0,0 +1,83 @@
+import path from 'node:path';
+import type {
+    ActiveTool,
+    BypassCache,
+    ExecutionParams,
+    FiledataPair,
+} from '../../types/compilation/compilation.interfaces.js';
+import type {ConfiguredOverrides} from '../../types/compilation/compiler-overrides.interfaces.js';
+import type {BasicExecutionResult, ExecutableExecutionOptions} from '../../types/execution/execution.interfaces.js';
+import type {ParseFiltersAndOutputOptions} from '../../types/features/filters.interfaces.js';
+import type {SelectedLibraryVersion} from '../../types/libraries/libraries.interfaces.js';
+import {BaseCompiler} from '../base-compiler.js';
+import type {IExecutionEnvironment} from '../execution/execution-env.interfaces.js';
+
+export class MicroPythonCompiler extends BaseCompiler {
+    static get key() {
+        return 'micropython';
+    }
+
+    override getOutputFilename(dirPath: string) {
+        return path.join(dirPath, `${path.basename(this.compileFilename, this.lang.extensions[0])}.mpy`);
+    }
+
+    override prepareArguments(
+        userOptions: string[],
+        filters: ParseFiltersAndOutputOptions,
+        backendOptions: Record<string, any>,
+        inputFilename: string,
+        outputFilename: string,
+        libraries: SelectedLibraryVersion[],
+        overrides: ConfiguredOverrides,
+    ) {
+        return [
+            '-o',
+            outputFilename,
+            ...(filters.dontMaskFilenames ? [] : ['-s', this.compileFilename]),
+            ...userOptions,
+            '--',
+            inputFilename,
+        ];
+    }
+
+    override async compile(
+        source: string,
+        options: string[],
+        backendOptions: Record<string, any>,
+        filters: ParseFiltersAndOutputOptions,
+        bypassCache: BypassCache,
+        tools: ActiveTool[],
+        executeParameters: ExecutionParams,
+        libraries: SelectedLibraryVersion[],
+        files: FiledataPair[],
+    ) {
+        filters.binary = !filters.binary;
+        return super.compile(
+            source,
+            options,
+            backendOptions,
+            filters,
+            bypassCache,
+            tools,
+            executeParameters,
+            libraries,
+            files,
+        );
+    }
+
+    override async runExecutable(
+        executable: string,
+        executeParameters: ExecutableExecutionOptions,
+        homeDir: string,
+    ): Promise<BasicExecutionResult> {
+        const execOptionsCopy: ExecutableExecutionOptions = JSON.parse(
+            JSON.stringify(executeParameters),
+        ) as ExecutableExecutionOptions;
+
+        execOptionsCopy.args = [...this.compiler.executionWrapperArgs, ...execOptionsCopy.args];
+        executable = this.compiler.executionWrapper;
+
+        const execEnv: IExecutionEnvironment = new this.executionEnvironmentClass(this.env);
+        return execEnv.execBinary(executable, execOptionsCopy, homeDir);
+    }
+}
