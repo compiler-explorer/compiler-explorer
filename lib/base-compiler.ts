compiler-explorer/compiler-explorer

commit 079d4957571398f72482ce156076b2d4fbddbbf7

Compiler overrides (#5001)

lib/toolchain-utils.ts
@@ -23,15 +23,21 @@
 // POSSIBILITY OF SUCH DAMAGE.
 
 import path from 'path';
+import fs from 'fs-extra';
 
 import {splitArguments} from './utils.js';
+import {PreliminaryCompilerInfo} from '../types/compiler.interfaces.js';
+import {CompilerOverrideOptions} from '../types/compilation/compiler-overrides.interfaces.js';
 
-export function getToolchainPath(compilerExe: string | null, compilerOptions?: string): string | false {
-    const options = compilerOptions ? splitArguments(compilerOptions) : [];
-    const existingChain = options.find(elem => elem.includes('--gcc-toolchain='));
+export const clang_style_toolchain_flag = '--gcc-toolchain=';
+export const icc_style_toolchain_flag = '--gxx-name=';
+export const clang_style_sysroot_flag = '--sysroot=';
+
+export function getToolchainPathWithOptionsArr(compilerExe: string | null, options: string[]): string | false {
+    const existingChain = options.find(elem => elem.includes(clang_style_toolchain_flag));
     if (existingChain) return existingChain.substring(16);
 
-    const gxxname = options.find(elem => elem.includes('--gxx-name='));
+    const gxxname = options.find(elem => elem.includes(icc_style_toolchain_flag));
     if (gxxname) {
         return path.resolve(path.dirname(gxxname.substring(11)), '..');
     } else if (typeof compilerExe === 'string' && compilerExe.includes('/g++')) {
@@ -41,6 +47,111 @@ export function getToolchainPath(compilerExe: string | null, compilerOptions?: s
     }
 }
 
+export function getToolchainPath(compilerExe: string | null, compilerOptions?: string): string | false {
+    const options = compilerOptions ? splitArguments(compilerOptions) : [];
+    return getToolchainPathWithOptionsArr(compilerExe, options);
+}
+
 export function removeToolchainArg(compilerOptions: string[]): string[] {
-    return compilerOptions.filter(elem => !elem.includes('--gcc-toolchain=') && !elem.includes('--gxx-name='));
+    return compilerOptions.filter(
+        elem => !elem.includes(clang_style_toolchain_flag) && !elem.includes(icc_style_toolchain_flag),
+    );
+}
+
+export function removeSysrootArg(compilerOptions: string[]): string[] {
+    return compilerOptions.filter(elem => !elem.includes(clang_style_sysroot_flag));
+}
+
+export function replaceToolchainArg(compilerOptions: string[], newPath: string): string[] {
+    return compilerOptions.map(elem => {
+        if (elem.includes(clang_style_toolchain_flag)) {
+            return clang_style_toolchain_flag + path.normalize(newPath);
+        } else if (elem.includes(icc_style_toolchain_flag)) {
+            return icc_style_toolchain_flag + path.normalize(newPath);
+        }
+
+        return elem;
+    });
+}
+
+export function replaceSysrootArg(compilerOptions: string[], newPath: string): string[] {
+    return compilerOptions.map(elem => {
+        if (elem.includes(clang_style_sysroot_flag)) {
+            return clang_style_sysroot_flag + path.normalize(newPath);
+        }
+
+        return elem;
+    });
+}
+
+export function getToolchainFlagFromOptions(options: string[]): string | false {
+    for (const elem of options) {
+        if (elem.includes(clang_style_toolchain_flag)) return clang_style_toolchain_flag;
+        if (elem.includes(icc_style_toolchain_flag)) return icc_style_toolchain_flag;
+    }
+
+    return false;
+}
+
+export function hasToolchainArg(options: string[]): boolean {
+    return !!getToolchainFlagFromOptions(options);
+}
+
+export function getSysrootFlagFromOptions(options: string[]): string | false {
+    for (const elem of options) {
+        if (elem.includes(clang_style_sysroot_flag)) return clang_style_sysroot_flag;
+    }
+
+    return false;
+}
+
+export function hasSysrootArg(options: string[]): boolean {
+    return !!getSysrootFlagFromOptions(options);
+}
+
+export async function getPossibleGccToolchainsFromCompilerInfo(
+    compilers: PreliminaryCompilerInfo[],
+): Promise<CompilerOverrideOptions> {
+    const overrideOptions: CompilerOverrideOptions = [];
+    for (const compiler of compilers) {
+        if (
+            compiler.compilerCategories?.includes('gcc') &&
+            !compiler.compilerCategories?.includes('mingw') &&
+            !compiler.hidden &&
+            compiler.exe &&
+            path.isAbsolute(compiler.exe)
+        ) {
+            try {
+                await fs.stat(compiler.exe);
+            } catch {
+                continue;
+            }
+
+            const toolchainPath = path.resolve(path.dirname(compiler.exe), '..');
+            if (!overrideOptions.find(opt => opt.value === toolchainPath)) {
+                overrideOptions.push({
+                    name: compiler.name,
+                    value: toolchainPath,
+                });
+            }
+        }
+    }
+    return overrideOptions;
+}
+
+export function getSpecificTargetBasedOnToolchainPath(target: string, toolchainPath: string) {
+    const lastPathBit = path.basename(toolchainPath);
+    if (lastPathBit.startsWith(target)) {
+        return lastPathBit;
+    }
+
+    return target;
+}
+
+export function getSysrootByToolchainPath(toolchainPath: string): string | undefined {
+    const lastPathBit = path.basename(toolchainPath);
+    const possibleSysrootPath = path.join(toolchainPath, lastPathBit, 'sysroot');
+    if (fs.existsSync(possibleSysrootPath)) {
+        return possibleSysrootPath;
+    }
 }

compiler-args-app.ts
@@ -61,6 +61,10 @@ const compilerParsers = {
     ts: Parsers.TypeScriptNativeParser,
     turboc: Parsers.TurboCParser,
     toit: Parsers.ToitParser,
+    circle: Parsers.CircleParser,
+    ghc: Parsers.GHCParser,
+    tendra: Parsers.TendraParser,
+    golang: Parsers.GolangParser,
 };
 
 class CompilerArgsApp {
@@ -88,28 +92,64 @@ class CompilerArgsApp {
         }
     }
 
-    async doTheParsing() {
+    async getPossibleStdvers() {
+        const parser = this.getParser();
+        return await parser.getPossibleStdvers(this.compiler);
+    }
+
+    async getPossibleTargets() {
+        const parser = this.getParser();
+        return await parser.getPossibleTargets(this.compiler);
+    }
+
+    async getPossibleEditions() {
+        const parser = this.getParser();
+        return await parser.getPossibleEditions(this.compiler);
+    }
+
+    getParser() {
         if (compilerParsers[this.parserName]) {
-            const parser = compilerParsers[this.parserName];
-            await parser.parse(this.compiler);
+            return compilerParsers[this.parserName];
         } else {
             console.error('Unknown parser type');
             process.exit(1);
         }
     }
 
-    print() {
+    async doTheParsing() {
+        const parser = this.getParser();
+        await parser.parse(this.compiler);
+        const options = this.compiler.possibleArguments.possibleArguments;
+        if (parser.hasSupportStartsWith(options, '--target=')) {
+            console.log('supportsTargetIs');
+        } else if (parser.hasSupportStartsWith(options, '--target ')) {
+            console.log('supportsTarget');
+        } else if (parser.hasSupportStartsWith(options, '--march=')) {
+            console.log('supportsMarch');
+        } else {
+            console.log('none of the things?');
+        }
+    }
+
+    async print() {
         const args = _.keys(this.compiler.possibleArguments.possibleArguments);
         for (const arg of args) {
             console.log(padRight(arg, this.pad) + this.compiler.possibleArguments.possibleArguments[arg].description);
         }
+
+        console.log('Stdvers:');
+        console.log(await this.getPossibleStdvers());
+        console.log('Targets:');
+        console.log(await this.getPossibleTargets());
+        console.log('Editions:');
+        console.log(await this.getPossibleEditions());
     }
 }
 
 if (!opts.parser || !opts.exe) {
     console.error(
         'Usage: ' +
-            'node -r esm -r ts-node/register compiler-args-app.ts ' +
+            'ts-node-esm compiler-args-app.ts ' +
             '--parser=<compilertype> --exe=<path> [--padding=<number>]\n' +
             'for example: --parser=clang --exe=/opt/compiler-explorer/clang-15.0.0/bin/clang++ --padding=50',
     );
@@ -117,8 +157,6 @@ if (!opts.parser || !opts.exe) {
 } else {
     const app = new CompilerArgsApp();
     app.doTheParsing()
-        .then(() => {
-            app.print();
-        })
+        .then(() => app.print())
         .catch(e => console.error(e));
 }

lib/compilation-env.ts
@@ -38,6 +38,7 @@ import {logger} from './logger.js';
 import {CompilerProps} from './properties.js';
 import type {PropertyGetter} from './properties.interfaces.js';
 import {unwrap} from './assert.js';
+import {CompilerOverrideOptions} from '../types/compilation/compiler-overrides.interfaces.js';
 
 export class CompilationEnvironment {
     ceProps: PropertyGetter;
@@ -52,6 +53,7 @@ export class CompilationEnvironment {
     multiarch: string | null;
     baseEnv: Record<string, string | undefined>;
     formatHandler: FormattingHandler;
+    possibleToolchains?: CompilerOverrideOptions;
 
     constructor(compilerProps, compilationQueue, doCache) {
         this.ceProps = compilerProps.ceProps;
@@ -107,6 +109,14 @@ export class CompilationEnvironment {
             env.CPLUS_INCLUDE_PATH = '/usr/include/' + this.multiarch;
         }
         return env;
+    }
+
+    setPossibleToolchains(toolchains: CompilerOverrideOptions) {
+        this.possibleToolchains = toolchains;
+    }
+
+    getPossibleToolchains(): CompilerOverrideOptions {
+        return this.possibleToolchains || [];
     }
 
     async cacheGet(object: CacheableValue) {

lib/compiler-finder.ts
@@ -42,6 +42,7 @@ import {ClientOptionsHandler, OptionHandlerArguments} from './options-handler.js
 import {CompilerProps} from './properties.js';
 import type {PropertyGetter} from './properties.interfaces.js';
 import {basic_comparator, remove} from './common-utils.js';
+import {getPossibleGccToolchainsFromCompilerInfo} from './toolchain-utils.js';
 
 const sleep = promisify(setTimeout);
 
@@ -332,6 +333,7 @@ export class CompilerFinder {
                 name: props<string>('licenseName'),
                 preamble: props<string>('licensePreamble'),
             },
+            possibleOverrides: [],
         };
 
         if (props('demanglerClassFile') !== undefined) {
@@ -463,6 +465,10 @@ export class CompilerFinder {
 
     async find() {
         const compilerList = await this.getCompilers();
+
+        const toolchains = await getPossibleGccToolchainsFromCompilerInfo(compilerList);
+        this.compileHandler.setPossibleToolchains(toolchains);
+
         const compilers = await this.compileHandler.setCompilers(compilerList, this.optionsHandler.get());
         if (!compilers) {
             logger.error('#### No compilers found: no compilation will be done!');

lib/compilers/ada.ts
@@ -29,6 +29,7 @@ import type {PreliminaryCompilerInfo} from '../../types/compiler.interfaces.js';
 import type {ParseFiltersAndOutputOptions} from '../../types/features/filters.interfaces.js';
 import {BaseCompiler} from '../base-compiler.js';
 import * as utils from '../utils.js';
+import type {ConfiguredOverrides} from '../../types/compilation/compiler-overrides.interfaces.js';
 
 export class AdaCompiler extends BaseCompiler {
     static get key() {
@@ -76,6 +77,7 @@ export class AdaCompiler extends BaseCompiler {
         inputFilename: string,
         outputFilename: string,
         libraries,
+        overrides: ConfiguredOverrides,
     ) {
         backendOptions = backendOptions || {};
 

lib/compilers/assembly.ts
@@ -143,6 +143,8 @@ export class AssemblyCompiler extends BaseCompiler {
         buildFilters.binary = true;
         buildFilters.execute = false;
 
+        const overrides = this.sanitizeCompilerOverrides(key.backendOptions.overrides);
+
         const compilerArguments = _.compact(
             this.prepareArguments(
                 key.options,
@@ -151,6 +153,7 @@ export class AssemblyCompiler extends BaseCompiler {
                 inputFilename,
                 outputFilename,
                 key.libraries,
+                overrides,
             ),
         );
 

commit ec1ac4703384013b310ff1bbafaccd449cef59fc

Fix `any` types for delayCleanupTemp and toolchainPath (#8409)
## Summary

- Fixed `delayCleanupTemp: any` → `boolean`
- Fixed `toolchainPath: any` → `string | undefined`
- Changed `getToolchainPath()` to return `string | undefined` instead of
`string | false` (more idiomatic TypeScript)
- Fixed a latent bug in `changeOptionsBasedOnOverrides`

### Bug fix details

The bug was introduced in commit 079d49575 ("Compiler overrides #5001")
on May 16, 2023.

The original **correct** code (from commit 86a84f7f6) was:
```typescript
let sysrootPath: string | undefined;
const overriddenToolchainPath = this.getOverridenToolchainPath(overrides);
if (overriddenToolchainPath) {
    sysrootPath = getSysrootByToolchainPath(overriddenToolchainPath);
}
```

The buggy refactor changed it to:
```typescript
const sysrootPath = overriddenToolchainPath ?? 
    getSysrootByToolchainPath(overriddenToolchainPath);
```

This had two issues:
1. **The fallback was never reached**: `false ?? x` returns `false`
because `false` is not nullish (only `null`/`undefined` trigger `??`)
2. **The fallback passed the wrong variable**: It passed
`overriddenToolchainPath` to the fallback, but when the fallback is
needed, that variable would be `false`/`undefined`

Fixed to match the original correct behaviour:
```typescript
const sysrootPath = overriddenToolchainPath
    ? getSysrootByToolchainPath(overriddenToolchainPath)
    : this.toolchainPath
      ? getSysrootByToolchainPath(this.toolchainPath)
      : undefined;
```

The sysroot is now always computed via `getSysrootByToolchainPath()`,
since toolchain paths (e.g. `/opt/compiler-explorer/gcc-7.2.0`) differ
from sysroot paths (e.g.
`/opt/compiler-explorer/gcc-7.2.0/x86_64-pc-linux-gnu/sysroot`).

## Test plan
- [x] `npm run ts-check` passes
- [x] `npm run test-min` passes
- [x] Pre-commit hooks pass

🤖 Generated with [Claude Code](https://claude.ai/code)

---------

Co-authored-by: Claude Opus 4.5 <noreply@anthropic.com>

lib/base-compiler.ts
@@ -190,7 +190,7 @@ export class BaseCompiler {
     protected env: CompilationEnvironment;
     protected compilerProps: PropertyGetter;
     protected alwaysResetLdPath: boolean;
-    protected delayCleanupTemp: any;
+    protected delayCleanupTemp: boolean;
     protected stubRe: RegExp;
     protected stubText: string;
     protected compilerWrapper: string | undefined;
@@ -198,7 +198,7 @@ export class BaseCompiler {
     protected llvmIr: LlvmIrParser;
     protected llvmPassDumpParser: LlvmPassDumpParser;
     protected llvmAst: LlvmAstParser;
-    protected toolchainPath: any;
+    protected toolchainPath: string | undefined;
     public possibleArguments: CompilerArguments;
     protected possibleTools: ITool[];
     protected demanglerClass: typeof BaseDemangler | null = null;
@@ -1100,7 +1100,7 @@ export class BaseCompiler {
         );
     }
 
-    getDefaultOrOverridenToolchainPath(overrides: ConfiguredOverrides): string {
+    getDefaultOrOverridenToolchainPath(overrides: ConfiguredOverrides): string | undefined {
         for (const override of overrides) {
             if (override.name !== CompilerOverrideType.env && override.value) {
                 const possible = this.compiler.possibleOverrides?.find(ov => ov.name === override.name);
@@ -1113,7 +1113,7 @@ export class BaseCompiler {
         return this.toolchainPath;
     }
 
-    getOverridenToolchainPath(overrides: ConfiguredOverrides): string | false {
+    getOverridenToolchainPath(overrides: ConfiguredOverrides): string | undefined {
         for (const override of overrides) {
             if (override.name !== CompilerOverrideType.env && override.value) {
                 const possible = this.compiler.possibleOverrides?.find(ov => ov.name === override.name);
@@ -1123,13 +1123,16 @@ export class BaseCompiler {
             }
         }
 
-        return false;
+        return undefined;
     }
 
     changeOptionsBasedOnOverrides(options: string[], overrides: ConfiguredOverrides): string[] {
         const overriddenToolchainPath = this.getOverridenToolchainPath(overrides);
-        const sysrootPath: string | false =
-            overriddenToolchainPath ?? getSysrootByToolchainPath(overriddenToolchainPath);
+        const sysrootPath = overriddenToolchainPath
+            ? getSysrootByToolchainPath(overriddenToolchainPath)
+            : this.toolchainPath
+              ? getSysrootByToolchainPath(this.toolchainPath)
+              : undefined;
         const targetOverride = overrides.find(ov => ov.name === CompilerOverrideType.arch);
         const hasNeedForSysRoot =
             targetOverride && targetOverride.name !== CompilerOverrideType.env && !targetOverride.value.includes('x86');
@@ -1246,7 +1249,7 @@ export class BaseCompiler {
     protected getLibLinkInfo(
         filters: ParseFiltersAndOutputOptions,
         libraries: SelectedLibraryVersion[],
-        toolchainPath: string,
+        toolchainPath: string | undefined,
         dirPath: string,
     ) {
         let libLinks: string[] = [];
@@ -2685,7 +2688,7 @@ export class BaseCompiler {
         execParams: ExecutionOptionsWithEnv,
         dirPath: string,
         libsAndOptions: LibsAndOptions,
-        toolchainPath: string,
+        toolchainPath: string | undefined,
     ): ExecutionOptionsWithEnv {
         const cmakeExecParams = Object.assign({}, execParams);
 

lib/toolchain-utils.ts
@@ -33,7 +33,7 @@ export const clang_style_toolchain_flag = '--gcc-toolchain=';
 export const icc_style_toolchain_flag = '--gxx-name=';
 export const clang_style_sysroot_flag = '--sysroot=';
 
-export function getToolchainPathWithOptionsArr(compilerExe: string | null, options: string[]): string | false {
+export function getToolchainPathWithOptionsArr(compilerExe: string | null, options: string[]): string | undefined {
     const existingChain = options.find(elem => elem.includes(clang_style_toolchain_flag));
     if (existingChain) return existingChain.substring(16);
 
@@ -44,10 +44,10 @@ export function getToolchainPathWithOptionsArr(compilerExe: string | null, optio
     if (typeof compilerExe === 'string' && (compilerExe.includes('/g++') || compilerExe.endsWith('-g++'))) {
         return path.resolve(path.dirname(compilerExe), '..');
     }
-    return false;
+    return undefined;
 }
 
-export function getToolchainPath(compilerExe: string | null, compilerOptions?: string): string | false {
+export function getToolchainPath(compilerExe: string | null, compilerOptions?: string): string | undefined {
     const options = compilerOptions ? splitArguments(compilerOptions) : [];
     return getToolchainPathWithOptionsArr(compilerExe, options);
 }

lib/tooling/compiler-dropin-tool.ts
@@ -43,7 +43,7 @@ export class CompilerDropinTool extends BaseTool {
         this.addOptionsToToolArgs = false;
     }
 
-    getToolchainPath(compilationInfo: CompilationInfo): string | false {
+    getToolchainPath(compilationInfo: CompilationInfo): string | undefined {
         return getToolchainPath(compilationInfo.compiler.exe, compilationInfo.compiler.options);
     }
 